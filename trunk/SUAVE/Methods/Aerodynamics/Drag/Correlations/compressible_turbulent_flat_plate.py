eef778fe3a1a38348c3ad387e477ef7148923ceb
Tim Momose <tmomose@Tim-MacBookPro.(none)>
2014-04-29 10:09:35 -0700

    Fixed variable naming inconsistency

--- trunk/SUAVE/Methods/Aerodynamics/Drag/Correlations/compressible_turbulent_flat_plate.py
+++ trunk/SUAVE/Methods/Aerodynamics/Drag/Correlations/compressible_turbulent_flat_plate.py
@@ -22,19 +22,19 @@ import numpy as np
 # ----------------------------------------------------------------------
 
 
-def compressible_turbulent_flat_plate(Re,Ma,T):
+def compressible_turbulent_flat_plate(Re,Ma,Tc):
     
     # incompressible skin friction coefficient
-    cf_inc = 0.455/(np.log10(Re_w))**2.58
+    cf_inc = 0.455/(np.log10(Re))**2.58
     
     # compressibility correction
-    Tw = Tc * (1. + 0.178*Mc**2.)
-    Td = Tc * (1. + 0.035*Mc**2. + 0.45*(Tw/Tc - 1.))
+    Tw = Tc * (1. + 0.178*Ma**2.)
+    Td = Tc * (1. + 0.035*Ma**2. + 0.45*(Tw/Tc - 1.))
     k_comp = (Tc/Td) 
     
     # reynolds correction
-    Rd_w = Re_w * (Td/Tc)**1.5 * ( (Td+216.) / (Tc+216.) )
-    k_reyn = (Re_w/Rd_w)**0.2
+    Rd_w = Re * (Td/Tc)**1.5 * ( (Td+216.) / (Tc+216.) )
+    k_reyn = (Re/Rd_w)**0.2
     
     # apply corrections
     cf_comp = cf_inc * k_comp * k_reyn
