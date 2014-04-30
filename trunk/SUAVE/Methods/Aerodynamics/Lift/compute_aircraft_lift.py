b78c0d5437ff44fa4d52d19dd6b73ece8082912a
anilvar <anilvar@stanford.edu>
2014-04-29 20:39:33 -0700

    aero method2

--- trunk/SUAVE/Methods/Aerodynamics/Lift/compute_aircraft_lift.py
+++ trunk/SUAVE/Methods/Aerodynamics/Lift/compute_aircraft_lift.py
@@ -2,13 +2,42 @@
 # 
 # Created:  Anil V., Dec 2013
 # Modified: Anil, Trent, Tarik, Feb 2014 
+# Modified: Anil  April 2014 
 
 # ----------------------------------------------------------------------
 #  Imports
 # ----------------------------------------------------------------------
-
+import SUAVE
 # suave imports
+
+from SUAVE.Structure import Data
+from SUAVE.Attributes import Units
+
 from SUAVE.Attributes.Results import Result
+from SUAVE.Attributes.Missions.Segments import Segment
+#from SUAVE import Vehicle
+from SUAVE.Components.Wings import Wing
+from SUAVE.Components.Fuselages import Fuselage
+from SUAVE.Components.Propulsors import Turbofan
+from SUAVE.Geometry.Two_Dimensional.Planform import wing_planform
+from SUAVE.Geometry.Two_Dimensional.Planform import fuselage_planform
+
+#from SUAVE.Attributes.Aerodynamics.Aerodynamics_Surrogate import Aerodynamics_Surrogate
+#from SUAVE.Attributes.Aerodynamics.Aerodynamics_Surrogate import Interpolation
+from SUAVE.Attributes.Aerodynamics.Aerodynamics_1d_Surrogate import Aerodynamics_1d_Surrogate
+from SUAVE.Methods.Aerodynamics.Drag import compute_aircraft_drag
+
+
+
+from SUAVE.Attributes.Aerodynamics.Configuration   import Configuration
+from SUAVE.Attributes.Aerodynamics.Conditions      import Conditions
+from SUAVE.Attributes.Aerodynamics.Geometry        import Geometry
+
+
+from SUAVE.Methods.Aerodynamics.Lift.weissenger_vortex_lattice import weissinger_vortex_lattice
+#from SUAVE.Methods.Aerodynamics.Lift import compute_aircraft_lift
+#from SUAVE.Methods.Aerodynamics.Drag import compute_aircraft_drag
+
 
 # python imports
 import os, sys, shutil
@@ -19,7 +48,11 @@ from warnings import warn
 import numpy as np
 import scipy as sp
 
-
+#def main():
+    
+    #test()
+    
+    #return
 # ----------------------------------------------------------------------
 #  The Function
 # ----------------------------------------------------------------------
@@ -64,9 +97,12 @@ def compute_aircraft_lift(conditions,configuration,geometry=None):
     
     # pack for interpolate
     X_interp = np.array([AoA]).T
+    #X_interp = AoA
     
     # interpolate
     wings_lift = wings_lift_model(X_interp)  
+    
+    
     
     # compressibility correction
     compress_corr = 1./(np.sqrt(1.-Mc**2.))
@@ -77,6 +113,7 @@ def compute_aircraft_lift(conditions,configuration,geometry=None):
     # total lift, accounting one fuselage
     aircraft_lift_total = wings_lift_comp * fus_correction 
     
+    #print aircraft_lift_total
     # store to results
     lift_results = Result(
         total                = aircraft_lift_total ,
@@ -85,9 +122,15 @@ def compute_aircraft_lift(conditions,configuration,geometry=None):
         compressibility_correction_factor = compress_corr  ,
         fuselage_correction_factor        = fus_correction ,
     )
-    conditions.lift_breakdown.update( lift_results )
+    #conditions.lift_breakdown.update( lift_results )    #update
     
-    # done!
+    conditions.lift_coefficient= aircraft_lift_total
+    conditions.clean_wing_lift[0] = wings_lift_comp
+    #conditions.clean_wing_lift[1] = 0.0
+    #conditions.clean_wing_lift[2] = 0.0
+    
+
+
     return aircraft_lift_total
 
 
@@ -95,5 +138,264 @@ def compute_aircraft_lift(conditions,configuration,geometry=None):
 #   Module Tests
 # ----------------------------------------------------------------------
 # this will run from command line, put simple tests for your code here
-if __name__ == '__main__':    
+#def test():
+    
+    #Seg=Segment()
+    #aircraft=SUAVE.Vehicle()
+    #aircraft.tag="aero_test"
+    #aircraft.Sref=125.00
+    
+    #Wing1=Wing(tag = 'Wing1')
+    #Wing2=Wing(tag = 'Wing2')
+    #Wing3=Wing(tag = 'Wing3')
+
+    
+    
+    #Seg.p = 23900.0            # Pa
+    #Seg.T = 218.0            # K
+    #Seg.rho = 0.41          # kg/m^3
+    #Seg.mew = 1.8*10**-5          # Ps-s
+    #Seg.M = 0.8            # dimensionless
+   
+    #Wing1.sref=124.862
+    #Wing1.ar = 9
+    #Wing1.span =35.66
+    #Wing1.sweep=25*np.pi/180
+    #Wing1.symmetric = True
+    #Wing1.t_c = 0.1
+    #Wing1.taper= 0.16
+    
+     
+    #wing_planform(Wing1)
+    #Wing1.chord_mac= 12.5
+    #Wing1.S_exposed=0.8*Wing1.area_wetted
+    #Wing1.S_affected=0.6*Wing1.area_wetted 
+    #Wing1.Cl = 0.3
+    #Wing1.e = 0.9
+    #Wing1.twist_rc =3.0*np.pi/180
+    #Wing1.twist_tc =-1.0*np.pi/180
+ 
+   
+    #aircraft.append_component(Wing1)
+  
+    
+    #Wing2.sref=32.488
+    #Wing2.ar = 6.16
+    #Wing2.span =100
+    #Wing2.sweep=30*np.pi/180
+    #Wing2.symmetric = True
+    #Wing2.t_c = 0.08
+    #Wing2.taper= 0.4
+    
+    
+    
+    
+    #wing_planform(Wing2)
+    ##Wing2.chord_mac = 12.5
+    #Wing2.chord_mac= 8.0
+    #Wing2.S_exposed=0.8*Wing2.area_wetted
+    #Wing2.S_affected=0.6*Wing2.area_wetted     
+    #Wing2.Cl = 0.2
+    #Wing2.e = 0.9
+    #Wing2.twist_rc =3.0*np.pi/180
+    #Wing2.twist_tc =-1.0*np.pi/180   
+  
+    #aircraft.append_component(Wing2)
+    
+ 
+    
+    
+    #Wing3.sref=32.488
+    #Wing3.ar = 1.91
+    #Wing3.span =100
+    #Wing3.sweep=25*np.pi/180
+    #Wing3.symmetric = False
+    #Wing3.t_c = 0.08
+    #Wing3.taper= 0.25
+       
+    #wing_planform(Wing3)
+    #Wing3.chord_mac= 8.0
+    #Wing3.S_exposed=0.8*Wing3.area_wetted
+    #Wing3.S_affected=0.6*Wing3.area_wetted     
+    #Wing3.Cl = 0.002  
+    #Wing3.e = 0.9
+    #Wing3.twist_rc =0.0*np.pi/180
+    #Wing3.twist_tc =0.0*np.pi/180       
+        
+    #aircraft.append_component(Wing3)
+    
+
+   
+    #fus=Fuselage(tag = 'fuselage1')
+    
+    #fus.num_coach_seats = 200
+    #fus.seat_pitch = 1
+    #fus.seats_abreast = 6
+    #fus.fineness_nose = 1.6
+    #fus.fineness_tail =  2
+    #fus.fwdspace = 6
+    #fus.aftspace = 5
+    #fus.width  =4
+    #fus.height =4   
+    
+    #fuselage_planform(fus)
+    #aircraft.append_component(fus)
+
+
+    #turbofan=Turbofan()
+    #turbofan.nacelle_dia= 4.0
+    #aircraft.append_component(turbofan)
+
+
+    ##wing_aero = SUAVE.Attributes.Aerodynamics.Fidelity_Zero()
+    
+    ##self.tag = 'Fidelity_Zero'
+      
+    #geometry      = Geometry()
+    
+    #configuration = Configuration()
+    
+    #conditions =Conditions()
+    
+    ## correction factors
+    #configuration.fuselage_lift_correction           = 1.14
+    #configuration.trim_drag_correction_factor        = 1.02
+    #configuration.wing_parasite_drag_form_factor     = 1.1
+    #configuration.fuselage_parasite_drag_form_factor = 2.3
+    #configuration.aircraft_span_efficiency_factor    = 0.78
+    
+    ## vortex lattice configurations
+    #configuration.number_panels_spanwise  = 5
+    #configuration.number_panels_chordwise = 1
+    
+    #Seg.p = 23900.0            # Pa
+    #Seg.T = 218.0            # K
+    #Seg.rho = 0.41          # kg/m^3
+    #Seg.mew = 1.8*10**-5          # Ps-s
+    #Seg.M = 0.8            # dimensionless
+    
+    #conditions.mach_number=Seg.M
+    #conditions.density=Seg.rho
+    #conditions.viscosity=Seg.mew
+    #conditions.temperature = Seg.T  
+    #conditions.pressure=Seg.p
+    ##conditions.angle_of_attack    
+    
+    #conditions_table = Conditions(
+        #angle_of_attack = np.linspace(-10., 10., 5) * Units.deg ,
+    #)
+    
+    #models = Data()    
+    
+    
+    ##wing_aero.initialize(aircraft)
+
+    ##conditions_table = self.conditions_table
+    ##geometry         = self.geometry
+    ##configuration    = self.configuration
+    ##
+    #AoA = conditions_table.angle_of_attack
+    #n_conditions = len(AoA)
+    
+    #vehicle=aircraft
+    
+    #print 'nacelle dia', vehicle.Propulsors[0].nacelle_dia
+
+    ## copy geometry
+    #for k in ['Fuselages','Wings','Propulsors']:
+        #geometry[k] = deepcopy(vehicle[k])
+        
+
+    
+    ## reference area
+    #geometry.reference_area = vehicle.Sref
+    
+    ## arrays
+    #CL  = np.zeros_like(AoA)
+    
+    ## condition input, local, do not keep
+    #konditions = Conditions()
+    
+    #konditions=conditions    
+    
+    ## calculate aerodynamics for table
+    #for i in xrange(n_conditions):
+        
+        ## overriding conditions, thus the name mangling
+        #konditions.angle_of_attack = AoA[i]
+        
+        ## these functions are inherited from Aerodynamics() or overridden
+        #CL[i] = calculate_lift_vortex_lattice(konditions, configuration, geometry)    
+    
+    #conditions_table.lift_coefficient = CL
+    
+    ##conditions_table = self.conditions_table
+    #AoA_data = conditions_table.angle_of_attack
+    ##
+    #CL_data  = conditions_table.lift_coefficient
+    
+    ## pack for surrogate
+    #X_data = np.array([AoA_data]).T        
+    
+    ## assign models
+    #Interpolation = Aerodynamics_1d_Surrogate.Interpolation(X_data,CL_data)
+    
+    ##print len(CL_data)
+    ##models.lift_coefficient = Interpolation(X_data,CL_data)
+    #models.lift_coefficient = Interpolation
+    
+    ## assign to configuration
+    #configuration.surrogate_models = models    
+    
+    #CL = compute_aircraft_lift(conditions,configuration,geometry)
+            
+            ## drag computes second
+    #CD = compute_aircraft_drag(conditions,configuration,geometry)
+
+    
+    ##aircraft.Aerodynamics = wing_aero 
+
+    
+    ##[Cd,Cl]=aircraft.Aerodynamics(0,Seg)
+  
+    #print 'Aerodynamics module test script'
+    #print 'aircraft Cl' , CL
+    #print 'aircraft Cd' , CD
+  
+  
+    #return
+
+
+
+    
+#def calculate_lift_vortex_lattice(conditions,configuration,geometry):
+    #""" calculate total vehicle lift coefficient by vortex lattice
+    #"""
+    
+    ## unpack
+    ##vehicle_reference_area = geometry.Sref
+    #vehicle_reference_area = geometry.reference_area
+    #total_lift = 0.0
+    
+    #for wing in geometry.Wings.values():
+        ##print 'wing span' , wing.sweep
+        #[wing_lift,wing_induced_drag] = weissinger_vortex_lattice(conditions,configuration,wing)
+        
+
+        #total_lift += wing_lift * wing.sref / vehicle_reference_area
+    
+    #return total_lift
+    
+    
+    
+####: class Aerodynamics_Surrogate()
+
+###test()
+
+if __name__ == '__main__':   
+    #test()
     raise RuntimeError , 'module test failed, not implemented'
+
+
+#-------runn this caase  - have a local test case---------------------
+
--- trunk/SUAVE/Methods/Aerodynamics/Lift/weissenger_vortex_lattice/weissinger_vortex_lattice.py
+++ trunk/SUAVE/Methods/Aerodynamics/Lift/weissenger_vortex_lattice/weissinger_vortex_lattice.py
@@ -36,6 +36,9 @@ def weissinger_vortex_lattice(conditions,configuration,wing):
     """
 
     #unpack
+    
+    
+    
     span       = wing.span
     root_chord = wing.chord_root
     tip_chord  = wing.chord_tip
@@ -45,7 +48,8 @@ def weissinger_vortex_lattice(conditions,configuration,wing):
     twist_tc   = wing.twist_tc
     sym_para   = wing.symmetric
     AR         = wing.ar
-    Sref       = wing.Sref
+    Sref       = wing.sref
+    orientation = wing.vertical
 
     n  = configuration.number_panels_spanwise
     nn = configuration.number_panels_chordwise
@@ -57,6 +61,8 @@ def weissinger_vortex_lattice(conditions,configuration,wing):
     Tc  = conditions.temperature    
     pc  = conditions.pressure
     aoa = conditions.angle_of_attack
+    
+    
 
     # freestream velocity
     Vinf = Mc * compute_speed_of_sound(Tc, pc) 
@@ -69,122 +75,137 @@ def weissinger_vortex_lattice(conditions,configuration,wing):
 
 
 
-    section_length= np.empty(n)
-    area_section=np.empty(n)
-    sl=np.empty(n)
+    if orientation == False :
 
-    xpos=np.empty(n)
-
-    ya=np.empty(n)
-    yb=np.empty(n)
-    xa=np.empty(n)
-    yab=np.empty(n)
-    ybb=np.empty(n)
-    y2=np.empty(n)   
-
-    x=np.empty(n)
-    y=np.empty(n)
-    twist_distri=np.empty(n)
-    xloc_leading=np.empty(n)
-    xloc_trailing=np.empty(n)  
-    RHS=np.empty([n,1])
-    w=np.empty([n,n])
-    wb=np.empty([n,n])
-    A=np.empty([n,n])
-    L=np.empty(n)
-    T=np.empty(n)
-
-    A_v=np.empty([n,n])
-    v=np.empty(n)
-
-    Lfi=np.empty(n)
-    Lfk=np.empty(n)
-
-    Lft=np.empty(n)
-    Dg=np.empty(n)   
-    D=np.empty(n)    
-
-
-
-
-    #--discretizing the wing sections into panels--------------------------------
-    for i in range(0,n):
-
-        section_length[i]= dchord/span*(span-(i+1)*deltax+deltax/2) + tip_chord
-        area_section[i]=section_length[i]*deltax
-        sl[i]=section_length[i]
-        twist_distri[i]=twist_rc + i/float(n)*(twist_tc-twist_rc)
-        xpos[i]=(i)*deltax
-
-        ya[i]=(i)*deltax
-        yb[i]=(i+1)*deltax
-        xa[i]=((i+1)*deltax-deltax/2)*np.tan(sweep)+ 0.25*sl[i]
-
-        x[i]=((i+1)*deltax-deltax/2)*np.tan(sweep) + 0.75*sl[i]
-        y[i]=((i+1)*deltax-deltax/2)
-        xloc_leading[i]=((i+1)*deltax)*np.tan(sweep)
-        xloc_trailing[i]=((i+1)*deltax)*np.tan(sweep)+sl[i]
-
-
-    #------Influence coefficient computation-----------------------
-    for i in range(0,n):
-
-        RHS[i,0]=Vinf*np.sin(twist_distri[i]+aoa)  #)
-
-        for j in range(0,n):
-
-
-            yad=y[i]-ya[j]
-            xd=x[i]-xa[j]
-            ybd=y[i]-yb[j]
-
-            yadd=y[i]-yab[j]
-            ybdd=y[i]-ybb[j]
-
-
-            A[i,j]=whav(x[i],y[i],xa[j],ya[j])-whav(x[i],y[i],xa[j],yb[j])-whav(x[i],y[i],xa[j],-ya[j])+whav(x[i],y[i],xa[j],-yb[j])
-            A[i,j]=A[i,j]*0.25/np.pi
-
-    #---------------Vortex strength computation by matrix inversion-----------
-    T=np.linalg.solve(A,RHS)
+        section_length= np.empty(n)
+        area_section=np.empty(n)
+        sl=np.empty(n)
     
-    #---Calculating the effective velocty--------------------------
-    for i in range(0,n):
-        v[i]=0.0      
-        for j in range(0,n):
+        xpos=np.empty(n)
+    
+        ya=np.empty(n)
+        yb=np.empty(n)
+        xa=np.empty(n)
+        yab=np.empty(n)
+        ybb=np.empty(n)
+        y2=np.empty(n)   
+    
+        x=np.empty(n)
+        y=np.empty(n)
+        twist_distri=np.empty(n)
+        xloc_leading=np.empty(n)
+        xloc_trailing=np.empty(n)  
+        RHS=np.empty([n,1])
+        w=np.empty([n,n])
+        wb=np.empty([n,n])
+        A=np.empty([n,n])
+        L=np.empty(n)
+        T=np.empty(n)
+    
+        A_v=np.empty([n,n])
+        v=np.empty(n)
+    
+        Lfi=np.empty(n)
+        Lfk=np.empty(n)
+    
+        Lft=np.empty(n)
+        Dg=np.empty(n)   
+        D=np.empty(n)    
+    
+    
+    
+    
+        #--discretizing the wing sections into panels--------------------------------
+        for i in range(0,n):
+    
+            section_length[i]= dchord/span*(span-(i+1)*deltax+deltax/2) + tip_chord
+            area_section[i]=section_length[i]*deltax
+            sl[i]=section_length[i]
+            twist_distri[i]=twist_rc + i/float(n)*(twist_tc-twist_rc)
+            xpos[i]=(i)*deltax
+    
+            ya[i]=(i)*deltax
+            yb[i]=(i+1)*deltax
+            xa[i]=((i+1)*deltax-deltax/2)*np.tan(sweep)+ 0.25*sl[i]
+    
+            x[i]=((i+1)*deltax-deltax/2)*np.tan(sweep) + 0.75*sl[i]
+            y[i]=((i+1)*deltax-deltax/2)
+            xloc_leading[i]=((i+1)*deltax)*np.tan(sweep)
+            xloc_trailing[i]=((i+1)*deltax)*np.tan(sweep)+sl[i]
+    
+    
+        #------Influence coefficient computation-----------------------
+        for i in range(0,n):
+    
+            RHS[i,0]=np.sin(twist_distri[i]+aoa)  #)
+            #RHS[i,0]=Vinf*np.sin(twist_distri[i]+aoa)  #)
+    
+            for j in range(0,n):
+    
+    
+                yad=y[i]-ya[j]
+                xd=x[i]-xa[j]
+                ybd=y[i]-yb[j]
+    
+                yadd=y[i]-yab[j]
+                ybdd=y[i]-ybb[j]
+    
+    
+                A[i,j]=whav(x[i],y[i],xa[j],ya[j])-whav(x[i],y[i],xa[j],yb[j])-whav(x[i],y[i],xa[j],-ya[j])+whav(x[i],y[i],xa[j],-yb[j])
+                A[i,j]=A[i,j]*0.25/np.pi
+    
+        #---------------Vortex strength computation by matrix inversion-----------
+        T=np.linalg.solve(A,RHS)
+        
+        #---Calculating the effective velocty--------------------------
+        for i in range(0,n):
+            v[i]=0.0      
+            for j in range(0,n):
+    
+                A_v[i,j]=whav(xa[i],y[i],xa[j],ya[j])-whav(xa[i],y[i],xa[j],yb[j])-whav(xa[i],y[i],xa[j],-ya[j])+whav(xa[i],y[i],xa[j],-yb[j])
+                A_v[i,j]=A[i,j]*0.25/np.pi*T[j]
+                v[i]=v[i]+A_v[i,j]
+    
+    
+            #Lfi[i]=-T[i]*(Vinf*np.sin(twist_tc)-v[i])
+            #Lfk[i]=T[i]*Vinf*np.cos(twist_tc)
+            
+            Lfi[i]=-T[i]*(np.sin(twist_tc)-v[i])
+            Lfk[i]=T[i]*np.cos(twist_tc)        
+    
+            Lft[i]=(-Lfi[i]*np.sin(twist_tc)+Lfk[i]*np.cos(twist_tc))
+            Dg[i]=(Lfi[i]*np.cos(twist_tc)+Lfk[i]*np.sin(twist_tc))
+    
+        #---------Lift computation from elements---------------------------------
+        LT=0.0
+        DT=0.0
+        arsec=0.0
+    
+        for i in range(0,n):
+    
+            L[i]=deltax*roc*Lft[i]   #T(i)*v(i)*sin(alpha)
+            D[i]=deltax*roc*Dg[i]    
+    
+            LT=LT+L[i]
+            DT=DT+D[i]
+    
+            arsec=arsec+area_section[i]
+    
+    
+        # compute lift and drag coefficients
+        #Cl=2*LT/(0.5*roc*Vinf**2*Sref)
+        #Cd=2*DT/(0.5*roc*Vinf**2*Sref)       
+    
+        Cl=2*LT/(0.5*roc*Sref)
+        Cd=2*DT/(0.5*roc*Sref)     
+    
+    else:
+        
+        Cl= 0.0
+        Cd= 0.0         
 
-            A_v[i,j]=whav(xa[i],y[i],xa[j],ya[j])-whav(xa[i],y[i],xa[j],yb[j])-whav(xa[i],y[i],xa[j],-ya[j])+whav(xa[i],y[i],xa[j],-yb[j])
-            A_v[i,j]=A[i,j]*0.25/np.pi*T[j]
-            v[i]=v[i]+A_v[i,j]
 
-
-        Lfi[i]=-T[i]*(Vinf*np.sin(twist_tc)-v[i])
-        Lfk[i]=T[i]*Vinf*np.cos(twist_tc)
-
-        Lft[i]=(-Lfi[i]*np.sin(twist_tc)+Lfk[i]*np.cos(twist_tc))
-        Dg[i]=(Lfi[i]*np.cos(twist_tc)+Lfk[i]*np.sin(twist_tc))
-
-    #---------Lift computation from elements---------------------------------
-    LT=0.0
-    DT=0.0
-    arsec=0.0
-
-    for i in range(0,n):
-
-        L[i]=deltax*roc*Lft[i]   #T(i)*v(i)*sin(alpha)
-        D[i]=deltax*roc*Dg[i]    
-
-        LT=LT+L[i]
-        DT=DT+D[i]
-
-        arsec=arsec+area_section[i]
-
-
-    # compute lift and drag coefficients
-    Cl=2*LT/(0.5*roc*Vinf**2*Sref)
-    Cd=2*DT/(0.5*roc*Vinf**2*Sref)       
-
-    return Cl, Cd
+    return Cl , Cd
 
 
 
