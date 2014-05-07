7d2dbd5af3e383df9a4b78f053b23ecfb421e895
Emilio Botero <emiliobotero@dn525jan.sunet>
2014-05-07 09:10:28 -0700

    solar model

--- trunk/SUAVE/Components/Energy/Storages/Battery.py
+++ trunk/SUAVE/Components/Energy/Storages/Battery.py
@@ -44,12 +44,12 @@ class Battery(Storage):
         Ploss= additional discharge power losses from the battery [W]
         mdot= mass gain rate of the lithium-air battery (if applicable) [W]
         """
-    
+        
         x=np.divide(self.CurrentEnergy,self.TotalEnergy)
         
         if self.MaxPower<power:
             print "Warning, battery not powerful enough"          
-        C=3600*power/self.TotalEnergy        #C rate of the power output
+        C=3600.*power/self.TotalEnergy        #C rate of the power output
         Eloss_ideal=power*t                  #ideal power losses
         f=1-np.exp(-20*x)-np.exp(-20*(1-x))  #empirical value for discharge
         
@@ -59,6 +59,11 @@ class Battery(Storage):
         Ploss=(Icur**2)*R                    #calculate resistive losses
         if power!=0:
             self.CurrentEnergy=self.CurrentEnergy-Eloss_ideal-Ploss*t
+
+        if power<0:
+            self.CurrentEnergy=self.CurrentEnergy-Eloss_ideal
+            if self.CurrentEnergy>self.TotalEnergy:
+                self.CurrentEnergy=self.TotalEnergy
             
         if self.CurrentEnergy<0:
             print 'Warning, battery out of energy'
@@ -66,6 +71,9 @@ class Battery(Storage):
         if self.type=='Li-Air':
             #model taken from EADS Voltair Paper
             mdot=-(power+Ploss) *(1.92E-4)*(1./3600.)      #weight gain of battery (positive means mass loss)
+            return Ploss, mdot
+        elif self.type=='Li-Ion':
+            mdot=0.
             return Ploss, mdot
             
         else:
