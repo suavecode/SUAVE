c2a8bfc92e24211b515964529b0a2fd12ebf8bbb
tarikorra <tarik.orra@embraer.com.br>
2014-05-09 18:25:43 -0300

    Update __init__.py
    import wing_fuel_volume

--- trunk/SUAVE/Geometry/Two_Dimensional/Planform/__init__.py
+++ trunk/SUAVE/Geometry/Two_Dimensional/Planform/__init__.py
@@ -4,3 +4,4 @@ from horizontal_tail_planform import horizontal_tail_planform
 from main_wing_planform import main_wing_planform
 from vertical_tail_planform import vertical_tail_planform
 from wing_planform import wing_planform
+from wing_fuel_volume import wing_fuel_volume
