d4ee1c9e73d4bce3a43d625ea1082dce15c26062
tarikorra <tarik.orra@embraer.com.br>
2014-06-24 17:48:09 -0300

    Importing wing_fuel_volume.py
    accidentally deleted in previous commit

--- trunk/SUAVE/Geometry/Two_Dimensional/Planform/__init__.py
+++ trunk/SUAVE/Geometry/Two_Dimensional/Planform/__init__.py
@@ -6,3 +6,4 @@ from vertical_tail_planform import vertical_tail_planform
 from wing_planform import wing_planform
 from horizontal_tail_planform_raymer import horizontal_tail_planform_raymer
 from vertical_tail_planform_raymer import vertical_tail_planform_raymer
+from wing_fuel_volume import wing_fuel_volume
