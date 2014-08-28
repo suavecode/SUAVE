0da23864372b10ad23fd5e890eddc6902244795c
Andrew Wendorff <andrew.wendorff@gmail.com>
2014-08-28 10:31:19 -0700

    Deleting experimental test_weights.py

--- scripts/automatic_regression.py
+++ scripts/automatic_regression.py
@@ -33,6 +33,8 @@ modules = [
     'regression/test_mission_Embraer_E190.py',
     'regression/tut_mission_Cessna_172_ic.py',
     'tutorials/mission_Boeing_737800/tut_mission_Boeing_737800.py',
+    'regression/test_weights.py'
+    'regression/DC_10_noise.py'
 ]
 
 
