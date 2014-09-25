17fcf588121e8445bab7d246584b04e129f292f5
Tarik Orra <tarik.orra@embraer.com.br>
2014-09-25 14:25:53 -0300

    Adding drag_coefficient_increment

--- trunk/SUAVE/Methods/Aerodynamics/Drag/compute_aircraft_drag.py
+++ trunk/SUAVE/Methods/Aerodynamics/Drag/compute_aircraft_drag.py
@@ -1,7 +1,7 @@
 # compute_aircraft_drag.py
-# 
+#
 # Created:  Anil V., Dec 2013
-# Modified: Anil, Trent, Tarik, Feb 2014 
+# Modified: Anil, Trent, Tarik, Feb 2014
 
 # ----------------------------------------------------------------------
 #  Imports
@@ -30,56 +30,64 @@ import scipy as sp
 
 def compute_aircraft_drag(conditions,configuration,geometry=None):
     """ SUAVE.Methods.Aerodynamics.compute_aircraft_drag(conditions,configuration,geometry)
-        computes the lift associated with an aircraft 
-        
+        computes the lift associated with an aircraft
+
         Inputs:
             conditions - data dictionary with fields:
                 mach_number - float or 1D array of freestream mach numbers
                 angle_of_attack - floar or 1D array of angle of attacks
-                
+
             configuration - data dictionary with fields:
-                surrogate_models.lift_coefficient - a callable function or class 
+                surrogate_models.lift_coefficient - a callable function or class
                     with inputs of angle of attack and outputs of lift coefficent
                 fuselage_lift_correction - the correction to fuselage contribution to lift
-                    
+
             geometry - the aircraft geoemtry with fields:
-            
-        
+
+
         Outputs:
             CD - float or 1D array of drag coefficients of the total aircraft
-        
+
         Updates:
             conditions.drag_breakdown - stores results here
-            
+
         Assumptions:
-            
-            
-    """    
-    
+
+
+    """
+
     # unpack inputs
-    trim_correction_factor = configuration.trim_drag_correction_factor
-    drag_breakdown = conditions.aerodynamics.drag_breakdown
-    
+    trim_correction_factor     = configuration.trim_drag_correction_factor
+    drag_coefficient_increment = configuration.drag_coefficient_increment
+    drag_breakdown             = conditions.aerodynamics.drag_breakdown
+
     # various drag components
-    parasite_total        = parasite_drag_aircraft     (conditions,configuration,geometry) 
+    parasite_total        = parasite_drag_aircraft     (conditions,configuration,geometry)
     induced_total         = induced_drag_aircraft      (conditions,configuration,geometry)
-    compressibility_total = compressibility_drag_wing  (conditions,configuration,geometry)    
+    compressibility_total = compressibility_drag_wing  (conditions,configuration,geometry)
     miscellaneous_drag    = miscellaneous_drag_aircraft(conditions,configuration,geometry)
-    
+
     # untrimmed drag
     aircraft_untrimmed = parasite_total        \
                        + induced_total         \
                        + compressibility_total \
-                       + miscellaneous_drag 
-    
+                       + miscellaneous_drag
+
+    # start additional corrections
+    aircraft_total_drag = aircraft_untrimmed
+
     # trim correction
-    aircraft_total_drag = aircraft_untrimmed * trim_correction_factor
+    aircraft_total_drag *= trim_correction_factor
     drag_breakdown.miscellaneous.trim_correction_factor = trim_correction_factor
+
+    # add drag_coefficient_increment
+    aircraft_total_drag += drag_coefficient_increment
+    drag_breakdown.drag_coefficient_increment = drag_coefficient_increment
 
     # store to results
     drag_breakdown.total     = aircraft_total_drag
     drag_breakdown.untrimmed = aircraft_untrimmed
-    
+
     # done!
     return aircraft_total_drag
 
@@ -88,7 +96,7 @@ def compute_aircraft_drag(conditions,configuration,geometry=None):
 #   Module Tests
 # ----------------------------------------------------------------------
 # this will run from command line, put simple tests for your code here
-if __name__ == '__main__':    
+if __name__ == '__main__':
     raise RuntimeError , 'module test failed, not implemented'
 
 
