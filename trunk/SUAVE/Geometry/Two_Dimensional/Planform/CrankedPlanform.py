Fixed leading edge calculation bug Added coordinate output for CrankedPlanform

trunk/SUAVE/Geometry/Two_Dimensional/Planform/CrankedPlanform.py
@@ -45,6 +45,9 @@ class CrankedPlanform:
         # instance parameters
         self.__semi_span = None
         self.__wing_origin = np.array([0, 0, 0])
+        self.__x_le_node = None
+        self.c_node = None
+        self.y_node = None
 
     def update(self):
         """
@@ -63,7 +66,7 @@ class CrankedPlanform:
         self.chord_tip = self.taper * chord_root_trap
 
         # span of control points: [root, break, tip]
-        y_node = np.array([0, self.span_ratio_break, 1])*self.__semi_span
+        self.y_node = np.array([0, self.span_ratio_break, 1])*self.__semi_span
 
         # break chord
         self.chord_break = chord_root_trap + self.span_ratio_break*(self.chord_tip-chord_root_trap)
@@ -75,10 +78,10 @@ class CrankedPlanform:
         self.chord_root = chord_root_trap*(1+self.lex_ratio+self.tex_ratio)
 
         # extended wing definition chords
-        c_node = np.array([self.chord_root, self.chord_break, self.chord_tip])
+        self.c_node = np.array([self.chord_root, self.chord_break, self.chord_tip])
 
         # compute the extended wing semi-planform geometry
-        cranked_semi_planform = SemiPlanform(c_node, y_node)
+        cranked_semi_planform = SemiPlanform(self.c_node, self.y_node)
         cranked_semi_planform.update()
 
         # sort the chords with y; guard against case where fuselage is wider than break point
@@ -97,14 +100,18 @@ class CrankedPlanform:
         exposed_semi_planform.sort_chord_by_y()
         exposed_semi_planform.update()
 
-        # compute the trapzoidal x quater chord location of all definition sections
-        x_le_node = y_node*np.tan(self.sweep_qc) - c_node_trap/4.
+        # compute the trapzoidal x quarter chord location of all definition sections
+        self.__x_le_node = self.y_node*np.tan(self.sweep_qc) - c_node_trap/4.
 
-        # include the effect of the lex
-        x_le_node += (self.lex_ratio+0.25)*c_node_trap[0]
+        # move root section to account for lex
+        self.__x_le_node[0] -= self.lex_ratio*c_node_trap[0]
+
+        # transform coordinate to LE
+        self.__x_le_node += (self.lex_ratio+0.25)*c_node_trap[0]
+
 
         # compute the aerodynamic center in the local coordinate system
-        x_ac_local = cranked_semi_planform.get_aerodynamic_center(x_le_node)
+        x_ac_local = cranked_semi_planform.get_aerodynamic_center(self.__x_le_node)
 
         # update
         self.area_gross = 2.0*cranked_semi_planform.area
@@ -114,6 +121,17 @@ class CrankedPlanform:
         self.mean_aerodynamic_chord_exposed = exposed_semi_planform.mean_aerodynamic_chord
         self.mean_geometric_chord = cranked_semi_planform.mean_geometric_chord
         self.aerodynamic_center = self.__wing_origin + np.array([x_ac_local, 0, 0])
+
+    def get_wing_coordinates(self):
+        """
+        # write out the planform for plotting
+        :return:
+        """
+
+        x_te_node = self.__x_le_node+self.c_node
+        x = np.append(self.__x_le_node, x_te_node[::-1], self.__x_le_node[0])
+        y = np.append(self.y_node, self.y_node[::-1], self.y_node[0])
+        return x, y
 
     def wing_origin(self, value):
         """