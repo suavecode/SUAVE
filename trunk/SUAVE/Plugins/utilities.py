42769f0b9d30f9f18ac290ab8c6545cac53b00bb
Jia Xu <jiaxu825@gmail.com>
2014-12-23 10:34:45 -0800

    Added some utility functions

---
+++ trunk/SUAVE/Plugins/utilities.py
@@ -0,0 +1,23 @@
+import code
+import sys
+
+
+def keyboard(banner=""):
+    '''
+    Function to imitate the keyboard command
+    :param banner:
+    :return:
+    '''
+
+    try:
+        raise None
+    except:
+        frame = sys.exc_info()[2].tb_frame.f_back
+    print "# Use quit() to exit :) Happy debugging!"
+    # evaluate commands in current namespace
+    namespace = frame.f_globals.copy()
+    namespace.update(frame.f_locals)
+    try:
+        code.interact(banner=banner, local=namespace)
+    except SystemExit:
+        return
