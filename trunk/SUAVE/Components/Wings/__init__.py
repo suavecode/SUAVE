ef9a96e75b9bdaabbed675b44b40911d4dc0326e
tmomose <tmomose@stanford.edu>
2014-12-29 14:52:44 -0500

    control surfaces

--- trunk/SUAVE/Components/Wings/__init__.py
+++ trunk/SUAVE/Components/Wings/__init__.py
@@ -1,6 +1,7 @@
 
 # classes
 from Wing import Wing
+from Wing import Control_Surface
 from Main_Wing import Main_Wing
 from Vertical_Tail import Vertical_Tail
 from Horizontal_Tail import Horizontal_Tail
