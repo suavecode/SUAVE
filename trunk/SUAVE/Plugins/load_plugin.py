454df54d2d268f1ff819ec7602340285eae8549a
Trent Lukaczyk <aerialhedgehog@gmail.com>
2014-12-30 01:38:04 -0500

    pluging loading function

---
+++ trunk/SUAVE/Plugins/load_plugin.py
@@ -0,0 +1,28 @@
+
+import os, sys
+__dir__ = os.path.abspath(os.path.dirname(__file__))
+
+def load_plugin(package_name):
+    """ This function loads a package that uses absolute package imports
+        by temporarily modifying the python sys.path.
+        Packages are assumed to be in the same folder as load_plugin.py
+    """
+
+    # save current path
+    saved_path = sys.path
+    
+    # remove references to package name in path
+    paths = [ p  for p in sys.path  if package_name in p ]
+    
+    for p in paths:  sys.path.remove(p)
+    
+    # add the plugin path
+    sys.path.append( __dir__ )
+    
+    package = __import__(package_name)
+    
+    sys.path = saved_path
+    
+    return package
+
+
