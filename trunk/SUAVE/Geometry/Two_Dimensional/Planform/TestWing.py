99dae3147d19d996a6b3fdaaa5574efb93145dbb
Jia Xu <jiaxu825@gmail.com>
2015-01-06 14:25:00 -0500

    Added unit test for cranked wing (from AA241)

--- trunk/SUAVE/Geometry/Two_Dimensional/Planform/TestWing.py
+++ trunk/SUAVE/Geometry/Two_Dimensional/Planform/TestWing.py
@@ -10,7 +10,61 @@ class TestWing(unittest.TestCase):
         pass
 
     def test_cranked_wing(self):
-        pass
+
+        # default wing from pass
+        taper = 0.3
+        sref = 1500
+        sweep = 45*Units.degree
+        ar = 8
+        span_ratio_break = 0.5
+        lex_ratio = 0.3
+        tex_ratio = 0.4
+        fuse_width = 11.448
+        thickness_to_chord = 0.1123  # undefined
+
+        span = np.sqrt(sref*ar)
+        span_ratio_fuselage = fuse_width/span
+
+        # compute wing planform geometry
+        wpt = CrankedPlanform(sref, ar, sweep, taper, span_ratio_fuselage,
+                              span_ratio_break, lex_ratio, tex_ratio)
+
+        # pass-computed quantities
+        span_pass = 109.54451
+        wing_x_position = 0.417
+        fuse_length = 107.82533
+        mac_pass = 21.811018
+        sref_pass = 1500
+        chord_wing_fuse_intersection_pass = 31.189398
+        x_ac_pass = 73.60473
+
+        # set the wing origin
+        origin = np.array((fuse_length*wing_x_position, 0, 0))
+        wpt.set_origin(origin)
+
+        wpt.update()
+
+        # span
+        self.assertAlmostEqual(wpt.span, span_pass, 2)
+
+        # chords
+        self.assertAlmostEqual(wpt.mean_aerodynamic_chord, mac_pass, 2)
+        # self.assertAlmostEqual(wpt.mean_geometric_chord, sref_pass/span_pass, 2)
+        self.assertAlmostEqual(wpt.chord_from_y(fuse_width/2.), chord_wing_fuse_intersection_pass, 3)
+
+        # areas
+        # self.assertAlmostEqual(wpt.area_gross, sgross_pass, 2)
+
+        # TODO: test wetted and exposed areas
+        self.assertLessEqual(wpt.area_exposed, wpt.area_gross)
+        self.assertLessEqual(wpt.area_exposed, wpt.calc_area_wetted(thickness_to_chord)/2.)
+
+        # test flapped area
+
+        # aerodynamic center - this is not very close
+
+        ac_delta = (x_ac_pass - wpt.x_aerodynamic_center)/x_ac_pass
+        self.assertLessEqual(abs(ac_delta), 0.01)
 
     def test_tapered_wing(self):
 
@@ -32,7 +86,7 @@ class TestWing(unittest.TestCase):
         wpt = CrankedPlanform(sref, ar, sweep, taper, span_ratio_fuselage,
                               span_ratio_break, lex_ratio, tex_ratio)
 
-        # pass quantities
+        # pass-computed quantities
         span_pass = 93.31179
         wing_x_position = 0.417
         fuse_length = 107.82533
@@ -62,7 +116,7 @@ class TestWing(unittest.TestCase):
         self.assertLessEqual(wpt.area_exposed, wpt.area_gross)
         self.assertLessEqual(wpt.area_exposed, wpt.calc_area_wetted(thickness_to_chord)/2.)
 
-        # TODO: test flapped area
+        # test flapped area
 
         # aerodynamic center
         self.assertAlmostEqual(wpt.x_aerodynamic_center, x_ac_pass , 2)
