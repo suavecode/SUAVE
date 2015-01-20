9faa8462aa837bc84e541fae19f5804b7455a4d1
tmomose <tmomose@stanford.edu>
2015-01-19 22:58:40 -0800

    moving Control_Surface out of Wing.py

--- trunk/SUAVE/Components/Wings/__init__.py
+++ trunk/SUAVE/Components/Wings/__init__.py
@@ -1,7 +1,7 @@
 
 # classes
 from Wing import Wing
-from Wing import Control_Surface
+from Control_Surface import Control_Surface
 from Main_Wing import Main_Wing
 from Vertical_Tail import Vertical_Tail
 from Horizontal_Tail import Horizontal_Tail
