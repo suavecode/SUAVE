b4c73d9976c7d8c5533d083785a4a8f559c5a906
tmomose <tmomose@stanford.edu>
2015-01-19 23:03:43 -0800

    using os for greater flexibility in specification of filenames

--- trunk/SUAVE/Methods/Aerodynamics/AVL/purge_files.py
+++ trunk/SUAVE/Methods/Aerodynamics/AVL/purge_files.py
@@ -4,13 +4,8 @@ import os
 
 def purge_files(filenames_array,directory=''):
 	
-	if directory == '':
-		pass
-	elif not directory[-1] == '/':
-		directory = directory + '/'
-	
 	for f in filenames_array:
 		try:
-			os.remove(directory+f)
+			os.remove(os.path.abspath(os.path.join(directory,f)))
 		except OSError:
 			print 'File {} was not found. Skipping purge.'.format(f)
