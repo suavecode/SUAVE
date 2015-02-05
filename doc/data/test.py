0e4c84236f75185bd3ae879f49c8508b3c712a75
Trent Lukaczyk <aerialhedgehog@gmail.com>
2015-02-04 20:27:04 -0800

    b737 d3.js

---
+++ doc/data/test.py
@@ -0,0 +1,29 @@
+
+
+
+import SUAVE
+import json, yaml
+
+data = SUAVE.Core.Data()
+
+data.x = 'x'
+data.y = 'y'
+data.sub = SUAVE.Core.Data()
+data.sub.z = 'z'
+data.sub.a = 1
+
+print json.dumps(data, indent=2)
+print yaml.dump(data.to_dict())
+
+
+
+from SUAVE.Core import Ordered_Bunch
+from SUAVE.Core import Data
+
+
+vehicle = SUAVE.Vehicle()
+
+SUAVE.Input_Output.D3JS.save_tree(vehicle,'tree.json')
+
+
+
