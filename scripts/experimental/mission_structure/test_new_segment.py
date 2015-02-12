cd7e87e9213904b719f401ce30a370c111d61164
Trent Lukaczyk <aerialhedgehog@gmail.com>
2015-02-12 11:58:33 -0800

    rename test folder

--- scripts/experimental/mission_structure/test_new_segment.py
+++ scripts/experimental/mission_structure/test_new_segment.py
@@ -40,39 +40,39 @@ def main():
     segment.air_speed = 230.412 * Units['m/s']
     segment.distance  = 3933.65 * Units.km
     
-    state1 = deepcopy( segment.state )    
-    state2 = deepcopy( segment.state )    
+    #state1 = deepcopy( segment.state )    
+    #state2 = deepcopy( segment.state )    
     
-    tic = time()
-    # once!
-    segment.evaluate( state1 )
-    print state1.conditions.weights.total_mass[-1,0]
+    #tic = time()
+    ## once!
+    #segment.evaluate( state1 )
+    #print state1.conditions.weights.total_mass[-1,0]
     
-    # again!
-    segment.evaluate( state2 )
-    print state2.conditions.weights.total_mass[-1,0]
+    ## again!
+    #segment.evaluate( state2 )
+    #print state2.conditions.weights.total_mass[-1,0]
     
-    print 't' , time()-tic
+    #print 't' , time()-tic
     
-    # Ok now do a climb segment
-    csegment = SUAVE.Analyses.Missions.Segments.Climb.Constant_Speed_Constant_Rate()
-    csegment.altitude_start = 0.0
-    csegment.altitude_end   = 10* Units.km
-    csegment.climb_rate     = 3.  * Units.m / Units.s
-    csegment.air_speed      = 230.412 * Units['m/s']
-    csegment.analyses.extend(analyses)
+    ## Ok now do a climb segment
+    #csegment = SUAVE.Analyses.Missions.Segments.Climb.Constant_Speed_Constant_Rate()
+    #csegment.altitude_start = 0.0
+    #csegment.altitude_end   = 10* Units.km
+    #csegment.climb_rate     = 3.  * Units.m / Units.s
+    #csegment.air_speed      = 230.412 * Units['m/s']
+    #csegment.analyses.extend(analyses)
     
-    state3 = deepcopy(csegment.state)
-    csegment.evaluate(state3)
-    print state3.conditions.weights.total_mass
-    print state3.conditions.freestream.altitude
+    #state3 = deepcopy(csegment.state)
+    #csegment.evaluate(state3)
+    #print state3.conditions.weights.total_mass
+    #print state3.conditions.freestream.altitude
     
     # segment container!
     
     segment_1 = deepcopy(segment)
     segment_2 = deepcopy(segment)
     
-    mission = SUAVE.Analyses.Missions.Segments.Cruise.Constant_Speed_Constant_Altitude.Container()
+    mission = SUAVE.Analyses.Missions.Mission()
     
     mission.segments.segment_1 = segment_1
     mission.segments.segment_2 = segment_2
@@ -84,6 +84,7 @@ def main():
     mission.evaluate( state )    
     
     print 't', time()-tic
+    print state.merged().conditions.weights.total_mass
     
     return
 
