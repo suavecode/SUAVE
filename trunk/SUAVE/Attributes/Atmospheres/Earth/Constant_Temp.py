5ed12d6f967bb04bcdd647ff0e83233cdcd43bf2
Trent Lukaczyk <aerialhedgehog@gmail.com>
2015-03-10 11:37:38 -0700

    fix atmosphere in cn_beta

--- trunk/SUAVE/Attributes/Atmospheres/Earth/Constant_Temp.py
+++ trunk/SUAVE/Attributes/Atmospheres/Earth/Constant_Temp.py
@@ -14,24 +14,4 @@ class Constant_Temp(Atmosphere):
     """ SUAVE.Attributes.Atmospheres.Constant_Temp
         ISA Pressure variation with constant (or input) outside temperature
     """
-    #def __setattr__(self,k,v):
-        #allow = ['alt', 'dt', 'oat']
-        #if k in allow:
-            #self.__dict__[k] = v
-            #self._calcAtmosphere()
-        #else:
-            ##protected attribute
-            #print '%s is calculated, not input' % k
-    ##end __setattr__
-    
-    #def _calcAirTemperature(self):
-        #""" Temperature directly input
-        #"""
-        #pass
-    
-    #def _calcAirPressure(self):
-        #""" Calculate pressure variation in standard atmosphere based on perfect gas law and
-        #static atmosphere assumption
-        #"""
-        #isa = IntlStandardAtmosphere(self.alt)
-        #self.__dict__['pressure'] = isa.pressure   
+    pass
