fa5d3227a108616e3793ccfa7639fa1b1fefff40
Emilio Botero <ebotero@stanford.edu>
2015-05-01 11:09:43 -0700

    Init file failing travis

--- trunk/SUAVE/Methods/Missions/Segments/Common/Energy.py
+++ trunk/SUAVE/Methods/Missions/Segments/Common/Energy.py
@@ -13,14 +13,18 @@ def initialize_battery(segment,state):
     
     if state.initials:
         energy_initial = state.initials.conditions.propulsion.battery_energy[-1,0]
+        
     else:
-        #energy_initial = segment.analyses.energy.max_battery_energy
         energy_initial = 0.0
-    
     
     state.conditions.propulsion.battery_energy[:,0] = energy_initial
 
     return
+
+# ----------------------------------------------------------------------
+#  Update Thrust
+# ----------------------------------------------------------------------
+
 def update_thrust(segment,state):
     """ update_energy()
         update energy conditions
