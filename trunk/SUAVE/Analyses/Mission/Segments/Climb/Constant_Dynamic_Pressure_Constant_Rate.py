8bec04ebafd67607c4c5541036e2295b3edac92b
Emilio Botero <ebotero@stanford.edu>
2015-06-27 16:43:45 -0700

    Constant dynamic pressure climb

---
+++ trunk/SUAVE/Analyses/Mission/Segments/Climb/Constant_Dynamic_Pressure_Constant_Rate.py
@@ -0,0 +1,41 @@
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+# SUAVE imports
+from SUAVE.Analyses.Mission.Segments import Aerodynamic
+from SUAVE.Analyses.Mission.Segments import Conditions
+
+from SUAVE.Methods.Missions import Segments as Methods
+
+from SUAVE.Analyses import Process
+from Unknown_Throttle import Unknown_Throttle
+
+# Units
+from SUAVE.Core import Units
+
+# ----------------------------------------------------------------------
+#  Segment
+# ----------------------------------------------------------------------
+
+class Constant_Dynamic_Pressure_Constant_Rate(Unknown_Throttle):
+    
+    def __defaults__(self):
+        
+        # --------------------------------------------------------------
+        #   User inputs
+        # --------------------------------------------------------------
+        self.altitude_start   = None # Optional
+        self.altitude_end     = 10. * Units.km
+        self.climb_rate       = 3.  * Units.m / Units.s
+        self.dynamic_pressure = 1600 * Units.pascals
+        
+        # --------------------------------------------------------------
+        #   The Solving Process
+        # --------------------------------------------------------------
+        initialize = self.process.initialize
+        initialize.conditions = Methods.Climb.Constant_Dynamic_Pressure_Constant_Rate.initialize_conditions
+    
+        return
+       
---
+++ trunk/SUAVE/Methods/Missions/Segments/Climb/Constant_Dynamic_Pressure_Constant_Rate.py
@@ -0,0 +1,41 @@
+import numpy as np
+import SUAVE
+
+# ----------------------------------------------------------------------
+#  Unpack Unknowns
+# ----------------------------------------------------------------------
+
+def initialize_conditions(segment,state):
+    
+    # unpack
+    climb_rate = segment.climb_rate
+    q          = segment.dynamic_pressure
+    alt0       = segment.altitude_start 
+    altf       = segment.altitude_end
+    t_nondim   = state.numerics.dimensionless.control_points
+    conditions = state.conditions
+    rho        = conditions.freestream.density[:,0]
+    
+    # Update freestream to get speed of sound
+    SUAVE.Methods.Missions.Segments.Common.Aerodynamics.update_atmosphere(segment,state)
+    rho        = conditions.freestream.density[:,0]   
+
+    # check for initial altitude
+    if alt0 is None:
+        if not state.initials: raise AttributeError('initial altitude not set')
+        alt0 = -1.0 * state.initials.conditions.frames.inertial.position_vector[-1,2]
+        segment.altitude_start = alt0
+
+    # discretize on altitude
+    alt = t_nondim * (altf-alt0) + alt0
+    
+    # process velocity vector
+    v_mag = np.sqrt(q/rho)
+    v_z   = -climb_rate # z points down
+    v_x   = np.sqrt( v_mag**2 - v_z**2 )
+    
+    # pack conditions    
+    conditions.frames.inertial.velocity_vector[:,0] = v_x
+    conditions.frames.inertial.velocity_vector[:,2] = v_z
+    conditions.frames.inertial.position_vector[:,2] = -alt[:,0] # z points down
+    conditions.freestream.altitude[:,0]             =  alt[:,0] # positive altitude in this context
