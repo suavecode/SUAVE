18eef7e9acac9e5a065f4199634e5a6f801f14d7
carlosilario <carlos.ilario@embraer.com.br>
2015-07-14 18:59:45 -0300

    Test for noise modules

---
+++ scripts/regression/test_noise.py
@@ -0,0 +1,531 @@
+# test_noise.py
+#
+# Created:  Carlos
+# Modified:
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+# SUave Imports
+
+import SUAVE
+from SUAVE.Core import Units
+
+from SUAVE.Methods.Noise.Fidelity_One.Airframe import noise_fidelity_one
+from SUAVE.Methods.Noise.Fidelity_One.Engine import noise_SAE
+
+
+import numpy as np
+
+
+from SUAVE.Core import (
+Data, Container, Data_Exception, Data_Warning,
+)
+
+from SUAVE.Methods.Propulsion.turbofan_sizing import turbofan_sizing
+
+# ----------------------------------------------------------------------
+#   Main
+# ----------------------------------------------------------------------
+
+def main():
+
+    configs, analyses = full_setup()
+
+    simple_sizing(configs)
+
+    configs.finalize()
+    analyses.finalize()
+
+      #noise
+    airframe_noise=noise_fidelity_one(configs,analyses)
+
+
+    turbofan = configs.base.propulsors[0]
+
+    turbofan = configs.base.propulsors.turbo_fan
+
+    engine_noise = noise_SAE(turbofan)
+
+
+# ----------------------------------------------------------------------
+#   Analysis Setup
+# ----------------------------------------------------------------------
+
+def full_setup():
+
+    # vehicle data
+    vehicle  = vehicle_setup()
+    configs  = configs_setup(vehicle)
+
+    analyses = base_analyses(vehicle)
+
+    return configs, analyses
+
+def vehicle_setup():
+
+    # ------------------------------------------------------------------
+    #   Initialize the Vehicle
+    # ------------------------------------------------------------------
+
+    vehicle = SUAVE.Vehicle()
+    vehicle.tag = 'Boeing_737800'
+
+
+    # ------------------------------------------------------------------
+    #   Vehicle-level Properties for Airframe Noise Calculation
+    # ------------------------------------------------------------------
+
+
+    # ------------------------------------------------------------------
+    #   Landing gear
+    # ------------------------------------------------------------------
+
+    vehicle.landing_gear = Data()
+    vehicle.landing_gear.main_tire_diameter = 1.0
+    vehicle.landing_gear.nose_tire_diameter = 1.0
+    vehicle.landing_gear.main_strut_length = 1.0
+    vehicle.landing_gear.nose_strut_length = 1.0
+    vehicle.landing_gear.number_wheels = 2
+
+
+    # ------------------------------------------------------------------
+    #   Main Wing
+    # ------------------------------------------------------------------
+
+    wing = SUAVE.Components.Wings.Main_Wing()
+    wing.tag = 'main_wing'
+
+    wing.aspect_ratio            = 10.18
+    wing.sweep                   = 25 * Units.deg
+    wing.thickness_to_chord      = 0.1
+    wing.taper                   = 0.16
+    wing.span_efficiency         = 0.9
+
+    wing.spans.projected         = 35.66
+
+    wing.chords.root             = 6.81
+    wing.chords.tip              = 1.09
+    wing.chords.mean_aerodynamic = 4.235
+
+    wing.areas.reference         = 124.862
+
+    wing.twists.root             = 3.0 * Units.degrees
+    wing.twists.tip              = 3.0 * Units.degrees
+
+    wing.origin                  = [20,0,0]
+    wing.aerodynamic_center      = [3,0,0]
+
+    wing.vertical                = False
+    wing.symmetric               = True
+    wing.high_lift               = True
+
+    wing.dynamic_pressure_ratio  = 1.0
+
+    wing.flaps.chord = 1.0
+    wing.flaps.area = 1.0
+    wing.flaps.number_slots = 2.0
+
+
+    # add to vehicle
+    vehicle.append_component(wing)
+
+
+    # ------------------------------------------------------------------
+    #  Horizontal Stabilizer
+    # ------------------------------------------------------------------
+
+    wing = SUAVE.Components.Wings.Wing()
+    wing.tag = 'horizontal_stabilizer'
+
+    wing.aspect_ratio            = 6.16      #
+    wing.sweep                   = 30 * Units.deg
+    wing.thickness_to_chord      = 0.08
+    wing.taper                   = 0.4
+    wing.span_efficiency         = 0.9
+
+    wing.spans.projected         = 14.146      #
+
+    wing.chords.root             = 3.28
+    wing.chords.tip              = 1.31
+    wing.chords.mean_aerodynamic = 8.0
+
+    wing.areas.reference         = 32.488    #
+
+    wing.twists.root             = 3.0 * Units.degrees
+    wing.twists.tip              = 3.0 * Units.degrees
+
+    wing.origin                  = [50,0,0]
+    wing.aerodynamic_center      = [2,0,0]
+
+    wing.vertical                = False
+    wing.symmetric               = True
+
+    wing.dynamic_pressure_ratio  = 0.9
+
+    # add to vehicle
+    vehicle.append_component(wing)
+
+
+    # ------------------------------------------------------------------
+    #   Vertical Stabilizer
+    # ------------------------------------------------------------------
+
+    wing = SUAVE.Components.Wings.Wing()
+    wing.tag = 'vertical_stabilizer'
+
+    wing.aspect_ratio            = 1.91      #
+    wing.sweep                   = 25 * Units.deg
+    wing.thickness_to_chord      = 0.08
+    wing.taper                   = 0.25
+    wing.span_efficiency         = 0.9
+
+    wing.spans.projected         = 7.877      #
+
+    wing.chords.root             = 6.60
+    wing.chords.tip              = 1.65
+    wing.chords.mean_aerodynamic = 8.0
+
+    wing.areas.reference         = 32.488    #
+
+    wing.twists.root             = 0.0 * Units.degrees
+    wing.twists.tip              = 0.0 * Units.degrees
+
+    wing.origin                  = [50,0,0]
+    wing.aerodynamic_center      = [2,0,0]
+
+    wing.vertical                = True
+    wing.symmetric               = False
+    wing.t_tail                  = False
+
+    wing.dynamic_pressure_ratio  = 1.0
+
+    # add to vehicle
+    vehicle.append_component(wing)
+
+
+    # ------------------------------------------------------------------
+    #   Turbofan Network
+    # ------------------------------------------------------------------
+
+    #instantiate the gas turbine network
+    turbofan = SUAVE.Components.Energy.Networks.Turbofan()
+    turbofan.tag = 'turbo_fan'
+
+    # setup
+    turbofan.number_of_engines = 2.0
+    turbofan.bypass_ratio      = 5.4
+    turbofan.engine_length     = 2.71
+    turbofan.nacelle_diameter  = 2.05
+
+    # working fluid
+    turbofan.working_fluid = SUAVE.Attributes.Gases.Air()
+
+
+    # ------------------------------------------------------------------
+    #   Component 1 - Ram
+
+    # to convert freestream static to stagnation quantities
+
+    # instantiate
+    ram = SUAVE.Components.Energy.Converters.Ram()
+    ram.tag = 'ram'
+
+    # add to the network
+    turbofan.append(ram)
+
+
+    # ------------------------------------------------------------------
+    #  Component 2 - Inlet Nozzle
+
+    # instantiate
+    inlet_nozzle = SUAVE.Components.Energy.Converters.Compression_Nozzle()
+    inlet_nozzle.tag = 'inlet_nozzle'
+
+    # setup
+    inlet_nozzle.polytropic_efficiency = 0.98
+    inlet_nozzle.pressure_ratio        = 0.98
+
+    # add to network
+    turbofan.append(inlet_nozzle)
+
+
+    # ------------------------------------------------------------------
+    #  Component 3 - Low Pressure Compressor
+
+    # instantiate
+    compressor = SUAVE.Components.Energy.Converters.Compressor()
+    compressor.tag = 'low_pressure_compressor'
+
+    # setup
+    compressor.polytropic_efficiency = 0.91
+    compressor.pressure_ratio        = 1.14
+
+    # add to network
+    turbofan.append(compressor)
+
+
+    # ------------------------------------------------------------------
+    #  Component 4 - High Pressure Compressor
+
+    # instantiate
+    compressor = SUAVE.Components.Energy.Converters.Compressor()
+    compressor.tag = 'high_pressure_compressor'
+
+    # setup
+    compressor.polytropic_efficiency = 0.91
+    compressor.pressure_ratio        = 13.415
+
+    # add to network
+    turbofan.append(compressor)
+
+
+    # ------------------------------------------------------------------
+    #  Component 5 - Low Pressure Turbine
+
+    # instantiate
+    turbine = SUAVE.Components.Energy.Converters.Turbine()
+    turbine.tag='low_pressure_turbine'
+
+    # setup
+    turbine.mechanical_efficiency = 0.99
+    turbine.polytropic_efficiency = 0.93
+
+    # add to network
+    turbofan.append(turbine)
+
+
+    # ------------------------------------------------------------------
+    #  Component 6 - High Pressure Turbine
+
+    # instantiate
+    turbine = SUAVE.Components.Energy.Converters.Turbine()
+    turbine.tag='high_pressure_turbine'
+
+    # setup
+    turbine.mechanical_efficiency = 0.99
+    turbine.polytropic_efficiency = 0.93
+
+    # add to network
+    turbofan.append(turbine)
+
+
+    # ------------------------------------------------------------------
+    #  Component 7 - Combustor
+
+    # instantiate
+    combustor = SUAVE.Components.Energy.Converters.Combustor()
+    combustor.tag = 'combustor'
+
+    # setup
+    combustor.efficiency                = 0.99
+    combustor.alphac                    = 1.0
+    combustor.turbine_inlet_temperature = 1450
+    combustor.pressure_ratio            = 0.95
+    combustor.fuel_data                 = SUAVE.Attributes.Propellants.Jet_A()
+
+    # add to network
+    turbofan.append(combustor)
+
+
+    # ------------------------------------------------------------------
+    #  Component 8 - Core Nozzle
+
+    # instantiate
+    nozzle = SUAVE.Components.Energy.Converters.Expansion_Nozzle()
+    nozzle.tag = 'core_nozzle'
+
+    # setup
+    nozzle.polytropic_efficiency = 0.95
+    nozzle.pressure_ratio        = 0.99
+
+    # for noise
+    nozzle.jet_diameter = 1.
+
+    # add to network
+    turbofan.append(nozzle)
+
+
+    # ------------------------------------------------------------------
+    #  Component 9 - Fan Nozzle
+
+    # instantiate
+    nozzle = SUAVE.Components.Energy.Converters.Expansion_Nozzle()
+    nozzle.tag = 'fan_nozzle'
+
+    # setup
+    nozzle.polytropic_efficiency = 0.95
+    nozzle.pressure_ratio        = 0.99
+
+    # for noise
+    nozzle.jet_diameter = 2.
+
+    # add to network
+    turbofan.append(nozzle)
+
+
+    # ------------------------------------------------------------------
+    #  Component 10 - Fan
+
+    # instantiate
+    fan = SUAVE.Components.Energy.Converters.Fan()
+    fan.tag = 'fan'
+
+    # setup
+    fan.polytropic_efficiency = 0.93
+    fan.pressure_ratio  = 1.7
+
+    # for noise
+    fan.rotation = 2.
+
+    # add to network
+    turbofan.append(fan)
+
+
+    # ------------------------------------------------------------------
+    #Component 10 : thrust (to compute the thrust)
+    thrust = SUAVE.Components.Energy.Processes.Thrust()
+    thrust.tag ='compute_thrust'
+
+    #total design thrust (includes all the engines)
+    thrust.total_design             = 2*24000. * Units.N #Newtons
+
+    #design sizing conditions
+    altitude      = 35000.0*Units.ft
+    mach_number   = 0.78
+    isa_deviation = 0.
+
+    # add to network
+    turbofan.thrust = thrust
+
+    #size the turbofan
+    turbofan_sizing(turbofan,mach_number,altitude)
+
+    # add  gas turbine network gt_engine to the vehicle
+    vehicle.append_component(turbofan)
+
+
+    # ------------------------------------------------------------------
+    #   Vehicle Definition Complete
+    # ------------------------------------------------------------------
+
+    return vehicle
+
+
+def base_analyses(vehicle):
+
+    analyses = SUAVE.Analyses.Vehicle()
+ # ------------------------------------------------------------------
+    #  Planet Analysis
+    planet = SUAVE.Analyses.Planets.Planet()
+    analyses.append(planet)
+
+    # ------------------------------------------------------------------
+    #  Atmosphere Analysis
+    atmosphere = SUAVE.Analyses.Atmospheric.US_Standard_1976()
+    atmosphere.features.planet = planet.features
+    analyses.append(atmosphere)
+
+    return analyses
+
+def configs_setup(vehicle):
+
+    # ------------------------------------------------------------------
+    #   Initialize Configurations
+    # ------------------------------------------------------------------
+
+    configs = SUAVE.Components.Configs.Config.Container()
+
+    base_config = SUAVE.Components.Configs.Config(vehicle)
+    base_config.tag = 'base'
+    base_config.wings['main_wing'].flaps.angle = 0. * Units.deg
+    base_config.wings['main_wing'].slats.angle = 0. * Units.deg
+    configs.append(base_config)
+
+    # ------------------------------------------------------------------
+    #   Cruise Configuration
+    # ------------------------------------------------------------------
+
+    config = SUAVE.Components.Configs.Config(base_config)
+    config.tag = 'cruise'
+
+    configs.append(config)
+
+
+    # ------------------------------------------------------------------
+    #   Takeoff Configuration
+    # ------------------------------------------------------------------
+
+    config = SUAVE.Components.Configs.Config(base_config)
+    config.tag = 'takeoff'
+
+    config.wings['main_wing'].flaps.angle = 20. * Units.deg
+    config.wings['main_wing'].slats.angle = 25. * Units.deg
+
+    config.V2_VS_ratio = 1.21
+    config.maximum_lift_coefficient = 2.
+
+    configs.append(config)
+
+
+    # ------------------------------------------------------------------
+    #   Landing Configuration
+    # ------------------------------------------------------------------
+
+    config = SUAVE.Components.Configs.Config(base_config)
+    config.tag = 'landing'
+
+    config.wings['main_wing'].flaps_angle = 30. * Units.deg
+    config.wings['main_wing'].slats_angle = 25. * Units.deg
+
+    config.Vref_VS_ratio = 1.23
+    config.maximum_lift_coefficient = 2.
+
+    configs.append(config)
+
+
+    # done!
+    return configs
+
+def simple_sizing(configs):
+
+    base = configs.base
+    base.pull_base()
+
+    # zero fuel weight
+    base.mass_properties.max_zero_fuel = 0.9 * base.mass_properties.max_takeoff
+
+    # wing areas
+    for wing in base.wings:
+        wing.areas.wetted   = 2.0 * wing.areas.reference
+        wing.areas.exposed  = 0.8 * wing.areas.wetted
+        wing.areas.affected = 0.6 * wing.areas.wetted
+
+
+
+    # diff the new data
+    base.store_diff()
+
+    # ------------------------------------------------------------------
+    #   Landing Configuration
+    # ------------------------------------------------------------------
+    landing = configs.landing
+
+    # make sure base data is current
+    landing.pull_base()
+
+    # landing weight
+    landing.mass_properties.landing = 0.85 * base.mass_properties.takeoff
+
+    # diff the new data
+    landing.store_diff()
+
+    # done!
+    return
+
+
+
+if __name__ == '__main__':
+    main()
+
