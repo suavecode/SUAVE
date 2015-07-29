e2aefc9350bb4bdc51ef0d773d5c2e59c59c035d
Emilio Botero <ebotero@stanford.edu>
2015-07-29 12:08:24 -0700

    loiter segments

---
+++ trunk/SUAVE/Analyses/Mission/Segments/Cruise/Constant_Mach_Constant_Altitude_Loiter.py
@@ -0,0 +1,44 @@
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+# SUAVE imports
+from SUAVE.Analyses.Mission.Segments import Aerodynamic
+from SUAVE.Analyses.Mission.Segments import Conditions
+
+from SUAVE.Methods.Missions import Segments as Methods
+
+from SUAVE.Analyses import Process
+
+from Constant_Speed_Constant_Altitude import Constant_Speed_Constant_Altitude
+
+# Units
+from SUAVE.Core import Units
+
+# ----------------------------------------------------------------------
+#  Segment
+# ----------------------------------------------------------------------
+
+class Constant_Mach_Constant_Altitude_Loiter(Constant_Speed_Constant_Altitude):
+    
+    def __defaults__(self):
+        
+        # --------------------------------------------------------------
+        #   User inputs
+        # --------------------------------------------------------------
+        self.altitude  = None
+        self.mach      = 0.5 
+        self.time      = 1.0 * Units.sec
+        
+        # --------------------------------------------------------------
+        #   The Solving Process
+        # --------------------------------------------------------------
+        
+        # only need to change one setup step from constant_speed_constant_altitude
+        initialize = self.process.initialize
+        initialize.conditions = Methods.Cruise.Constant_Mach_Constant_Altitude.initialize_conditions
+
+
+        return
+
