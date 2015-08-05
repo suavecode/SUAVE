9b533cbfbca9e0aa855a1931a4ced42f7550fae4
carlosilario <carlos.ilario@embraer.com.br>
2015-08-04 17:29:53 -0700

    Minor

--- trunk/SUAVE/Methods/Noise/Fidelity_One/Airframe/noise_landing_gear.py
+++ trunk/SUAVE/Methods/Noise/Fidelity_One/Airframe/noise_landing_gear.py
@@ -51,19 +51,19 @@ def noise_landing_gear (D,H,wheels,M,velocity,phi,theta,distance,frequency):
     velocity_kts=velocity/Units.knots
 
     if (wheels==1 or wheels==2):
-        G1=13+np.log10(4.5*((frequency*D/(velocity_fts*(1-M*np.math.cos(theta))))**2)* \
-            (12.5+((frequency*D/(velocity_fts*(1-M*np.math.cos(theta))))**2))**-2.25)
+        G1=13+np.log10(4.5*((frequency*D/(velocity_fts*(1-M*np.cos(theta))))**2)* \
+            (12.5+((frequency*D/(velocity_fts*(1-M*np.cos(theta))))**2))**-2.25)
         G2=(13+np.log10(2.0*(frequency*D/(velocity_fts*(1-M*np.math.cos(theta)))**2.0))* \
-            (30+(frequency*D/(velocity_fts*(1-M*np.math.cos(theta))))**8)**-1*(0.34*H/D))* \
+            (30+(frequency*D/(velocity_fts*(1-M*np.cos(theta))))**8)**-1*(0.34*H/D))* \
             (np.math.sin(phi))**2
     elif wheels==4:
         G1=12+np.log10(frequency*D/(velocity_fts*(1-M*np.cos(theta))))**2 \
         *(0.4+(frequency*D/(velocity_fts*(1-M*np.cos(theta))))**2)**(-1.6)
-        G2=(12+np.log10(7.0*(frequency*D/(velocity_fts*(1-M*np.math.cos(theta))))**3.0 * \
-            (1.06+(frequency*D/(velocity_fts*(1-M*cos(theta))))**2)**(-3.0)*(1)))*(np.math.sin(phi))**2
+        G2=(12+np.log10(7.0*(frequency*D/(velocity_fts*(1-M*np.cos(theta))))**3.0 * \
+            (1.06+(frequency*D/(velocity_fts*(1-M*np.cos(theta))))**2)**(-3.0)*(1)))*(np.sin(phi))**2
 
 
-    G3=12.79+np.log10(0.34*H/D)*(np.math.sin(phi))**2
+    G3=12.79+np.log10(0.34*H/D)*(np.sin(phi))**2
 
     SPL=60.*np.log10(velocity_kts/194.0)+20.*np.log10(D/distance)+10.*np.log10(10.0**G1+10.0**G2)
     OASPL=60.*np.log10(velocity_kts/194.0)+20.*np.log10(D/distance)+10.*np.log10(10.0**12.52+10.0**G3)
