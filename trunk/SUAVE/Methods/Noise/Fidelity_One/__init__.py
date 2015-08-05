2f61aa6c278eaf6e97098f32a50a7539d7f5f27d
carlosilario <carlos.ilario@embraer.com.br>
2015-08-04 17:27:32 -0700

    Minor

--- trunk/SUAVE/Methods/Noise/Fidelity_One/__init__.py
+++ trunk/SUAVE/Methods/Noise/Fidelity_One/__init__.py
@@ -5,3 +5,4 @@ import Engine
 from flight_trajectory import flight_trajectory
 from pnl_noise	import pnl_noise
 from epnl_noise import epnl_noise
+from atmospheric_attenuation import atmospheric_attenuation
