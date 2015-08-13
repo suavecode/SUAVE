e5d18e636e94e5fae3d8b8f3269f3ca9378b230d
carlosilario <carlos.ilario@embraer.com.br>
2015-08-13 09:45:54 -0700

    New Stone method for engine noise calculation

---
+++ trunk/SUAVE/Methods/Noise/Fidelity_One/Engine/Stone_method/engine_noise_Stone.py
@@ -0,0 +1,35 @@
+# test_noise.py
+#
+# Created:  Carlos, August 2015
+# Modified:
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+# SUave Imports
+
+def engine_noise_Stone():
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return()
+
+
+    pass
+
+if __name__ == '__main__':
+    main()
