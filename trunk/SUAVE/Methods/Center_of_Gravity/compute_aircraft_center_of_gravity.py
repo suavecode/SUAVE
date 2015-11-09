f3ea384b84837285f44f272fa2d4c7b43acb475f
jmvegh <jmvegh@stanford.edu>
2015-11-09 12:06:09 -0800

    cleaned up cg code

--- trunk/SUAVE/Methods/Center_of_Gravity/compute_aircraft_center_of_gravity.py
+++ trunk/SUAVE/Methods/Center_of_Gravity/compute_aircraft_center_of_gravity.py
@@ -6,66 +6,96 @@ import copy
 
 
 
-def compute_aircraft_center_of_gravity(vehicle, nose_load_fraction=.6):
+def compute_aircraft_center_of_gravity(vehicle, nose_load_fraction=.06):
     
     #unpack components
-    wing= vehicle.wings['main_wing']
-    h_tail=vehicle.wings['horizontal_stabilizer']
-    v_tail=vehicle.wings['vertical_stabilizer']
-    control_systems=vehicle.control_systems
-    fuselage=vehicle.fuselages['fuselage']
-    landing_gear=vehicle.landing_gear
-    turbo_fan=vehicle.propulsors['turbo_fan']
+    wing               = vehicle.wings['main_wing']
+    h_tail             =vehicle.wings['horizontal_stabilizer']
+    v_tail             =vehicle.wings['vertical_stabilizer']
+    control_systems    =vehicle.control_systems
+    fuselage           =vehicle.fuselages['fuselage']
+    landing_gear       =vehicle.landing_gear
+    turbo_fan          =vehicle.propulsors['turbo_fan']
     electrical_systems=vehicle.electrical_systems
-    avionics=vehicle.avionics
-    furnishings=vehicle.furnishings
-    passenger_weights=vehicle.passenger_weights
-    air_conditioner=vehicle.air_conditioner
-    fuel=vehicle.fuel
-    apu=vehicle.apu
-    hydraulics=vehicle.hydraulics
-    optionals=vehicle.optionals
+    avionics           =vehicle.avionics
+    furnishings        =vehicle.furnishings
+    passenger_weights  =vehicle.passenger_weights
+    air_conditioner    =vehicle.air_conditioner
+    fuel               =vehicle.fuel
+    apu                =vehicle.apu
+    hydraulics         =vehicle.hydraulics
+    optionals          =vehicle.optionals
     
     #compute moments from each component about the nose of the aircraft
-    wing_moment              =(wing.origin+wing.mass_properties.center_of_gravity)*wing.mass_properties.mass
-    h_tail_moment            =(h_tail.origin+h_tail.mass_properties.center_of_gravity)*h_tail.mass_properties.mass
-    v_tail_moment            =(v_tail.origin+v_tail.mass_properties.center_of_gravity)*(v_tail.mass_properties.mass+v_tail.rudder.mass_properties.mass)
-    control_systems_moment   =(control_systems.origin+control_systems.mass_properties.center_of_gravity)*control_systems.mass_properties.mass
-    fuselage_moment          =(fuselage.origin+fuselage.mass_properties.center_of_gravity)*fuselage.mass_properties.mass
-    turbo_fan_moment         =(turbo_fan.origin+turbo_fan.mass_properties.center_of_gravity)*turbo_fan.mass_properties.mass
-    electrical_systems_moment=(electrical_systems.origin+electrical_systems.mass_properties.center_of_gravity)*electrical_systems.mass_properties.mass
-    avionics_moment          =(avionics.origin+avionics.mass_properties.center_of_gravity)*avionics.mass_properties.mass
-    furnishings_moment       =(furnishings.origin+furnishings.mass_properties.center_of_gravity)*furnishings.mass_properties.mass
-    passengers_moment        =(passenger_weights.origin+passenger_weights.mass_properties.center_of_gravity)*passenger_weights.mass_properties.mass
-    ac_moment                =(air_conditioner.origin+air_conditioner.mass_properties.center_of_gravity)*air_conditioner.mass_properties.mass
-    fuel_moment              =(fuel.origin+fuel.mass_properties.center_of_gravity)*fuel.mass_properties.mass
-    apu_moment               =(apu.origin+apu.mass_properties.center_of_gravity)*apu.mass_properties.mass
-    hydraulics_moment        =(hydraulics.origin+hydraulics.mass_properties.center_of_gravity)*hydraulics.mass_properties.mass
-    optionals_moment         =(optionals.origin+optionals.mass_properties.center_of_gravity)*optionals.mass_properties.mass
+    wing_cg                  =wing.mass_properties.center_of_gravity
+    wing_moment              =(wing.origin+wing_cg)*wing.mass_properties.mass
+    
+    h_tail_cg                =h_tail.mass_properties.center_of_gravity
+    h_tail_moment            =(h_tail.origin+h_tail_cg)*h_tail.mass_properties.mass
+    
+    v_tail_cg                 =v_tail.mass_properties.center_of_gravity
+    v_tail_moment            =(v_tail.origin+ v_tail_cg)*(v_tail.mass_properties.mass+v_tail.rudder.mass_properties.mass)
+    
+    control_systems_cg       =control_systems.mass_properties.center_of_gravity
+    control_systems_moment   =(control_systems.origin+control_systems_cg )*control_systems.mass_properties.mass
+    
+    fuselage_cg              =fuselage.mass_properties.center_of_gravity
+    fuselage_moment          =(fuselage.origin+fuselage_cg)*fuselage.mass_properties.mass
+    
+    
+    turbo_fan_cg             =turbo_fan.mass_properties.center_of_gravity
+    turbo_fan_moment         =(turbo_fan.origin+turbo_fan_cg)*turbo_fan.mass_properties.mass
+    
+    electrical_systems_cg    =electrical_systems.mass_properties.center_of_gravity
+    electrical_systems_moment=(electrical_systems.origin+electrical_systems_cg)*electrical_systems.mass_properties.mass
+    
+    avionics_cg              =avionics.mass_properties.center_of_gravity
+    avionics_moment          =(avionics.origin+avionics_cg)*avionics.mass_properties.mass
+    
+    furnishings_cg           =furnishings.mass_properties.center_of_gravity
+    furnishings_moment       =(furnishings.origin+furnishings_cg )*furnishings.mass_properties.mass
+    
+    passengers_cg            =passenger_weights.mass_properties.center_of_gravity
+    passengers_moment        =(passenger_weights.origin+passengers_cg)*passenger_weights.mass_properties.mass
+    
+    ac_cg                    =air_conditioner.mass_properties.center_of_gravity
+    ac_moment                =(air_conditioner.origin+ac_cg)*air_conditioner.mass_properties.mass
+    
+    fuel_cg                  =fuel.mass_properties.center_of_gravity
+    fuel_moment              =(fuel.origin+fuel_cg)*fuel.mass_properties.mass
+    
+    apu_cg                   =apu.mass_properties.center_of_gravity
+    apu_moment               =(apu.origin+apu_cg)*apu.mass_properties.mass
+    
+    hydraulics_cg            =hydraulics.mass_properties.center_of_gravity
+    hydraulics_moment        =(hydraulics.origin+hydraulics_cg)*hydraulics.mass_properties.mass
+    
+    optionals_cg             =optionals.mass_properties.center_of_gravity
+    optionals_moment         =(optionals.origin+optionals_cg)*optionals.mass_properties.mass
     
     #optionals_moment         =np.array([fuselage.lengths.total*.51,0,0])*optionals.mass_properties.mass 
     
     
-    center_of_gravity_guess=[0,0,0]
-    error =1
-    aft_gear_location=np.zeros(3) #guess
+    center_of_gravity_guess  =[0,0,0]
+    error                    =1
+    aft_gear_location        =np.zeros(3) #guess
     
-    landing_gear.origin=copy.copy(fuselage.origin)#front gear location
-    landing_gear.origin[0]=fuselage.origin[0]+fuselage.lengths.nose
-    count=0 #don't allow this iteration to break the code
+    landing_gear.origin      =1*(fuselage.origin)#front gear location
+    landing_gear.origin[0]   =fuselage.origin[0]+fuselage.lengths.nose
+    count                    =0 #don't allow this iteration to break the code
     
    
     while np.abs(error)>.01 and count<10:
-        landing_gear.mass_properties.center_of_gravity=aft_gear_location*2./3.
-        landing_gear_moment=(landing_gear.origin+landing_gear.mass_properties.center_of_gravity)*landing_gear.mass_properties.mass
-        vehicle.mass_properties.center_of_gravity=(wing_moment+h_tail_moment+v_tail_moment+control_systems_moment+\
+        landing_gear.mass_properties.center_of_gravity      =aft_gear_location*2./3.   #assume that nose landing gear is 1/3 of overall landing gear (fix later)
+        landing_gear_moment                                 =(landing_gear.origin+landing_gear.mass_properties.center_of_gravity)*landing_gear.mass_properties.mass
+        vehicle.mass_properties.center_of_gravity           =(wing_moment+h_tail_moment+v_tail_moment+control_systems_moment+\
         fuselage_moment+turbo_fan_moment+electrical_systems_moment+avionics_moment+furnishings_moment+passengers_moment+\
         ac_moment+fuel_moment+apu_moment+landing_gear_moment+ hydraulics_moment+optionals_moment  )/(vehicle.mass_properties.max_takeoff)
         
-        aft_gear_location[0]=(vehicle.mass_properties.center_of_gravity[0]-landing_gear.origin[0])*\
+        aft_gear_location[0]                                =(vehicle.mass_properties.center_of_gravity[0]-landing_gear.origin[0])*\
         nose_load_fraction/(1-nose_load_fraction)+vehicle.mass_properties.center_of_gravity[0]-landing_gear.origin[0]
-        error=(center_of_gravity_guess[0]-vehicle.mass_properties.center_of_gravity[0])/((center_of_gravity_guess[0]+vehicle.mass_properties.center_of_gravity[0])/2.)
-        center_of_gravity_guess=copy.copy(vehicle.mass_properties.center_of_gravity)
+        error                                               =(center_of_gravity_guess[0]-vehicle.mass_properties.center_of_gravity[0])/((center_of_gravity_guess[0]+vehicle.mass_properties.center_of_gravity[0])/2.)
+        center_of_gravity_guess                             =1*vehicle.mass_properties.center_of_gravity #copy value
         count+=1 
     vehicle.mass_properties.center_of_gravity[1]=0 #symmetric aircraft
     
--- trunk/SUAVE/Methods/Center_of_Gravity/compute_component_centers_of_gravity.py
+++ trunk/SUAVE/Methods/Center_of_Gravity/compute_component_centers_of_gravity.py
@@ -10,32 +10,32 @@ from SUAVE.Methods.Geometry.Three_Dimensional.compute_chord_length_from_span_loc
 
 def compute_component_centers_of_gravity(vehicle):
     
-    wing= vehicle.wings['main_wing']
-    h_tail=vehicle.wings['horizontal_stabilizer']
-    v_tail=vehicle.wings['vertical_stabilizer']
-    control_systems=vehicle.control_systems
-    fuselage=vehicle.fuselages['fuselage']
-    landing_gear=vehicle.landing_gear
-    turbo_fan=vehicle.propulsors['turbo_fan']
+    wing              = vehicle.wings['main_wing']
+    h_tail            =vehicle.wings['horizontal_stabilizer']
+    v_tail            =vehicle.wings['vertical_stabilizer']
+    control_systems   =vehicle.control_systems
+    fuselage          =vehicle.fuselages['fuselage']
+    landing_gear      =vehicle.landing_gear
+    turbo_fan         =vehicle.propulsors['turbo_fan']
     electrical_systems=vehicle.electrical_systems
-    avionics=vehicle.avionics
-    furnishings=vehicle.furnishings
-    passenger_weights=vehicle.passenger_weights
-    air_conditioner=vehicle.air_conditioner
-    fuel=vehicle.fuel
-    apu=vehicle.apu
-    hydraulics=vehicle.hydraulics
-    optionals=vehicle.optionals
+    avionics          =vehicle.avionics
+    furnishings       =vehicle.furnishings
+    passenger_weights =vehicle.passenger_weights
+    air_conditioner   =vehicle.air_conditioner
+    fuel              =vehicle.fuel
+    apu               =vehicle.apu
+    hydraulics        =vehicle.hydraulics
+    optionals         =vehicle.optionals
     
-    span_location_mac=compute_span_location_from_chord_length(wing, wing.chords.mean_aerodynamic)
-    mac_le_offset=.8*np.sin(wing.sweep)*span_location_mac  #assume that 80% of the chord difference is from leading edge sweep
-    chord_length_h_tail_35_percent_semi_span=compute_chord_length_from_span_location(h_tail,.35*h_tail.spans.projected*.5)
-    chord_length_v_tail_35_percent_semi_span=compute_chord_length_from_span_location(v_tail,.35*v_tail.spans.projected*.5)
+    span_location_mac                          =compute_span_location_from_chord_length(wing, wing.chords.mean_aerodynamic)
+    mac_le_offset                              =.8*np.sin(wing.sweep)*span_location_mac  #assume that 80% of the chord difference is from leading edge sweep
+    chord_length_h_tail_35_percent_semi_span   =compute_chord_length_from_span_location(h_tail,.35*h_tail.spans.projected*.5)
+    chord_length_v_tail_35_percent_semi_span   =compute_chord_length_from_span_location(v_tail,.35*v_tail.spans.projected*.5)
     
-    h_tail_35_percent_semi_span_offset=.8*np.sin(h_tail.sweep)*.35*.5*h_tail.spans.projected
-    v_tail_35_percent_semi_span_offset=.8*np.sin(v_tail.sweep)*.35*.5*v_tail.spans.projected
+    h_tail_35_percent_semi_span_offset         =.8*np.sin(h_tail.sweep)*.35*.5*h_tail.spans.projected             #x distance from leading edge of root chord to leading edge of aerodynamic center
+    v_tail_35_percent_semi_span_offset         =.8*np.sin(v_tail.sweep)*.35*.5*v_tail.spans.projected
     
-    wing.mass_properties.center_of_gravity[0]=.3*wing.chords.mean_aerodynamic+mac_le_offset
+    wing.mass_properties.center_of_gravity[0]  =.3*wing.chords.mean_aerodynamic+mac_le_offset
     h_tail.mass_properties.center_of_gravity[0]=.3*chord_length_h_tail_35_percent_semi_span+h_tail_35_percent_semi_span_offset
     #h_tail.mass_properties.center_of_gravity[1]=.35*h_tail.spans.projected
     v_tail.mass_properties.center_of_gravity[0]=.3*chord_length_v_tail_35_percent_semi_span+v_tail_35_percent_semi_span_offset
@@ -43,31 +43,39 @@ def compute_component_centers_of_gravity(vehicle):
  
     
     
-    control_systems.origin=wing.origin
-    control_systems.mass_properties.center_of_gravity[0]=.4*wing.chords.mean_aerodynamic+mac_le_offset
-    fuselage.mass_properties.center_of_gravity[0]=.45*fuselage.lengths.total
-    turbo_fan.origin=wing.origin+mac_le_offset/2.-(3./4.)*turbo_fan.engine_length
-    turbo_fan.mass_properties.center_of_gravity[0]=turbo_fan.engine_length*.5
+    control_systems.origin                                 =wing.origin
+    control_systems.mass_properties.center_of_gravity[0]   =.4*wing.chords.mean_aerodynamic+mac_le_offset
+    fuselage.mass_properties.center_of_gravity[0]          =.45*fuselage.lengths.total
+    turbo_fan.origin                                       =wing.origin+mac_le_offset/2.-(3./4.)*turbo_fan.engine_length
+    turbo_fan.mass_properties.center_of_gravity[0]         =turbo_fan.engine_length*.5
     electrical_systems.mass_properties.center_of_gravity[0]=.75*(fuselage.origin[0]+\
                 .5*fuselage.lengths.total)+.25*(turbo_fan.origin[0]+turbo_fan.mass_properties.center_of_gravity[0])
-    avionics.origin=fuselage.origin
-    avionics.mass_properties.center_of_gravity[0]=.4*fuselage.lengths.nose
-    furnishings.origin=fuselage.origin
-    furnishings.mass_properties.center_of_gravity[0]=.51*fuselage.lengths.total
-    passenger_weights.origin=fuselage.origin
+    
+    
+    avionics.origin                                       =fuselage.origin
+    avionics.mass_properties.center_of_gravity[0]         =.4*fuselage.lengths.nose
+    furnishings.origin                                    =fuselage.origin
+    furnishings.mass_properties.center_of_gravity[0]      =.51*fuselage.lengths.total
+    passenger_weights.origin                              =fuselage.origin
     passenger_weights.mass_properties.center_of_gravity[0]=.51*fuselage.lengths.total
-    air_conditioner.origin=fuselage.origin
-    air_conditioner.mass_properties.center_of_gravity[0]=fuselage.lengths.nose
-    fuel.origin=wing.origin
-    fuel.mass_properties.center_of_gravity=wing.mass_properties.center_of_gravity
+    air_conditioner.origin                                =fuselage.origin
+    air_conditioner.mass_properties.center_of_gravity[0]  =fuselage.lengths.nose
+    fuel.origin                                           =wing.origin
+    fuel.mass_properties.center_of_gravity                =wing.mass_properties.center_of_gravity #assume fuel cg is wing cg (not from notes)
     
-    apu.origin=fuselage.origin
-    apu.mass_properties.center_of_gravity[0]=.9*fuselage.lengths.total
-    hydraulics.origin=fuselage.origin
-    hydraulics.mass_properties.center_of_gravity=.75*(wing.origin+wing.mass_properties.center_of_gravity)+.25*(turbo_fan.origin+turbo_fan.mass_properties.center_of_gravity)
+    apu.origin                                            =fuselage.origin
+    apu.mass_properties.center_of_gravity[0]              =.9*fuselage.lengths.total #assumption that it's at 90% of fuselage length (not from notes)
+    hydraulics.origin                                     =fuselage.origin
+    hydraulics.mass_properties.center_of_gravity          =.75*(wing.origin+wing.mass_properties.center_of_gravity)+.25*(turbo_fan.origin+turbo_fan.mass_properties.center_of_gravity)
     
-    optionals_origin=fuselage.origin
-    optionals.mass_properties.center_of_gravity[0]=.51*fuselage.lengths.total
+    optionals_origin                                      =fuselage.origin
+    optionals.mass_properties.center_of_gravity[0]        =.51*fuselage.lengths.total
+    
+    
+  
+    
+    
+    
     
     
     
