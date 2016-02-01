33a5a0910673aafb8cd951b0172c1aeebda3af8b
jmvegh <jmvegh@stanford.edu>
2016-02-01 13:43:24 -0800

    added softmax option to initialize_from_energy_and_power.py

--- trunk/SUAVE/Methods/Power/Battery/Sizing/initialize_from_energy_and_power.py
+++ trunk/SUAVE/Methods/Power/Battery/Sizing/initialize_from_energy_and_power.py
@@ -1,5 +1,8 @@
-"""sizes battery based on total battery properties"""
-#by M. Vegh
+# initialize_from_energy_and_power
+
+# Created:  Feb 2015, M. Vegh
+# Modified: Feb 2016, M. Vegh
+
 
 """ SUAVE Methods for Energy Systems """
 
@@ -12,10 +15,15 @@ import numpy as np
 #  Methods
 # ----------------------------------------------------------------------
 
-def initialize_from_energy_and_power(battery, energy, power): #adds a battery that is optimized based on power and energy requirements and technology
-    
-    
-    mass=np.maximum(energy/battery.specific_energy, power/battery.specific_power)
+def initialize_from_energy_and_power(battery, energy, power, max='hard'): #adds a battery that is optimized based on power and energy requirements and technology
+    energy_mass=energy/battery.specific_energy
+    power_mass=power/battery.specific_power
+    mass=np.maximum(energy_mass, power_mass)
+    if max=='soft': #use softmax function (makes it differentiable)
+        
+        min_mass=np.minimum(energy_mass, power_mass)
+        mass=mass+np.log(1.+np.exp(min_mass-mass))  #write it this way to prevent overflow
+            
     battery.mass_properties.mass=mass
     battery.max_energy=battery.specific_energy*mass
     battery.max_power =battery.specific_power*mass
