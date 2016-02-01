7f9976d67c9b413b3a7211bad921b29eac7be179
jmvegh <jmvegh@stanford.edu>
2016-02-01 14:24:07 -0800

    improved scaling of softmax in initialize_from_energy_and_power.py

--- trunk/SUAVE/Methods/Power/Battery/Sizing/initialize_from_energy_and_power.py
+++ trunk/SUAVE/Methods/Power/Battery/Sizing/initialize_from_energy_and_power.py
@@ -20,10 +20,9 @@ def initialize_from_energy_and_power(battery, energy, power, max='hard'): #adds 
     power_mass=power/battery.specific_power
     mass=np.maximum(energy_mass, power_mass)
     if max=='soft': #use softmax function (makes it differentiable)
-        
-        min_mass=np.minimum(energy_mass, power_mass)
-        mass=mass+np.log(1.+np.exp(min_mass-mass))  #write it this way to prevent overflow
-            
+        scaling=10.**(np.floor(np.log10(mass))-1) #make it so the exponentials are taking ~10 (closer numerically, while still differentiable)
+        mass=scaling*np.log(np.exp(energy_mass/scaling)+np.exp(power_mass/scaling))  #write it this way to prevent overflow
+
     battery.mass_properties.mass=mass
     battery.max_energy=battery.specific_energy*mass
     battery.max_power =battery.specific_power*mass
