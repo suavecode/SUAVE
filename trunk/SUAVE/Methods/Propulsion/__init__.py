03aaccf955f6e686ebd5737986eed0d135b503cb
Emilio Botero <ebotero@stanford.edu>
2016-02-02 13:44:37 -0800

    bad init file

--- trunk/SUAVE/Methods/Propulsion/__init__.py
+++ trunk/SUAVE/Methods/Propulsion/__init__.py
@@ -1,15 +1,12 @@
 
 
 from engine_analysis import engine_analysis
-from engine_analysis_1d import engine_analysis_1d
 from engine_analysis_ductedfan import engine_analysis_ductedfan
 from engine_analysis_pass import engine_analysis_pass
 from engine_sizing import engine_sizing
 from engine_sizing_1d import engine_sizing_1d
 from engine_sizing_ductedfan import engine_sizing_ductedfan
 from engine_sizing_pass import engine_sizing_pass
-from evaluate import evaluate
 from fm_id import fm_id
-from initialize import initialize
 from propeller_design import propeller_design
 from turbofan_nox_emission_index import turbofan_nox_emission_index
