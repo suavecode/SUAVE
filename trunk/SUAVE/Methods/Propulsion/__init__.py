a12f0733c5453fd04f3fa51acd638ddf28be0349
Emilio Botero <ebotero@stanford.edu>
2016-02-02 21:09:55 -0800

    reintroducing fm_id

--- trunk/SUAVE/Methods/Propulsion/__init__.py
+++ trunk/SUAVE/Methods/Propulsion/__init__.py
@@ -3,3 +3,4 @@ from propeller_design import propeller_design
 from turbofan_nox_emission_index import turbofan_nox_emission_index
 from turbofan_sizing import turbofan_sizing
 from turbojet_sizing import turbojet_sizing
+from fm_id import fm_id
