converging with wrong gradients

trunk/SUAVE/Methods/Geometry/Three_Dimensional/angles_to_dcms.py
@@ -1,4 +1,4 @@
-
+import autograd
 import autograd.numpy as np 
 from numpy import cos, sin
 from orientation_product import orientation_product
@@ -41,19 +41,22 @@ def T0(a):
     cos = np.cos(a)
     sin = np.sin(a)
                   
-    T = new_tensor(a)
+    #T = new_tensor(a)
     
-    T[:,1,1] = cos
-    T[:,1,2] = sin
-    T[:,2,1] = -sin
-    T[:,2,2] = cos
+    #T[:,1,1] = cos
+    #T[:,1,2] = sin
+    #T[:,2,1] = -sin
+    #T[:,2,2] = cos
     
-    #Ta = np.array([[1, 0, 0],[0, cos[0], sin[0]],[0, -sin[0], cos[0]]])
-    #Tb = np.array([[1, 0, 0],[0, cos[1], sin[1]],[0, -sin[1], cos[1]]])
-    #Tc = np.array([[1, 0, 0],[0, cos[2], sin[2]],[0, -sin[2], cos[2]]])
-    
-    #T  = np.array([Ta, Tb, Tc])
 
+    
+    T = np.array([[1, 0, 0],[0, cos[0], sin[0]],[0, -sin[0], cos[0]]])
+    for ii in xrange(1,len(a)):
+        Ta = np.array([[1, 0, 0],[0, cos[ii], sin[ii]],[0, -sin[ii], cos[ii]]])
+        T  = autograd.numpy.concatenate((T,Ta))
+    
+    T = np.reshape(T,(len(a),3,3))
+    
     return T
         
 
@@ -66,18 +69,19 @@ def T1(a):
     cos = np.cos(a)
     sin = np.sin(a)     
     
-    T = new_tensor(a)
+    #T = new_tensor(a)
     
-    T[:,0,0] = cos
-    T[:,0,2] = -sin
-    T[:,2,0] = sin
-    T[:,2,2] = cos
+    #T[:,0,0] = cos
+    #T[:,0,2] = -sin
+    #T[:,2,0] = sin
+    #T[:,2,2] = cos   
     
-    #Ta = np.array([[cos[0], 0, -sin[0]],[0, 1, 0],[sin[0], 0, cos[0]]])    
-    #Tb = np.array([[cos[1], 0, -sin[1]],[0, 1, 0],[sin[1], 0, cos[1]]])    
-    #Tc = np.array([[cos[2], 0, -sin[2]],[0, 1, 0],[sin[2], 0, cos[2]]])    
+    T = np.array([[cos[0], 0, -sin[0]],[0, 1, 0],[sin[0], 0, cos[0]]]) 
+    for ii in xrange(1,len(a)):
+        Ta = np.array([[cos[ii], 0, -sin[ii]],[0, 1, 0],[sin[ii], 0, cos[ii]]]) 
+        T  = autograd.numpy.concatenate((T,Ta))
     
-    #T  = np.array([Ta, Tb, Tc])
+    T = np.reshape(T,(len(a),3,3))
     
     return T
 
@@ -90,18 +94,20 @@ def T2(a):
     cos = np.cos(a)
     sin = np.sin(a)     
     
-    T = new_tensor(a)
+    #T = new_tensor(a)
     
-    T[:,0,0] = cos
-    T[:,0,1] = sin
-    T[:,1,0] = -sin
-    T[:,1,1] = cos
+    #T[:,0,0] = cos
+    #T[:,0,1] = sin
+    #T[:,1,0] = -sin
+    #T[:,1,1] = cos
     
-    #Ta = np.array([[cos[0], sin[0], 0],[-sin[0], cos[0], 0],[0, 0, 1]])
-    #Tb = np.array([[cos[1], sin[1], 0],[-sin[1], cos[1], 0],[0, 0, 1]])
-    #Tc = np.array([[cos[2], sin[2], 0],[-sin[2], cos[2], 0],[0, 0, 1]])
+    T = np.array([[cos[0], sin[0], 0],[-sin[0], cos[0], 0],[0, 0, 1]])
     
-    #T  = np.array([Ta, Tb, Tc])
+    for ii in xrange(1,len(a)):
+        Ta = np.array([[cos[ii], sin[ii], 0],[-sin[ii], cos[ii], 0],[0, 0, 1]]) 
+        T  = autograd.numpy.concatenate((T,Ta))
+    
+    T = np.reshape(T,(len(a),3,3))
         
     return T
 

trunk/SUAVE/Methods/Missions/Segments/Climb/Common.py
@@ -21,7 +21,7 @@ def unpack_unknowns(segment,state):
     rots     = state.conditions.frames.body.inertial_rotations
     
     # apply unknowns
-    rotated = np.array([rots[:,0],np.transpose(theta[:,0]),rots[:,2]])
+    rotated = np.transpose(np.array([rots[:,0],np.transpose(theta[:,0]),rots[:,2]]))
     
     state.conditions.propulsion.throttle = throttle
     state.conditions.frames.body.inertial_rotations = rotated

trunk/SUAVE/Methods/Missions/Segments/Common/Aerodynamics.py
@@ -157,12 +157,11 @@ def update_aerodynamics(segment,state):
     CL = results.lift.total
     CD = results.drag.total
     
-    # dimensionalize
-    L = state.ones_row(3) * 0.0
-    D = state.ones_row(3) * 0.0
-
-    L[:,2] = ( -CL * q * Sref )[:,0]
-    D[:,0] = ( -CD * q * Sref )[:,0]
+    # dimensionalize    
+    zeros = state.ones_row(1) * 0.0
+    
+    L = np.transpose(np.array((zeros[:,0],zeros[:,0],( -CL * q * Sref )[:,0])))
+    D = np.transpose(np.array((( -CD * q * Sref )[:,0],zeros[:,0],zeros[:,0])))
 
     results.lift_force_vector = L
     results.drag_force_vector = D    
@@ -170,8 +169,8 @@ def update_aerodynamics(segment,state):
     # pack conditions
     conditions.aerodynamics.lift_coefficient = CL
     conditions.aerodynamics.drag_coefficient = CD
-    conditions.frames.wind.lift_force_vector[:,:] = L[:,:] # z-axis
-    conditions.frames.wind.drag_force_vector[:,:] = D[:,:] # x-axis
+    conditions.frames.wind.lift_force_vector = L # z-axis
+    conditions.frames.wind.drag_force_vector = D # x-axis
 
 
 # ----------------------------------------------------------------------

trunk/SUAVE/Methods/Missions/Segments/Common/Frames.py
@@ -69,8 +69,8 @@ def initialize_planet_position(segment,state):
         latitude_initial  = 0.0
 
 
-    state.conditions.frames.planet.longitude[:,0] = longitude_initial
-    state.conditions.frames.planet.latitude[:,0]  = latitude_initial    
+    state.conditions.frames.planet.longitude = longitude_initial
+    state.conditions.frames.planet.latitude  = latitude_initial    
 
     return
     
@@ -100,9 +100,9 @@ def update_planet_position(segment,state):
 
     # Find the velocities and integrate the positions
     lamdadot  = (V/R)*np.cos(gamma)*np.cos(psi)
-    lamda     = np.dot(I,lamdadot) / Units.deg # Latitude
+    lamda     = np.dot(I,lamdadot) * 180./np.pi # Latitude
     mudot     = (V/R)*np.cos(gamma)*np.sin(psi)/np.cos(lamda)
-    mu        = np.dot(I,mudot) / Units.deg # Longitude
+    mu        = np.dot(I,mudot) * 180./np.pi # Longitude
 
     # Reshape the size of the vectorss
     shape     = np.shape(conditions.freestream.velocity)
@@ -110,8 +110,8 @@ def update_planet_position(segment,state):
     lamda     = np.reshape(lamda,shape)
 
     # Pack'r up
-    lat = conditions.frames.planet.latitude[0,0]
-    lon = conditions.frames.planet.longitude[0,0]
+    lat = conditions.frames.planet.latitude
+    lon = conditions.frames.planet.longitude
     conditions.frames.planet.latitude  = lat + lamda
     conditions.frames.planet.longitude = lon + mu
 
@@ -134,6 +134,7 @@ def update_orientations(segment,state):
     # ------------------------------------------------------------------
 
     # body frame rotations
+    zeros = state.ones_row(1) * 0.0
     phi   = body_inertial_rotations[:,0,None]
     theta = body_inertial_rotations[:,1,None]
     psi   = body_inertial_rotations[:,2,None]
@@ -146,21 +147,20 @@ def update_orientations(segment,state):
     V_body = orientation_product(T_inertial2body,V_inertial)
 
     # project inertial velocity into body x-z plane
-    V_stability = V_body
-    V_stability[:,1] = 0
+    V_stability = np.transpose(np.array((V_body[:,0],zeros[:,0],V_body[:,2])))
     V_stability_magnitude = np.sqrt( np.sum(V_stability**2,axis=1) )[:,None]
     #V_stability_direction = V_stability / V_stability_magnitude
 
     # calculate angle of attack
-    alpha = np.arctan2(V_stability[:,2],V_stability[:,0])[:,None]
+    alpha = np.arctan(V_stability[:,2]/V_stability[:,0])[:,None]
 
     # calculate side slip
-    beta = np.arctan2(V_body[:,1],V_stability_magnitude[:,0])[:,None]
+    beta = np.arctan(V_body[:,1]/V_stability_magnitude[:,0])[:,None]
 
     # pack aerodynamics angles
-    conditions.aerodynamics.angle_of_attack[:,0] = alpha[:,0]
-    conditions.aerodynamics.side_slip_angle[:,0] = beta[:,0]
-    conditions.aerodynamics.roll_angle[:,0]      = phi[:,0]
+    conditions.aerodynamics.angle_of_attack = alpha
+    conditions.aerodynamics.side_slip_angle = beta
+    conditions.aerodynamics.roll_angle      = phi
 
     # pack transformation tensor
     conditions.frames.body.transform_to_inertial = T_body2inertial
@@ -170,12 +170,9 @@ def update_orientations(segment,state):
     #  Wind Frame
     # ------------------------------------------------------------------
 
-    # back calculate wind frame rotations
-    wind_body_rotations = body_inertial_rotations * 0.
-    wind_body_rotations[:,0] = 0          # no roll in wind frame
-    wind_body_rotations[:,1] = alpha[:,0] # theta is angle of attack
-    wind_body_rotations[:,2] = beta[:,0]  # psi is side slip angle
-
+    # back calculate wind frame rotations    
+    wind_body_rotations = np.transpose(np.array((zeros[:,0],alpha[:,0],beta[:,0])))
+    
     # wind frame tranformation matricies
     T_wind2body = angles_to_dcms(wind_body_rotations,(2,1,0))
     T_body2wind = orientation_transpose(T_wind2body)

trunk/SUAVE/Methods/Missions/Segments/Common/Weights.py
@@ -72,6 +72,6 @@ def update_weights(segment,state):
     conditions.weights.total_mass = m
     grav = np.array((grav[:,0],grav[:,1],W[:,0]))
     
-    conditions.frames.inertial.gravity_force_vector = grav
+    conditions.frames.inertial.gravity_force_vector = np.transpose(grav)
 
     return

trunk/SUAVE/Methods/Missions/Segments/Cruise/Common.py
@@ -21,7 +21,7 @@ def unpack_unknowns(segment,state):
     rots     = state.conditions.frames.body.inertial_rotations
     
     # apply unknowns
-    rotated = np.array([rots[:,0],np.transpose(theta[:,0]),rots[:,2]])
+    rotated = np.transpose(np.array([rots[:,0],np.transpose(theta[:,0]),rots[:,2]]))
     
     state.conditions.propulsion.throttle = throttle
     state.conditions.frames.body.inertial_rotations = rotated
@@ -39,7 +39,7 @@ def residual_total_forces(segment,state):
     # vertical
     res_2 = FT[:,2]
     
-    state.residuals.forces = np.stack((res_1,res_2),axis=1)
+    state.residuals.forces = np.array([res_1,res_2])
 
     return
     

trunk/SUAVE/Methods/Missions/Segments/converge_root.py
@@ -15,6 +15,7 @@ from SUAVE.Core.Arrays import array_type
 from autograd.numpy import np
 from autograd.convenience_wrappers import elementwise_grad, multigrad
 from autograd.numpy.numpy_extra import ArrayNode
+from autograd.util import quick_grad_check
 
 # ----------------------------------------------------------------------
 #  Converge Root
@@ -29,15 +30,13 @@ def converge_root(segment,state):
     except AttributeError:
         root_finder = scipy.optimize.fsolve 
         
-    #prime = make_into_jacobian(elementwise_grad(iterate))
-    
-    #print prime(unknowns,(segment,state))
+    prime = make_into_jacobian(elementwise_grad(iterate))    
     
     unknowns = root_finder( iterate,
                             unknowns,
                             args = [segment,state],
                             xtol = state.numerics.tolerance_solution,
-                            )#fprime = prime)
+                            fprime = prime)
 
     return
     
@@ -58,14 +57,14 @@ def iterate(unknowns,(segment,state)):
 
     if isinstance(unknowns,array_type):
         state.unknowns.unpack_array(unknowns)
+        segment.process.iterate(segment,state)
+        residuals = state.residuals.pack_array()
     elif isinstance(unknowns,ArrayNode):
-        state.unknowns = unpack_autograd(state.unknowns, unknowns)   
+        state.unknowns = unpack_autograd(state.unknowns, unknowns)  
+        segment.process.iterate(segment,state)
+        residuals = np.reshape(state.residuals.forces[:,:],(len(unknowns)))
     else:
         state.unknowns = unknowns
-        
-    segment.process.iterate(segment,state)
-    
-    residuals = state.residuals.pack_array()
         
     return residuals 
 