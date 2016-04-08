b2ce576172ad8e321e3b5b5cbbf988a864c8dd82
Emilio Botero <ebotero@stanford.edu>
2016-04-08 14:36:19 -0700

    changes for multiprocessing

--- trunk/SUAVE/Methods/Missions/Segments/converge_root.py
+++ trunk/SUAVE/Methods/Missions/Segments/converge_root.py
@@ -30,17 +30,21 @@ def converge_root(segment,state):
     except AttributeError:
         root_finder = scipy.optimize.fsolve 
     
-    #unknowns,infodict,ier,msg = root_finder( iterate,
-                            #unknowns,
-                            #args = [segment,state],
-                            #xtol = state.numerics.tolerance_solution,
-                            #full_output=1)    
-                            
-    unknowns = root_finder( iterate,
+    unknowns,infodict,ier,msg = root_finder( iterate,
                             unknowns,
                             args = [segment,state],
                             xtol = state.numerics.tolerance_solution,
-                            fprime = jacobian)      
+                            full_output=1,
+                            fprime = jacobian3) 
+    
+    print 'Calls'
+    print infodict[ 'nfev']
+                            
+    #unknowns = root_finder( iterate,
+                            #unknowns,
+                            #args = [segment,state],
+                            #xtol = state.numerics.tolerance_solution,
+                            #fprime = jacobian2)      
                             
     return
     
@@ -63,21 +67,38 @@ def iterate(unknowns,(segment,state)):
 
 def jacobian2(unknowns,(segment,state)):
     
-    # number of processes
-    n = 8
+    # step size
     h = 1e-8
     
     # number of unknowns
     nu = len(unknowns) 
     
-    jac = np.zeros((nu,nu))
+    jac = np.empty((nu,nu))
     baseline = iterate(unknowns, (segment,state))
     
     for ii in xrange(nu):
         unknowns2 = unknowns*1.
         unknowns2[ii] = unknowns[ii]+h
         jac[:,ii] = (iterate(unknowns2, (segment,state)) - baseline)/h
-
+        
+    #print jac
+    
+    #jac = np.array(  [[  1.99993905e+00,   0.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                        #-4.49905601e+00,   0.00000000e+00,   0.00000000e+00,   0.00000000e+00],
+                      #[  0.00000000e+00,   1.87597297e+00,  -1.89750438e-03,   8.58102478e-04,
+                         #0.00000000e+00,  -4.19321883e+00,   0.00000000e+00,   0.00000000e+00],
+                      #[  0.00000000e+00,   4.62506700e-03,   1.62836827e+00,   0.00000000e+00,
+                         #0.00000000e+00,   0.00000000e+00,  -3.62796918e+00,   0.00000000e+00],
+                      #[  0.00000000e+00,   4.23611146e-03,   2.22203367e-03,   1.51125154e+00,
+                         #0.00000000e+00,   0.00000000e+00,   0.00000000e+00,  -3.36759000e+00],
+                      #[  0.00000000e+00,   0.00000000e+00,   0.00000000e+00,   0.00000000e+00,
+                        #-1.21877796e+02,   0.00000000e+00,   0.00000000e+00,   0.00000000e+00],
+                      #[  0.00000000e+00,   8.46469561e-03,  -4.60769201e-03,   2.08366657e-03,
+                         #0.00000000e+00,  -1.13682446e+02,   0.00000000e+00,   0.00000000e+00],
+                      #[  0.00000000e+00,   1.12448717e-02,   3.27702310e-03,   0.00000000e+00,
+                         #0.00000000e+00,   0.00000000e+00,  -9.85174388e+01,   0.00000000e+00],
+                      #[  0.00000000e+00,   1.03318243e-02,   5.41948708e-03,   1.68487446e-03,
+                         #0.00000000e+00,   0.00000000e+00,   0.00000000e+00,  -9.15235235e+01]])
     
     return jac
     
@@ -134,3 +155,39 @@ def jacobian(unknowns,(segment,state)):
     jac = (y-base_jac)/h
 
     return jac
+
+
+from VyPy import parallel as para
+
+def jacobian3(unknowns,(segment,state)):
+    
+    # number of processes
+    n =4
+    h = 1e-8
+    
+    # number of unknowns
+    nu = len(unknowns) 
+    
+    #jac = np.zeros((nu,nu))
+    baseline = iterate(unknowns, (segment,state))
+    base_jac = np.tile(baseline, (nu,1))
+    
+    # Make all of the FD inputs
+    inputs = np.tile(unknowns, (nu,1)) + np.eye(nu)*h
+    
+    # indexify inputs
+    x = [ix for ix in enumerate(inputs)]    
+    
+    args = (segment,state)
+    
+    # structure to run in parallel
+    e = Evaluator(iterate,args)   
+    
+    func = para.MultiTask(e,copies=1)
+    y = func(inputs)
+    
+    jac = (y-base_jac)/h
+
+    return jac
+
+
