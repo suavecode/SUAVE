4b23df4634d905ca8015ab1fd55be8a826a07d94
Emilio Botero <ebotero@stanford.edu>
2016-06-10 15:23:24 -0700

    more consolidation

--- trunk/SUAVE/Core/Data.py
+++ trunk/SUAVE/Core/Data.py
@@ -40,9 +40,9 @@ class Data(OrderedBunch):
     
     def __getitem__(self,k):
         if not isinstance(k,int):
-            return super(Data,self).__getitem__(k)
+            return super(Data,self).__getattribute__(k)
         else:
-            return super(Data,self).__getitem__(self.keys()[k])
+            return super(Data,self).__getattribute__(self.keys()[k])
     
     
     def __new__(cls,*args,**kwarg):
@@ -92,24 +92,6 @@ class Data(OrderedBunch):
         
     def __repr__(self):
         return self.dataname()
-
-    def find_instances(self,data_type):
-        """ DataBunch.find_instances(data_type)
-            
-            searches DataBunch() for instances of given data_type
-            
-            Inputs:
-                data_type  - a class type, for example type(myData)
-                
-            Outputs:
-                data - DataBunch() of the discovered data
-        """
-        
-        output = Data()
-        for key,value in self.iteritems():
-            if isinstance(value,type):
-                output[key] = value
-        return output
     
     def get_bases(self):
         """ find all DataBunch() base classes, return in a list """
@@ -167,7 +149,201 @@ class Data(OrderedBunch):
         
         return value
         
+    def pack_array(self,output='vector'):
+        """ OrderedDict.pack_array(output='vector')
+            maps the data dict to a 1D vector or 2D column array
+            
+            Inputs - 
+                output - either 'vector' (default), or 'array'
+                         chooses whether to output a 1d vector or 
+                         2d column array
+            Outputs - 
+                array - the packed array
+                
+            Assumptions - 
+                will only pack int, float, np.array and np.matrix (max rank 2)
+                if using output = 'matrix', all data values must have 
+                same length (if 1D) or number of rows (if 2D), otherwise is skipped
+        
+        """
+        
+        # dont require dict to have numpy
+        import numpy as np
+        from Arrays import atleast_2d_col, array_type, matrix_type
+        
+        # check output type
+        if not output in ('vector','array'): raise Exception , 'output type must be "vector" or "array"'        
+        vector = output == 'vector'
+        
+        # list to pre-dump array elements
+        M = []
+        
+        # valid types for output
+        valid_types = ( int, float,
+                        array_type,
+                        matrix_type )
+        
+        # initialize array row size (for array output)
+        size = [False]
+        
+        # the packing function
+        def do_pack(D):
+            for v in D.itervalues():
+                # type checking
+                if isinstance( v, OrderedBunch ): 
+                    do_pack(v) # recursion!
+                    continue
+                elif not isinstance( v, valid_types ): continue
+                elif np.rank(v) > 2: continue
+                # make column vectors
+                v = atleast_2d_col(v)
+                # handle output type
+                if vector:
+                    # unravel into 1d vector
+                    v = v.ravel(order='F')
+                else:
+                    # check array size
+                    size[0] = size[0] or v.shape[0] # updates size once on first array
+                    if v.shape[0] != size[0]: 
+                        #warn ('array size mismatch, skipping. all values in data must have same number of rows for array packing',RuntimeWarning)
+                        continue
+                # dump to list
+                M.append(v)
+            #: for each value
+        
+        # do the packing
+        do_pack(self)
+        
+        # pack into final array
+        if M:
+            M = np.hstack(M)
+        else:
+            # empty result
+            if vector:
+                M = np.array([])
+            else:
+                M = np.array([[]])
+        
+        # done!
+        return M
     
+    def unpack_array(self,M):
+        """ OrderedDict.unpack_array(array)
+            unpacks an input 1d vector or 2d column array into the data dictionary
+                following the same order that it was unpacked
+            important that the structure of the data dictionary, and the shapes
+                of the contained values are the same as the data from which the 
+                array was packed
+        
+            Inputs:
+                 array - either a 1D vector or 2D column array
+                 
+            Outputs:
+                 a reference to self, updates self in place
+                 
+        """
+        
+        # dont require dict to have numpy
+        import numpy as np
+        from Arrays import atleast_2d_col, array_type, matrix_type
+        
+        # check input type
+        vector = np.rank(M) == 1
+        
+        # valid types for output
+        valid_types = ( int, float,
+                        array_type,
+                        matrix_type )
+        
+        # counter for unpacking
+        _index = [0]
+        
+        # the unpacking function
+        def do_unpack(D):
+            for k,v in D.iteritems():
+                
+                # type checking
+                if isinstance(v, OrderedBunch ): 
+                    do_unpack(v) # recursion!
+                    continue
+                elif not isinstance(v,valid_types): continue
+                
+                # get this value's rank
+                rank = np.rank(v)
+                
+                # get unpack index
+                index = _index[0]                
+                
+                # skip if too big
+                if rank > 2: 
+                    continue
+                
+                # scalars
+                elif rank == 0:
+                    if vector:
+                        D[k] = M[index]
+                        index += 1
+                    else:#array
+                        continue
+                        #raise RuntimeError , 'array size mismatch, all values in data must have same number of rows for array unpacking'
+                    
+                # 1d vectors
+                elif rank == 1:
+                    n = len(v)
+                    if vector:
+                        D[k][:] = M[index:(index+n)]
+                        index += n
+                    else:#array
+                        D[k][:] = M[:,index]
+                        index += 1
+                    
+                # 2d arrays
+                elif rank == 2:
+                    n,m = v.shape
+                    if vector:
+                        D[k][:,:] = np.reshape( M[index:(index+(n*m))] ,[n,m], order='F')
+                        index += n*m 
+                    else:#array
+                        D[k][:,:] = M[:,index:(index+m)]
+                        index += m
+                
+                #: switch rank
+                
+                _index[0] = index
+
+            #: for each itme
+        
+        # do the unpack
+        do_unpack(self)
+         
+        # check
+        if not M.shape[-1] == _index[0]: warn('did not unpack all values',RuntimeWarning)
+         
+        # done!
+        return self    
+    
+    
+    def update(self,other):
+        """ Dict.update(other)
+            updates the dictionary in place, recursing into additional
+            dictionaries inside of other
+            
+            Assumptions:
+              skips keys that start with '_'
+        """
+        if not isinstance(other,dict):
+            raise TypeError , 'input is not a dictionary type'
+        for k,v in other.iteritems():
+            # recurse only if self's value is a Dict()
+            if k.startswith('_'):
+                continue
+        
+            try:
+                self[k].update(v)
+            except:
+                self[k] = v
+        return 
+
 
 # ----------------------------------------------------------------------
 #   Module Tests
