603a8ca2567a5ea35504a3276f14919b444ffeba
jmvegh <jmvegh@stanford.edu>
2016-07-11 11:42:15 -0700

    added Surrogate_Optimization.py

--- trunk/SUAVE/Optimization/__init__.py
+++ trunk/SUAVE/Optimization/__init__.py
@@ -4,4 +4,5 @@ import helper_functions
 import Package_Setups
 from read_optimization_outputs import read_optimization_outputs
 from carpet_plot import carpet_plot
+from Surrogate_Optimization import Surrogate_Optimization
 
