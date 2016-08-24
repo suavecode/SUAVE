4191d2bceb2edc29f9209a40b193bc191d443a10
Carlos Roberto Ilário da Silva <carlosilario@users.noreply.github.com>
2016-08-24 10:17:38 -0700

    Revert "Merging"

---
+++ trunk/SUAVE/Components/Landing_gear/Nose_Landing_Gear.py
@@ -0,0 +1,40 @@
+# Nose_Landing_Gear.py
+# 
+# Created:  Aug 2015, C. R. I. da Silva
+# Modified: Feb 2016, T. MacDonald
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+from Landing_Gear import Landing_Gear
+
+# ----------------------------------------------------------------------
+#  Attribute
+# ----------------------------------------------------------------------
+
+class Nose_Landing_Gear(Landing_Gear):
+    """ SUAVE.Components.Landing_Gear.Nose_Landing_Gear()
+
+        Attributes:
+
+        Methods:
+
+        Assumptions:
+            if needed
+
+    """
+
+    def __defaults__(self):        
+          
+        self.tire_diameter = 0.    
+        self.strut_length  = 0.    
+        self.units         = 0. # number of nose landing gear    
+        self.wheels        = 0. # number of wheels on the nose landing gear
+
+# ----------------------------------------------------------------------
+#   Unit Tests
+# ----------------------------------------------------------------------
+# this will run from command line, put simple tests for your code here
+if __name__ == '__main__':
+    raise RuntimeError , 'test failed, not implemented'
