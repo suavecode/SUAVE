9c1f5927896d9f400f87d73804f854b1df1af30f
timdmacdo <timdmacdo@users.noreply.github.com>
2016-09-30 10:02:45 -0700

    Update print_mission_breakdown.py

--- trunk/SUAVE/Input_Output/Results/print_mission_breakdown.py
+++ trunk/SUAVE/Input_Output/Results/print_mission_breakdown.py
@@ -1,7 +1,7 @@
 # print_mission_breakdown.py
 
-# Created: SUAVE team
-# Updated: Luke Kulik, August 2016
+# Created:  SUAVE team
+# Modified: Aug 2016, L. Kulik
 
 # ----------------------------------------------------------------------
 #  Imports
