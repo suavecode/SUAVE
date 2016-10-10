8aedea435ca8784c93b70ccfdea9f52591af9cdc
carlosilario <carlos.ilario@embraer.com.br>
2016-10-10 10:23:23 -0700

    Propeller noise

--- trunk/SUAVE/Methods/Noise/Fidelity_One/Noise_Tools/__init__.py
+++ trunk/SUAVE/Methods/Noise/Fidelity_One/Noise_Tools/__init__.py
@@ -6,5 +6,6 @@ from noise_tone_correction import noise_tone_correction
 from dbA_noise import dbA_noise
 from noise_geometric import noise_geometric
 from noise_certification_limits import noise_certification_limits
+from noise_certification_limits import noise_certification_propeller
 from noise_counterplot import noise_counterplot
 from senel_noise import senel_noise
