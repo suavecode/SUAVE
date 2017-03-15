4cf75f35ac29df5488c5bf75b4c4a81c5e920daa
Tim MacDonald <timdmacdo@gmail.com>
2017-03-15 15:50:23 -0700

    working case, need to fix time

---
+++ trunk/SUAVE/Methods/Missions/Segments/Single_Point/Set_Speed_Set_Altitude.py
@@ -0,0 +1,42 @@
+# Constant_Speed_Constant_Altitude.py
+# 
+# Created:  Jul 2014, SUAVE Team
+# Modified: Jan 2016, E. Botero
+
+# ----------------------------------------------------------------------
+#  Initialize Conditions
+# ----------------------------------------------------------------------
+
+def initialize_conditions(segment,state):
+    
+    # unpack
+    alt        = segment.altitude
+    air_speed  = segment.air_speed       
+    conditions = state.conditions 
+    
+    # check for initial altitude
+    if alt is None:
+        if not state.initials: raise AttributeError('altitude not set')
+        alt = -1.0 * state.initials.conditions.frames.inertial.position_vector[-1,2]
+        segment.altitude = alt
+    
+    # pack
+    state.conditions.freestream.altitude[:,0]             = alt
+    state.conditions.frames.inertial.position_vector[:,2] = -alt # z points down
+    state.conditions.frames.inertial.velocity_vector[:,0] = air_speed
+    #state.conditions.frames.inertial.time                 = state.initials.conditions.frames.inertial.time
+    
+def update_weights(segment,state):
+    
+    # unpack
+    conditions = state.conditions
+    m0        = conditions.weights.total_mass[0,0]
+    g         = conditions.freestream.gravity
+
+    # weight
+    W = m0*g
+
+    # pack
+    conditions.frames.inertial.gravity_force_vector[:,2] = W
+
+    return
