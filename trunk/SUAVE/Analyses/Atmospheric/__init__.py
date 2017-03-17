372be1e18eb77c362ccbd0546ee052429eb9d438
Tim MacDonald <timdmacdo@gmail.com>
2017-03-17 10:35:22 -0700

    format update

--- trunk/SUAVE/Analyses/Atmospheric/__init__.py
+++ trunk/SUAVE/Analyses/Atmospheric/__init__.py
@@ -1,5 +1,5 @@
 ## @defgroup Atmospheric
-# @ingroup Analyses
 # Classes that represent the different types of atmospheres that may be used for analysis.
+## @ingroup Analyses
 from Atmospheric import Atmospheric
 from US_Standard_1976 import US_Standard_1976
