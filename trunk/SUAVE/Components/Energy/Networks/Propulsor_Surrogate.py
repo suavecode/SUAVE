271f48f5c2fcdff1cd0a450e6e2e12f71246c7eb
Emilio Botero <ebotero@stanford.edu>
2017-03-17 13:56:30 -0700

    propulsor surrogate

---
+++ trunk/SUAVE/Components/Energy/Networks/Propulsor_Surrogate.py
@@ -0,0 +1,118 @@
+# propulsor_surrogate.py
+#
+# Created:  Mar 2017, E. Botero
+# Modified:
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+# suave imports
+import SUAVE
+
+# package imports
+import numpy as np
+from SUAVE.Components.Propulsors.Propulsor import Propulsor
+
+from SUAVE.Core import Data
+import sklearn
+from sklearn import gaussian_process
+from sklearn import neighbors
+from sklearn import svm
+
+# ----------------------------------------------------------------------
+#  Network
+# ----------------------------------------------------------------------
+
+# The input format for this should be Altitude, Mach, Throttle, Thrust, SFC
+
+class Propulsor_Surrogate(Propulsor):
+    def __defaults__(self): 
+        self.nacelle_diameter  = None
+        self.engine_length     = None
+        self.number_of_engines = None
+        self.tag               = 'network'
+        self.input_file        = None
+        self.sfc_surrogate     = None
+        self.thrust_surrogate  = None
+        self.areas             = Data()
+    
+    # manage process with a driver function
+    def evaluate_thrust(self,state):
+        
+        # Unpack the surrogate
+        sfc_surrogate = self.sfc_surrogate
+        thr_surrogate = self.thrust_surrogate
+        
+        # Unpack the conditions
+        conditions = state.conditions
+        altitude   = conditions.freestream.altitude
+        mach       = conditions.freestream.mach_number
+        throttle   = conditions.propulsion.throttle
+        
+        cond = np.hstack([altitude,mach,throttle])
+        
+        # Run the surrogate for a range of altitudes
+        data_len = len(altitude)
+        sfc = np.zeros([data_len,1])  
+        thr = np.zeros([data_len,1]) 
+        for ii,_ in enumerate(altitude):
+            sfc[ii] = sfc_surrogate.predict(np.array([altitude[ii][0],mach[ii][0],throttle[ii][0]]))    
+            thr[ii] = thr_surrogate.predict(np.array([altitude[ii][0],mach[ii][0],throttle[ii][0]]))   
+        
+        F    = thr
+        mdot = thr*sfc*self.number_of_engines
+        
+        # Save the output
+        results = Data()
+        results.thrust_force_vector = self.number_of_engines * F * [1,0,0] 
+        results.vehicle_mass_rate   = mdot
+    
+        return results          
+    
+    def build_surrogate(self):
+        
+        # file name to look for
+        file_name = self.input_file
+        
+        # Load the CSV file
+        my_data = np.genfromtxt(file_name, delimiter=',')
+        
+        # Clean up to remove redundant lines
+        # Section 1
+        col1 = my_data[1:,:1]
+        col2 = my_data[1:,1:2]
+        col3 = my_data[1:,2:3]
+        for ii in xrange(0, len(col1)-1):
+            if (col1[ii] == col1[ii+1])&(col2[ii] == col2[ii+1])&(col3[ii] == col3[ii+1]):
+                np.delete(my_data,(ii), axis=0)
+
+                
+    
+        xy  = my_data[1:,:3] # Altitude, Mach, Throttle
+        thr = np.transpose(np.atleast_2d(my_data[1:,3])) # Thrust
+        sfc = np.transpose(np.atleast_2d(my_data[1:,4]))  # SFC
+        
+
+        # Pick the type of process
+        regr_sfc = gaussian_process.GaussianProcess(theta0=.3)
+        regr_thr = gaussian_process.GaussianProcess(theta0=.3)      
+        thr_surrogate = regr_thr.fit(xy, thr)
+        sfc_surrogate = regr_sfc.fit(xy, sfc)          
+        
+        ## KNN
+        #regr_sfc = neighbors.KNeighborsRegressor(n_neighbors=1,weights='distance')
+        #regr_thr = neighbors.KNeighborsRegressor(n_neighbors=1,weights='distance')
+        #sfc_surrogate = regr_sfc.fit(xy, sfc)
+        #thr_surrogate = regr_thr.fit(xy, thr)  
+    
+        ## SVR
+        #regr_thr = svm.SVR(C=500.)
+        #regr_sfc = svm.SVR(C=500.)
+        #sfc_surrogate  = regr_sfc.fit(xy, sfc)
+        #thr_surrogate  = regr_thr.fit(xy, thr)           
+        
+        
+        # Save the output
+        self.sfc_surrogate    = sfc_surrogate
+        self.thrust_surrogate = thr_surrogate     
--- trunk/SUAVE/Components/Energy/Networks/__init__.py
+++ trunk/SUAVE/Components/Energy/Networks/__init__.py
@@ -10,3 +10,4 @@ from Turbofan import Turbofan
 from Turbojet_Super import Turbojet_Super
 from Solar_Low_Fidelity import Solar_Low_Fidelity
 from Dual_Battery_Ducted_Fan import Dual_Battery_Ducted_Fan
+from Propulsor_Surrogate import Propulsor_Surrogate
