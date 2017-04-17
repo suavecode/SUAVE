66bfef7b9d4dace5c7d02df45b781ba6b3b04e0b
mclarke2 <mclarke2@stanford.edu>
2017-04-17 00:31:44 -0700

    Update __init__.py

--- trunk/SUAVE/Methods/Flight_Dynamics/Static_Stability/Approximations/Supporting_Functions/__init__.py
+++ trunk/SUAVE/Methods/Flight_Dynamics/Static_Stability/Approximations/Supporting_Functions/__init__.py
@@ -1,3 +1,5 @@
+## @defgroup methods-flight_dynamics-static_stability-approximations-supporting_functions Supporting_Functions 
+# @ingroup methods-flight_dynamics-static_stability-approximations
 from convert_sweep import convert_sweep
 from extend_to_ref_area import extend_to_ref_area
 from trapezoid_ac_x import trapezoid_ac_x
