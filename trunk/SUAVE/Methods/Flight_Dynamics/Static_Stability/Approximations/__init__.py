e95698882c17b35ea92dad4ce5a42556cedf883e
mclarke2 <mclarke2@stanford.edu>
2017-04-17 00:04:14 -0700

    Update __init__.py

--- trunk/SUAVE/Methods/Flight_Dynamics/Static_Stability/Approximations/__init__.py
+++ trunk/SUAVE/Methods/Flight_Dynamics/Static_Stability/Approximations/__init__.py
@@ -1,3 +1,5 @@
+## @defgroup methods-flight_dynamics-static_stability-approximations Approximations
+# @ingroup methods-flight_dynamics-static_stability
 from datcom import datcom
 import Tube_Wing
 import Supporting_Functions
