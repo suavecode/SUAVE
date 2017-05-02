1269f9bfbf6f741f6e956af941f372645965a3c1
Matthew Clarke <mclarke2@stanford.edu>
2017-05-01 20:44:38 -0700

    AVL

--- trunk/SUAVE/Analyses/Stability/AVL.py
+++ trunk/SUAVE/Analyses/Stability/AVL.py
@@ -112,7 +112,7 @@ class AVL(Stability):
 	self.stability_model.dutch_roll.natural_frequency = 0.0
 	
 
-    def initialize(self):
+    def finalize(self):
 
         geometry = self.geometry
         self.tag      = 'avl_analysis_of_{}'.format(geometry.tag)
@@ -140,7 +140,7 @@ class AVL(Stability):
 
         return
 
-    def evaluate(self,conditions):
+    def __call__(self,conditions):
 
         # Unpack
         surrogates = self.surrogates  
@@ -159,7 +159,8 @@ class AVL(Stability):
 	
 	
         moment_model = surrogates.moment_coefficient
-        
+        Cm_alpha_model = surrogates.Cm_alpha_moment_coefficient
+	Cn_beta_model = surrogates.Cn_beta_moment_coefficient
 
 	configuration   = self.configuration
 	stability_model = self.stability_model
@@ -175,38 +176,48 @@ class AVL(Stability):
 	#Run Analysis
 	data_len = len(AoA)
 	CM = np.zeros([data_len,1])
+	Cm_alpha = np.zeros([data_len,1])
+	Cn_beta = np.zeros([data_len,1]) 
 	for ii,_ in enumerate(AoA):
 	    CM[ii] = moment_model.predict(np.array([AoA[ii][0],mach[ii][0]]))
-	    static_stability.CM  = CM               
+	    Cm_alpha[ii] = Cm_alpha_model.predict(np.array([AoA[ii][0],mach[ii][0]]))
+	    Cn_beta[ii] = Cn_beta_model.predict(np.array([AoA[ii][0],mach[ii][0]]))
+	    
+	    static_stability.CM = CM
+	    static_stability.Cm_alpha  = Cm_alpha  
+	    static_stability.Cn_beta  = Cn_beta  
+	    
 	
 	
-	print static_stability.CM
-	if geometry.wings['vertical_stabilizer']:
-	    static_stability.cn_beta[i]  = case_results.aerodynamics.cn_beta            
+	
+	#if geometry.wings['vertical_stabilizer']:
+	    #static_stability.cn_beta[i]  = 0 
+	    #static_stability.cn_beta[i]  = case_results.aerodynamics.cn_beta            
+	    
 	    
     
-	# Dynamic Stability
-	if np.count_nonzero(configuration.mass_properties.moments_of_inertia.tensor) > 0:    
-	    # Dynamic Stability Approximation Methods - valid for non-zero I tensor            
+	## Dynamic Stability
+	#if np.count_nonzero(configuration.mass_properties.moments_of_inertia.tensor) > 0:    
+	    ## Dynamic Stability Approximation Methods - valid for non-zero I tensor            
     
-	    for i,_ in enumerate(aero):
+	    #for i,_ in enumerate(aero):
 		
-		# Dynamic Stability
-		dynamic_stability.cn_r[i]             = case_results.aerodynamics.Cn_r
-		dynamic_stability.cl_p[i]             = case_results.aerodynamics.Cl_p
-		dynamic_stability.cl_beta[i]          = case_results.aerodynamics.cl_beta
-		dynamic_stability.cy_beta[i]          = 0
-		dynamic_stability.cm_q[i]             = case_results.aerodynamics.Cm_q
-		dynamic_stability.cm_alpha_dot[i]     = static_stability.cm_alpha[i]*(2*run_conditions.freestream.velocity/mac)
-		dynamic_stability.cz_alpha[i]         = case_results.aerodynamics.cz_alpha
+		## Dynamic Stability
+		#dynamic_stability.cn_r[i]             = case_results.aerodynamics.Cn_r
+		#dynamic_stability.cl_p[i]             = case_results.aerodynamics.Cl_p
+		#dynamic_stability.cl_beta[i]          = case_results.aerodynamics.cl_beta
+		#dynamic_stability.cy_beta[i]          = 0
+		#dynamic_stability.cm_q[i]             = case_results.aerodynamics.Cm_q
+		#dynamic_stability.cm_alpha_dot[i]     = static_stability.cm_alpha[i]*(2*run_conditions.freestream.velocity/mac)
+		#dynamic_stability.cz_alpha[i]         = case_results.aerodynamics.cz_alpha
 		
-		dynamic_stability.cl_psi[i]           = aero.lift_coefficient[i]
-		dynamic_stability.cL_u[i]             = 0
-		dynamic_stability.cz_u[i]             = -2(aero.lift_coefficient[i] - velocity[i]*dynamic_stability.cL_u[i])  
-		dynamic_stability.cz_alpha_dot[i]     = static_stability.cz_alpha[i]*(2*run_conditions.freestream.velocity/mac)
-		dynamic_stability.cz_q[i]             = 2. * 1.1 * static_stability.cm_alpha[i]
-		dynamic_stability.cx_u[i]             = -2. * aero.drag_coefficient[i]
-		dynamic_stability.cx_alpha[i]         = aero.lift_coefficient[i] - conditions.lift_curve_slope[i]
+		#dynamic_stability.cl_psi[i]           = aero.lift_coefficient[i]
+		#dynamic_stability.cL_u[i]             = 0
+		#dynamic_stability.cz_u[i]             = -2(aero.lift_coefficient[i] - velocity[i]*dynamic_stability.cL_u[i])  
+		#dynamic_stability.cz_alpha_dot[i]     = static_stability.cz_alpha[i]*(2*run_conditions.freestream.velocity/mac)
+		#dynamic_stability.cz_q[i]             = 2. * 1.1 * static_stability.cm_alpha[i]
+		#dynamic_stability.cx_u[i]             = -2. * aero.drag_coefficient[i]
+		#dynamic_stability.cx_alpha[i]         = aero.lift_coefficient[i] - conditions.lift_curve_slope[i]
     
 		#stability_model.dutch_roll.damping_ratio[i]       = (1/(1 + (case_results.aerodynamics.dutch_roll_mode_1_imag /case_results.aerodynamics.dutch_roll_mode_1_real)**2))**0.5
 		#stability_model.dutch_roll.natural_frequency[i]   =  - (case_results.aerodynamics.dutch_roll_mode_1_real/stability_model.dutch_roll.damping_ratio)
@@ -224,7 +235,7 @@ class AVL(Stability):
 	results.static  = static_stability
 	results.dynamic = dynamic_stability
 	
-	return results, 0        
+	return results    
          
   
     def sample_training(self):
@@ -239,7 +250,8 @@ class AVL(Stability):
         CL   = np.zeros([len(AoA)*len(mach),1])
         CD   = np.zeros([len(AoA)*len(mach),1])
         CM   = np.zeros([len(AoA)*len(mach),1])
-
+	Cm_alpha = np.zeros([len(AoA)*len(mach),1])
+	Cn_beta = np.zeros([len(AoA)*len(mach),1]) 
         
         if self.training_file is None:
             # Calculate aerodynamics for table
@@ -269,7 +281,8 @@ class AVL(Stability):
                 CL[count*len(mach):(count+1)*len(mach),0] = results.aerodynamics.lift_coefficient[:,0]
                 CD[count*len(mach):(count+1)*len(mach),0] = results.aerodynamics.drag_breakdown.induced.total[:,0]
                 CM[count*len(mach):(count+1)*len(mach),0] = results.aerodynamics.pitch_moment_coefficient[:,0]
-           
+                Cm_alpha[count*len(mach):(count+1)*len(mach),0] = results.aerodynamics.cm_alpha[:,0]
+		Cn_beta[count*len(mach):(count+1)*len(mach),0] = results.aerodynamics.cn_beta[:,0]
                 
        
                 count += 1
@@ -283,12 +296,13 @@ class AVL(Stability):
             CL         = data_array[:,2:3]
             CD         = data_array[:,3:4]
             CM         = data_array[:,4:5]
-
+            Cm_alpha   = data_array[:,5:6]
+	    Cn_beta    = data_array[:,6:7]
         # Save the data
-        np.savetxt(geometry.tag+'_data.txt',np.hstack([xy,CL,CD,CM]),fmt='%10.8f',header='AoA Mach CL CD')
+        np.savetxt(geometry.tag+'_data.txt',np.hstack([xy,CL,CD,CM,Cm_alpha, Cn_beta]),fmt='%10.8f',header='AoA Mach CL CD')
 
         # Store training data
-        training.coefficients = np.hstack([CL,CD,CM])
+        training.coefficients = np.hstack([CL,CD,CM,Cm_alpha, Cn_beta])
         training.grid_points  = xy
         
 
@@ -303,21 +317,29 @@ class AVL(Stability):
         CL_data   = training.coefficients[:,0]
         CD_data   = training.coefficients[:,1]
         CM_data   = training.coefficients[:,2]
+	Cm_alpha_data = training.coefficients[:,3]
+	Cn_beta_data = training.coefficients[:,4]
+	
         xy        = training.grid_points 
         
         # Gaussian Process New
+	regr_cm_alpha = gaussian_process.GaussianProcess()
+	regr_cn_beta = gaussian_process.GaussianProcess()
         regr_cm = gaussian_process.GaussianProcess()
         regr_cl = gaussian_process.GaussianProcess()
         regr_cd = gaussian_process.GaussianProcess()
         cl_surrogate = regr_cl.fit(xy, CL_data)
         cd_surrogate = regr_cd.fit(xy, CD_data)
         cm_surrogate = regr_cm.fit(xy, CM_data) 
-   
+        cm_alpha_surrogate = regr_cm.fit(xy, Cm_alpha_data) 
+	cn_beta_surrogate = regr_cm.fit(xy, Cn_beta_data)
 
         self.surrogates.lift_coefficient = cl_surrogate
         self.surrogates.drag_coefficient = cd_surrogate
         self.surrogates.moment_coefficient = cm_surrogate
-        
+        self.surrogates.Cm_alpha_moment_coefficient = cm_alpha_surrogate
+        self.surrogates.Cn_beta_moment_coefficient  = cn_beta_surrogate	
+      
         # Standard subsonic test case
         AoA_points = np.linspace(-1.,7.,100)*Units.deg
         mach_points = np.linspace(.25,.9,100)      
@@ -327,14 +349,25 @@ class AVL(Stability):
         CL_sur = np.zeros(np.shape(AoA_mesh))
         CD_sur = np.zeros(np.shape(AoA_mesh))
         CM_sur = np.zeros(np.shape(AoA_mesh))
-        
+        Cm_a_sur = np.zeros(np.shape(AoA_mesh))
+	Cn_b_sur = np.zeros(np.shape(AoA_mesh)) 
         
         for jj in range(len(AoA_points)):
             for ii in range(len(mach_points)):
                 CL_sur[ii,jj] = cl_surrogate.predict(np.array([AoA_mesh[ii,jj],mach_mesh[ii,jj]]))
                 CD_sur[ii,jj] = cd_surrogate.predict(np.array([AoA_mesh[ii,jj],mach_mesh[ii,jj]]))
                 CM_sur[ii,jj] = cm_surrogate.predict(np.array([AoA_mesh[ii,jj],mach_mesh[ii,jj]]))
+		Cm_a_sur[ii,jj] = cm_alpha_surrogate.predict(np.array([AoA_mesh[ii,jj],mach_mesh[ii,jj]]))
+		Cn_b_sur[ii,jj] = cn_beta_surrogate.predict(np.array([AoA_mesh[ii,jj],mach_mesh[ii,jj]]))
   
+	fig = plt.figure('Coefficient of Moment Surrogate Plot')    
+	plt_handle = plt.contourf(AoA_mesh/Units.deg,mach_mesh,CM_sur,levels=None)
+	#plt.clabel(plt_handle, inline=1, fontsize=10)
+	cbar = plt.colorbar()
+	plt.scatter(xy[:,0]/Units.deg,xy[:,1])
+	plt.xlabel('Angle of Attack (deg)')
+	plt.ylabel('Mach Number')
+	cbar.ax.set_ylabel('Coefficient of Moment')  
         return
         
               
