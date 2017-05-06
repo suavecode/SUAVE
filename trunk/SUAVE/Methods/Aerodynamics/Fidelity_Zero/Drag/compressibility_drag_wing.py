589bb192e53fb405cfe94fba0ef8b86891be2ca1
Tim MacDonald <timdmacdo@gmail.com>
2017-05-05 21:49:54 -0700

    updating aero files

--- trunk/SUAVE/Methods/Aerodynamics/Fidelity_Zero/Drag/compressibility_drag_wing.py
+++ trunk/SUAVE/Methods/Aerodynamics/Fidelity_Zero/Drag/compressibility_drag_wing.py
@@ -1,19 +1,24 @@
 # compressibility_drag_wing.py
 # 
-# Created:  Dec 2013, SUAVE Team
-# Modified: Nov 2016, T. MacDonald
-#        
+# Created:  Your Name, Dec 2013
+# Modified:         
 
 # ----------------------------------------------------------------------
 #  Imports
 # ----------------------------------------------------------------------
 
-# SUAVE imports
+# suave imports
 from SUAVE.Analyses import Results
 from SUAVE.Core import (
-    Data, Container
+    Data, Container,  
 )
 from SUAVE.Components import Wings
+
+# python imports
+import os, sys, shutil
+from copy import deepcopy
+from warnings import warn
+import copy
 
 # package imports
 import numpy as np
@@ -25,6 +30,7 @@ import scipy as sp
 # ----------------------------------------------------------------------
 
 def compressibility_drag_wing(state,settings,geometry):
+#def compressibility_drag_wing(conditions,configuration,geometry):
     """ SUAVE.Methods.compressibility_drag_wing(conditions,configuration,geometry)
         computes the induced drag associated with a wing 
         
@@ -36,24 +42,28 @@ def compressibility_drag_wing(state,settings,geometry):
             based on a set of fits
             
     """
-    
+
     # unpack
-    conditions    = state.conditions
+    conditions = state.conditions
     configuration = settings    
     
-    wing = geometry
-    if wing.tag == 'main_wing':
+    wing       = geometry
+    if isinstance(wing,Wings.Main_Wing):
         wing_lifts = conditions.aerodynamics.lift_breakdown.compressible_wings # currently the total aircraft lift
     elif wing.vertical:
         wing_lifts = 0
     else:
-        wing_lifts = 0.15 * conditions.aerodynamics.lift_breakdown.compressible_wings
+        wing_lifts = 0.0 * conditions.aerodynamics.lift_breakdown.compressible_wings
         
-    mach           = conditions.freestream.mach_number
+    mach       = conditions.freestream.mach_number
     drag_breakdown = conditions.aerodynamics.drag_breakdown
     
+
     # start result
     total_compressibility_drag = 0.0
+    #drag_breakdown.compressible = Results()
+
+    
         
     # unpack wing
     t_c_w   = wing.thickness_to_chord
@@ -64,26 +74,31 @@ def compressibility_drag_wing(state,settings,geometry):
         cl_w = wing_lifts
     else:
         cl_w = 0
-        
-    cos_sweep = np.cos(sweep_w)
 
     # get effective Cl and sweep
-    tc = t_c_w /(cos_sweep)
-    cl = cl_w / (cos_sweep*cos_sweep)
+    tc = t_c_w /(np.cos(sweep_w))
+    cl = cl_w / (np.cos(sweep_w))**2
+    
+
+
+
+
+    #-----------Existing SUAVE-----------------------------------
+
 
     # compressibility drag based on regressed fits from AA241
     mcc_cos_ws = 0.922321524499352       \
                - 1.153885166170620*tc    \
                - 0.304541067183461*cl    \
-               + 0.332881324404729*tc*tc \
+               + 0.332881324404729*tc**2 \
                + 0.467317361111105*tc*cl \
-               + 0.087490431201549*cl*cl
+               + 0.087490431201549*cl**2
         
     # crest-critical mach number, corrected for wing sweep
-    mcc = mcc_cos_ws / cos_sweep
+    mcc = mcc_cos_ws / np.cos(sweep_w)
     
     # divergence mach number
-    MDiv = mcc * ( 1.02 + 0.08*(1 - cos_sweep) )
+    MDiv = mcc * ( 1.02 + 0.08*(1 - np.cos(sweep_w)) )
     
     # divergence ratio
     mo_mc = mach/mcc
@@ -92,7 +107,69 @@ def compressibility_drag_wing(state,settings,geometry):
     dcdc_cos3g = 0.0019*mo_mc**14.641
     
     # compressibility drag
-    cd_c = dcdc_cos3g * cos_sweep*cos_sweep*cos_sweep
+    cd_c = dcdc_cos3g * (np.cos(sweep_w))**3
+    
+    #-----------------------------------------------------------------
+
+
+
+
+
+
+
+    ##----------------------------------------------------------------------------------
+
+
+
+
+
+
+    
+    
+    ##modified implementation -------------------------------------------------------
+    
+    supercrit = 1.0    
+    
+    mcc = (0.95399999999999996 - 0.23499999999999999*cl)+0.025899999999999999*cl*cl
+    mcc = mcc - ((1.9630000000000001 - 1.0780000000000001 * cl) + 0.34999999999999998 * cl * cl) * tc
+    mcc = mcc + ((2.9689999999999999 - 2.738 * cl) + 1.4690000000000001 * cl * cl) * tc * tc
+    mcc = mcc + supercrit * 0.059999999999999998
+    mcc = mcc / np.cos(sweep_w)
+    rm = mach / mcc
+    dm = rm - 1.0
+    
+    cd_c = copy.deepcopy(mach)
+        
+
+    for irm in range(0,len(rm)): 
+        if((rm[irm] >= 0.5) and (rm[irm] < 0.80000000000000004)):
+            cd_c[irm] = 0.00013888951999999999 + 0.00055555999999999997 * dm[irm] + 0.00055556192 * dm[irm] * dm[irm]
+        elif((rm[irm] >= 0.80000000000000004) and (rm[irm] < 0.94999999999999996)):
+            cd_c[irm] = 0.00070899999999999999 + 0.0067330000000000003 * dm[irm] + 0.019560000000000001 * dm[irm] * dm[irm] + 0.011849999999999999 * dm[irm] * dm[irm] * dm[irm]
+
+        elif((rm[irm] >= 0.94999999999999996) and (rm[irm] < 1.0)):
+            cd_c[irm] = 0.001 + 0.027269999999999999 * dm[irm] + 0.49199999999999999 * dm[irm] * dm[irm] + 3.5738500000000002 * dm[irm] * dm[irm] * dm[irm]
+        elif(rm[irm] >= 1.0):
+            cd_c[irm] = ((0.001 + 0.027269999999999999 * dm[irm]) - 0.19520000000000001 * dm[irm] * dm[irm]) + 19.09 * dm[irm] * dm[irm] * dm[irm]   
+            
+        else:
+            cd_c[irm] = 0.0
+    
+    
+    
+    cd_c = cd_c * np.cos(sweep_w)**3.0 
+    mdiv = mcc * (1.02 + (1.0 - np.cos(sweep_w)) * 0.080000000000000002)
+    
+    #cd_c = cd_c.tolist()
+
+    MDiv  = mdiv    
+
+    
+    ##----------------------------------------------------------------------------------
+
+    
+    
+    
     
     # increment
     #total_compressibility_drag += cd_c
@@ -106,5 +183,13 @@ def compressibility_drag_wing(state,settings,geometry):
         divergence_mach           = MDiv    ,
     )
     drag_breakdown.compressible[wing.tag] = wing_results
+
+    
+
+
+    # dump total comp drag
+    #drag_breakdown.compressible.total = total_compressibility_drag
+    #conditions.aerodynamics.drag_breakdown.compressible[wing.tag] = wing_results
+    #conditions.aerodynamics.drag_breakdown.compressible.total = total_compressibility_drag
     
     return total_compressibility_drag
--- trunk/SUAVE/Methods/Aerodynamics/Fidelity_Zero/Drag/induced_drag_aircraft.py
+++ trunk/SUAVE/Methods/Aerodynamics/Fidelity_Zero/Drag/induced_drag_aircraft.py
@@ -1,8 +1,8 @@
+
 # induced_drag_aircraft.py
 # 
-# Created:  Dec 2013, SUAVE Team
-# Modified: Jan 2016, E. Botero
-       
+# Created:  Your Name, Dec 2013
+# Modified:         
 
 # ----------------------------------------------------------------------
 #  Imports
@@ -11,13 +11,21 @@
 # suave imports
 from SUAVE.Analyses import Results
 
+# python imports
+import os, sys, shutil
+from copy import deepcopy
+from warnings import warn
+
 # package imports
 import numpy as np
+import scipy as sp
+
 
 # ----------------------------------------------------------------------
-#  Induced Drag Aircraft
+#  The Function
 # ----------------------------------------------------------------------
 
+#def induced_drag_aircraft(conditions,configuration,geometry):
 def induced_drag_aircraft(state,settings,geometry):
     """ SUAVE.Methods.induced_drag_aircraft(conditions,configuration,geometry)
         computes the induced drag associated with a wing 
@@ -32,7 +40,7 @@ def induced_drag_aircraft(state,settings,geometry):
     """
 
     # unpack inputs
-    conditions    = state.conditions
+    conditions = state.conditions
     configuration = settings
     
     
@@ -42,18 +50,95 @@ def induced_drag_aircraft(state,settings,geometry):
     wing_e        = geometry.wings['main_wing'].span_efficiency
     ar            = geometry.wings['main_wing'].aspect_ratio # TODO: get estimate from weissinger
     CDp           = state.conditions.aerodynamics.drag_breakdown.parasite.total
+    wingleth      = 0.   #winglet length #geometry.wings['main_wing'].wingleth
+    tc            = geometry.wings['main_wing'].thickness_to_chord
+    lamda         = geometry.wings['main_wing'].taper
+    mach_number   = conditions.freestream.mach_number
+    Ne            = 0.0
     
-    if e == None:
-        e = 1/((1/wing_e)+np.pi*ar*K*CDp)
     
-    # start the result
-    total_induced_drag = aircraft_lift**2 / (np.pi*ar*e)
+    #SUAVE implementation-----------------------------
+    
+    #if e == None:
+        #e = 1/((1/wing_e)+np.pi*ar*K*CDp)
+    
+    
+    #start the result
+    total_induced_drag = 0.0
+    
+    #print("In induced_drag_aircraft:")
+    #print aircraft_lift
+    if geometry.fuselages.has_key('fuselage'):
+        fusewidth = geometry.fuselages['fuselage'].effective_diameter
+        fusefraction = fusewidth/geometry.wings['main_wing'].spans.projected
+        if(fusefraction > 0.5):
+            fusefraction = 0.5 
+        #s = 1.0 - 0.041599999999999998 * fusefraction - 1.7986 * fusefraction * fusefraction
+        s = 1.0 - 2.0 * fusefraction * fusefraction
+    else:
+        s = 1.
+    wing_e = s
+    
+
+    
+    cossw = np.cos(geometry.wings['main_wing'].sweeps.quarter_chord)
+    
+    
+    #raymer effect of winglets
+    arweff = ar * (1.0 + 1.9 * wingleth)    
+    
+    K_visc = 0.38/(cossw * cossw)
+    
+    #e_tot modified/shevell
+    #e_tot = 1.0/((1.0/wing_e)) #+ np.pi*ar*K_visc*CDp)
+    e_tot = 1.0/((1.0/wing_e)+ np.pi*arweff*K_visc*CDp)
+        
+    #arweff = ar * (1.0 + 0.80000000000000004 * wingleth)**2.0
+    #arweff = ar
+    
+    #wing_e raymer
+    #e_tot = 4.61*(1.0-0.045*arweff**0.68)*(cossw**0.15) - 3.1 
+    
+    #e_tot Grosu
+    #e_tot = 1.0/(1.08 + 0.028*np.pi*arweff*tc/aircraft_lift**2.0)
+    
+    #e_tot Howe
+#    fl = 0.005*(1.0+1.5*(lamda-0.6)**2.0)
+#    t1 = 1.0 + 0.12*mach_number**2.0
+#    t2_1 = (0.142 + fl*arweff*(10.0*tc)**0.33)/(cossw*cossw)
+#    t2_2 = 0.1*(3.0*Ne + 1.0)/((4.0 + arweff)**0.8)
+    #e_tot = 1.0/(t1*(1.0 + t2_1 + t2_2))
+
+    
+    #total_induced_drag = aircraft_lift**2.0 / (np.pi*ar*wing_e) + aircraft_lift**2.0*CDp*K_visc
+    #total_induced_drag = aircraft_lift**2.0 / (np.pi*ar*e_tot)
+    total_induced_drag = aircraft_lift**2.0 / (np.pi*arweff*e_tot)
+    
+    
+    
+    ##given e
+    #e_tot = geometry.wings[0].span_efficiency
+    #wing_e = e_tot
+    #total_induced_drag = aircraft_lift**2.0 / (np.pi*ar*e_tot)
+    
+        
+        
+        
+    
+        
+    #total_induced_drag = cdi
+
+    #print "induced drag inviscid",cdiwing[0]
         
     # store data
     conditions.aerodynamics.drag_breakdown.induced = Results(
         total             = total_induced_drag ,
         efficiency_factor = e                  ,
         aspect_ratio      = ar                 ,
+        e_total           = e_tot            ,
+        e_inviscid        = wing_e          ,
     )
+    
+    # done!
 
     return total_induced_drag
