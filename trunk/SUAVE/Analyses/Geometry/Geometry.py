dea0229936c04425c0966d34ab8bd1922f6f85d6
Unknown <jtrentsmart@gmail.com>
2017-08-10 21:38:09 -0700

    Loads and Geometry Documentation

--- trunk/SUAVE/Analyses/Geometry/Geometry.py
+++ trunk/SUAVE/Analyses/Geometry/Geometry.py
@@ -1,3 +1,4 @@
+## @ingroup Analyses-Geometry
 # Geometry.py
 #
 # Created:  
@@ -19,12 +20,46 @@ class Geometry(Analysis):
     """ SUAVE.Analyses.Geometry.Geometry()
     """
     def __defaults__(self):
+        """This sets the default values and methods for the analysis.
+            
+                    Assumptions:
+                    None
+            
+                    Source:
+                    N/A
+            
+                    Inputs:
+                    None
+            
+                    Outputs:
+                    None
+            
+                    Properties Used:
+                    N/A
+                """        
         self.tag    = 'geometry'
         self.features = Data()
         self.settings = Data()
         
         
     def evaluate(self,condtitions):
+        """Evaluate the Geometry analysis.
+            
+                    Assumptions:
+                    None
+            
+                    Source:
+                    N/A
+            
+                    Inputs:
+                    None
+            
+                    Outputs:
+                    Results of the Geometry Analysis
+            
+                    Properties Used:
+                    N/A                
+                """        
         return Results()
     
         
