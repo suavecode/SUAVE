28fa97087501bd542fbcf5abbe9be22254c21225
Unknown <jtrentsmart@gmail.com>
2017-08-10 22:43:50 -0700

    Vehicle Analyses Class Documentation

--- trunk/SUAVE/Analyses/Vehicle.py
+++ trunk/SUAVE/Analyses/Vehicle.py
@@ -1,3 +1,4 @@
+## @ingroup Analyses
 # Vehicle.py
 #
 # Created:
@@ -17,8 +18,33 @@ from Analysis import Analysis
 
 class Vehicle(Analysis.Container):
     """ SUAVE.Analyses.Vehicle()
+    
+        The Vehicle Analyses Container Class
+        
+            Assumptions:
+            None
+            
+            Source:
+            N/A
     """
     def __defaults__(self):
+        """This sets the default analyses to be applied to the vehicle.
+        
+                Assumptions:
+                None
+                
+                Source:
+                N/A
+                
+                Inputs:
+                None
+                
+                Outputs:
+                None
+                
+                Properties Used:
+                N/A
+        """
         self.sizing       = None
         self.weights      = None
         self.aerodynamics = None
@@ -30,6 +56,23 @@ class Vehicle(Analysis.Container):
         self.costs        = None
 
     def append(self,analysis):
+        """This is used to add new analyses to the container.
+        
+                Assumptions:
+                None
+                
+                Source:
+                N/A
+                
+                Inputs:
+                Analysis to be added
+                
+                Outputs:
+                None
+                
+                Properties Used:
+                N/A
+        """
 
         key = self.get_root(analysis)
 
@@ -39,6 +82,24 @@ class Vehicle(Analysis.Container):
     _analyses_map = None
 
     def __init__(self,*args,**kwarg):
+        """This sets the initialization behavior of the vehicle analysis
+           container. Maps analysis paths to string keys.
+           
+                Assumptions:
+                None
+                
+                Source:
+                N/A
+                
+                Inputs:
+                None
+                
+                Outputs:
+                None
+                
+                Properties Used:
+                N/A
+        """
 
         Analysis.Container.__init__(self,*args,**kwarg)
 
@@ -56,7 +117,26 @@ class Vehicle(Analysis.Container):
 
     def get_root(self,analysis):
 
-        # find analysis root by type, allow subclasses
+        """ This is used to determine the root of the analysis path associated
+            with a particular analysis key by the analysis map.
+            
+                Assumptions:
+                None
+                
+                Source:
+                N/A
+                
+                Inputs:
+                Analysis key to be checked
+                
+                Outputs:
+                Path root of analysis
+                
+                Properties Used:
+                N/A
+                
+                
+        """
         for analysis_type, analysis_root in self._analyses_map.iteritems():
             if isinstance(analysis,analysis_type):
                 break
