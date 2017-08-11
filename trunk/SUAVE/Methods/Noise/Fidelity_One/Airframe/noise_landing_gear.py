bd6e7a8c41e42c0782dc20744eb965f5d1f8c4b4
Carlos Roberto Ilário da Silva <carlosilario@users.noreply.github.com>
2017-08-11 07:16:10 -0300

    clean up

--- trunk/SUAVE/Methods/Noise/Fidelity_One/Airframe/noise_landing_gear.py
+++ trunk/SUAVE/Methods/Noise/Fidelity_One/Airframe/noise_landing_gear.py
@@ -1,7 +1,7 @@
 ## @ingroupMethods-Noise-Fidelity_One-Airframe
 # noise_landing_gear.py
 # 
-# Created:  Jun 2015, Carlos
+# Created:  Jun 2015, C. Ilario
 # Modified: Jan 2016, E. Botero
 
 # ----------------------------------------------------------------------        
