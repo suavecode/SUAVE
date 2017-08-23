d7fa732e610c729509bd9210e7c87d1e1573f605
Emilio Botero <ebotero@stanford.edu>
2017-08-23 13:44:13 -0700

    removing unused files

--- trunk/SUAVE/Methods/Aerodynamics/Supersonic_Zero/Drag/__init__.py
+++ trunk/SUAVE/Methods/Aerodynamics/Supersonic_Zero/Drag/__init__.py
@@ -3,13 +3,11 @@
 # @ingroup Methods-Aerodynamics-Supersonic_Zero
 
 from parasite_drag_propulsor import parasite_drag_propulsor
-from parasite_drag_aircraft import parasite_drag_aircraft
 from miscellaneous_drag_aircraft import miscellaneous_drag_aircraft
 from induced_drag_aircraft import induced_drag_aircraft
 from wave_drag_lift import wave_drag_lift
 from wave_drag_volume import wave_drag_volume
 from wave_drag_body_of_rev import wave_drag_body_of_rev
 from compressibility_drag_total import compressibility_drag_total
-from compute_aircraft_drag import compute_aircraft_drag
 from total_aircraft import total_aircraft 
 
