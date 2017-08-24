fb2acd570470b334e45b81b72ab367cf3619d3e1
Emilio Botero <ebotero@stanford.edu>
2017-08-23 23:42:40 -0700

    basic working lifting line, completely vectorized

--- trunk/SUAVE/Methods/Aerodynamics/Lifting_Line/Lifting_Line.py
+++ trunk/SUAVE/Methods/Aerodynamics/Lifting_Line/Lifting_Line.py
@@ -36,6 +36,8 @@ def lifting_line(state,settings,geometry):
     """  
     
     # Unpack first round:
+    wing        = geometry
+    freestream  = state.conditions.freestream
     orientation = wing.vertical    
     
     # Don't bother doing the calculation if it is a vertical tail
@@ -44,34 +46,39 @@ def lifting_line(state,settings,geometry):
         Cd = 0.0
         return Cl, Cd
     else:
-        continue
+        pass
         
-
-    
     # Potentially useful unpacks
-    sweep       = wing.sweeps.quarter_chord
-    sym_para    = wing.symmetric
-    AR          = wing.aspect_ratio
+    #sweep       = wing.sweeps.quarter_chord
+    #sym_para    = wing.symmetric
+    #
 
+    # Unpack fo'real
+    b     = wing.spans.projected
+    S     = wing.areas.reference
+    AR    = wing.aspect_ratio
+    MAC   = wing.chords.mean_aerodynamic
+    r     = settings.number_of_stations # Number of divisions
+    rho   = freestream.density # Freestream density
+    V     = freestream.velocity # Freestream velocity
+    mu    = freestream.dynamic_viscosity # Freestream viscosity
+    alpha = state.conditions.aerodynamics.angle_of_attack
     
+    repeats = np.size(alpha)
     
-    # unpack
-    b     = wing.spans.projected # Wingspan
-    S     = wing.areas.reference # Reference area
-    r     = 20   # Need to set somewhere, this will be a setting
-    rho   = None # Freestream density
-    V     = None # Freestream velocity
-    mu    = None # Freestream viscosity
-    cla   = None # 2-D lift curve slope
-    azl   = None # 2-D 
-    alpha = None
+    # Need to set to something
+    cla   = 2 * np.pi # 2-D lift curve slope
+    azl   = 0. # 2-D 
 
-    
+
+    # Start doing calculations
     N      = r-1                  # number of spanwise divisions
-    n      = np.linspace(0,N,N)   # vectorize
+    n      = np.linspace(1,N,N)   # vectorize
     thetan = n*np.pi/r            # angular stations
     yn     = -b*np.cos(thetan)/2. # y locations based on the angular spacing
     etan   = np.abs(2.*yn/b)       # normalized coordinates
+    etam   = np.pi*np.sin(thetan)/(2*r) # Useful mulitplier
+
     
     # Project the spanwise y locations into the chords
     # If spanwise stations are setup
@@ -92,29 +99,127 @@ def lifting_line(state,settings,geometry):
         root_chord  = wing.chords.root
         tip_chord   = wing.chords.tip  
         
-        c    = root_chord+root_chord*(taper-1.)*yn
-        ageo = (tip_twist-root_twist)*yn+root_twist
-        
+        c    = root_chord+root_chord*(taper-1.)*etan
+        ageo = (tip_twist-root_twist)*etan+root_twist
 
-    # Setup for loops to be removed later, both can be vectorized
-    A = np.zeros((N,N))
-    for ii in xrange(0,N):
-        for jj in xrange(0,N):
-            RHS[ii,jj] = np.sin(ii*thetan[jj])*(np.sin(thetan)+ii*c[jj]*cla[jj]/(4.*b))
-            
-    LHS = np.zeros(N)
-    for ii in xrange(0,N):
-        LHS[ii] = (c[ii]*cla[ii]*np.sin(thetan[ii])*(alpha+ageo[ii]-azl[ii]))/(4.*b)
+    
+    
+    k = c*cla/(4.*b) # Grouped term 
+    
+    n_trans = np.atleast_2d(n).T
         
-    A = np.linalg.solve(RHS,LHS)
+    # Right hand side matrix
+    RHS = (np.sin(n_trans*thetan)*(np.sin(thetan)+n_trans*k))
     
-    #clcb = np.zeros(N)
-    #for ii in xrange(0,N):
-        #clcb = 4. * A[ii] * np.sin(ii*thetan(ii))
+    # Expand out for all the angles of attack
+    RHS2 =  np.tile(RHS, (repeats,1,1))
+
+    # Left hand side vector    
+    LHS = (k*np.sin(thetan)*(alpha+ageo-azl))
         
-    CD = 0.0
-    CL = 0.0
+    # The Fourier Coefficients
+    A = np.linalg.solve(RHS2,LHS)
     
+    # The 3-D Coefficient of lift
+    CL = A[:,0]*np.pi*AR
     
+    # Find the sectional coefficients of lift
+    Cl = b*np.cumsum(4*A*np.sin(n*thetan),axis=1)/c
+    
+    # induced alpha
+    alpha_i = np.cumsum(n*A*np.sin(n*A)/np.sin(thetan),axis=1)
+    
+    # Sectional vortex drag
+    Cdv = Cl*alpha_i
+    
+    # Total vortex drag
+    CDv = np.sum(Cdv*AR*etam,axis=1)
+    
+    #############
+    # Profile drag of a 2-D section
+    Cdn = 0.01
+    #############
+    
+    # Find the profile drag
+    CDp = np.sum(Cdn*c*etam)/MAC
+    
+    CD  = CDv + CDp
    
     return CL, CD
+
+
+# ----------------------------------------------------------------------
+#   Unit Tests
+# ----------------------------------------------------------------------
+# this will run from command line, put simple tests for your code here
+if __name__ == '__main__':
+    
+    import SUAVE
+    from SUAVE.Core import Data, Units
+    
+    
+    # ------------------------------------------------------------------        
+    #   State
+    # ------------------------------------------------------------------           
+    
+    state = Data()
+    state.conditions = Data()
+    state.conditions.freestream = Data()
+    state.conditions.aerodynamics = Data()
+    
+    atmosphere = SUAVE.Analyses.Atmospheric.US_Standard_1976()
+    atmosphere_conditions = atmosphere.compute_values(0.)
+    state.conditions.freestream.update(atmosphere_conditions)
+    state.conditions.freestream.velocity = np.atleast_2d([100., 100.]).T
+    state.conditions.aerodynamics.angle_of_attack = np.atleast_2d([2. * Units.deg, 2. * Units.deg]).T
+
+    # ------------------------------------------------------------------        
+    #   Main Wing
+    # ------------------------------------------------------------------        
+    
+    wing = SUAVE.Components.Wings.Main_Wing()
+    wing.tag = 'main_wing'
+    
+    wing.aspect_ratio            = 10.18
+    wing.sweeps.quarter_chord    = 25 * Units.deg
+    wing.thickness_to_chord      = 0.1
+    wing.taper                   = 0.1
+    wing.span_efficiency         = 0.9
+    
+    wing.spans.projected         = 34.32   
+    
+    wing.chords.root             = 7.760 * Units.meter
+    wing.chords.tip              = 0.782 * Units.meter
+    wing.chords.mean_aerodynamic = 4.235 * Units.meter
+    
+    wing.areas.reference         = 124.862 
+    
+    wing.twists.root             = 4.0 * Units.degrees
+    wing.twists.tip              = 0.0 * Units.degrees
+    
+    wing.origin                  = [13.61,0,-1.27]
+    wing.aerodynamic_center      = [0,0,0]  #[3,0,0]
+    
+    wing.vertical                = False
+    wing.symmetric               = True
+    wing.high_lift               = True
+    
+    wing.dynamic_pressure_ratio  = 1.0    
+    
+    geometry = wing
+    
+    # ------------------------------------------------------------------        
+    #   Main Wing
+    # ------------------------------------------------------------------      
+    
+    settings = Data()
+    settings.number_of_stations = 4
+    
+    
+    
+    CL, CD = lifting_line(state, settings, geometry)
+    
+    print 'CL: '
+    print CL
+    print 'CD: '
+    print CD
