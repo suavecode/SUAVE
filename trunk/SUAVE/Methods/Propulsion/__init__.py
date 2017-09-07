5d7195eb5748f75639a8b13c3b361197525e9c06
pedro <pedro.mg.94@hotmail.com>
2017-09-07 11:01:05 +0100

    Created: Ramjet sizing

--- trunk/SUAVE/Methods/Propulsion/__init__.py
+++ trunk/SUAVE/Methods/Propulsion/__init__.py
@@ -8,5 +8,6 @@ from turbofan_nox_emission_index import turbofan_nox_emission_index
 from electric_motor_sizing import size_from_kv, size_from_mass
 from turbofan_sizing import turbofan_sizing
 from turbojet_sizing import turbojet_sizing
+from ramjet_sizing import ramjet_sizing
 from fm_id import fm_id
 import electric_motor_sizing
