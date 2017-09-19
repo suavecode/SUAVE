ae531650a8e2d383184c5d833ce3602f5268b9c0
Pedro Goncalves <pedro.mg.94@hotmail.com>
2017-09-19 19:02:55 +0100

    Modified: Pressure loss correction on inlet

--- trunk/SUAVE/Components/Energy/Converters/Compression_Nozzle.py
+++ trunk/SUAVE/Components/Energy/Converters/Compression_Nozzle.py
@@ -147,7 +147,7 @@ class Compression_Nozzle(Energy_Component):
             #-- Inlet Mach > 1.0, normal shock
             Mach[i_high]   = np.sqrt((1.+(gamma-1.)/2.*Mo[i_high]**2.)/(gamma*Mo[i_high]**2-(gamma-1.)/2.))
             T_out[i_high]  = Tt_out[i_high]/(1.+(gamma-1.)/2*Mach[i_high]*Mach[i_high])
-            Pt_out[i_high] = Pt_in[i_high]*((((gamma+1.)*(Mo[i_high]**2.))/((gamma-1.)*Mo[i_high]**2.+2.))**(gamma/(gamma-1.)))*((gamma+1.)/(2.*gamma*Mo[i_high]**2.-(gamma-1.)))**(1./(gamma-1.))
+            Pt_out[i_high] = pid*Pt_in[i_high]*((((gamma+1.)*(Mo[i_high]**2.))/((gamma-1.)*Mo[i_high]**2.+2.))**(gamma/(gamma-1.)))*((gamma+1.)/(2.*gamma*Mo[i_high]**2.-(gamma-1.)))**(1./(gamma-1.))
             P_out[i_high]  = Pt_out[i_high]*(1.+(2.*gamma/(gamma+1.))*(Mach[i_high]**2.-1.))
         
         else:
