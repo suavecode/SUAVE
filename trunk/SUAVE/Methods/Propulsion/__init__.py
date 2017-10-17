aa730432ffb01e3c45429183641d94448e925a5e
Emilio Botero <ebotero@stanford.edu>
2017-10-17 10:47:00 -0700

    updating emission index

--- trunk/SUAVE/Methods/Propulsion/__init__.py
+++ trunk/SUAVE/Methods/Propulsion/__init__.py
@@ -4,7 +4,7 @@
 
 from ducted_fan_sizing import ducted_fan_sizing
 from propeller_design import propeller_design
-from turbofan_nox_emission_index import turbofan_nox_emission_index
+from turbofan_emission_index import turbofan_emission_index
 from electric_motor_sizing import size_from_kv, size_from_mass
 from turbofan_sizing import turbofan_sizing
 from turbojet_sizing import turbojet_sizing
