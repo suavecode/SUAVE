6023f792c46152667a3de5f1920a878d2dfb46e2
Emilio Botero <ebotero@stanford.edu>
2017-10-24 12:02:46 -0700

    energy container

--- trunk/SUAVE/Components/Energy/Energy.py
+++ trunk/SUAVE/Components/Energy/Energy.py
@@ -3,6 +3,7 @@
 # 
 # Created:  Aug 2014, E. Botero
 # Modified: Feb 2016, T. MacDonald
+#           Oct 2017, E. Botero
 
 # ------------------------------------------------------------
 #  Imports
@@ -42,77 +43,3 @@ class Energy(Physical_Component):
         N/A
         """            
         pass
-
-
-# ------------------------------------------------------------
-#  Energy Component Classes
-# ------------------------------------------------------------
-## @ingroup Energy
-class Component(Physical_Component):
-    """A class representing an generic energy component.
-    
-    Assumptions:
-    None
-    
-    Source:
-    N/A
-    """     
-    def __defaults__(self):
-        """This sets the default tag for a component.
-
-        Assumptions:
-        None
-
-        Source:
-        N/A
-
-        Inputs:
-        None
-
-        Outputs:
-        None
-
-        Properties Used:
-        N/A
-        """          
-        self.tag = 'Energy Component'
-    
-    def provide_power():
-        """A stub for providing power.
-
-        Assumptions:
-        None
-
-        Source:
-        N/A
-
-        Inputs:
-        None
-
-        Outputs:
-        None
-
-        Properties Used:
-        N/A
-        """          
-        pass
-    
-## @ingroup Energy
-class ComponentContainer(Physical_Component.Container):
-    """A container for an energy component.
-    
-    Assumptions:
-    None
-    
-    Source:
-    N/A
-    """     
-    pass
-
-# ------------------------------------------------------------
-#  Handle Linking
-# ------------------------------------------------------------
-Energy.Component = Component
-Energy.Component.Container = ComponentContainer
-
-
