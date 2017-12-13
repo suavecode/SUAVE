4befee54c59467b6b66ce70cee8d30d9526c4c50
Wally Maier <wallytmaier@gmail.com>
2017-12-13 11:04:31 -0800

    I wanna be included. Variable changes to make consistant

--- trunk/SUAVE/Methods/Propulsion/fm_id.py
+++ trunk/SUAVE/Methods/Propulsion/fm_id.py
@@ -1,31 +1,31 @@
 ## @ingroup Methods-Propulsion
 # fm_id.py
-# 
+#
 # Created:  ### ####, SUAVE Team
 # Modified: Feb 2016, E. Botero
-
+#           Dec 2017, W. Maier
 # ----------------------------------------------------------------------
 #  fm_id
 # ----------------------------------------------------------------------
 
 ## @ingroup Methods-Propulsion
-def fm_id(M,g):
-    """
-    Function that takes in the Mach number, and outputs a function fm
-    that's commonly used in compressible flow calculations
-    
+def fm_id(M,gamma):
+    """Function that takes in the Mach number and isentropic expansion factor,
+    and outputs a value for f(M) that's commonly used in compressible flow
+    calculations.
+
     Inputs:
     M       [dimensionless]
-    
+    gamma       [dimensionless]
+
     Outputs:
     fm
-    
     """
-    
-    m0 = (g+1)/(2*(g-1))
-    m1 = ((g+1)/2)**m0
-    m2 = (1+(g-1)/2*M**2)**m0
-    
+
+    m0 = (gamma+1)/(2*(gamma-1))
+    m1 = ((gamma+1)/2)**m0
+    m2 = (1+(gamma-1)/2*M**2)**m0
+
     fm = m1*M/m2
-    
+
     return fm
--- trunk/SUAVE/Methods/Propulsion/fm_solver.py
+++ trunk/SUAVE/Methods/Propulsion/fm_solver.py
@@ -1,10 +1,14 @@
 ## @ingroup Methods-Propulsion
 # fm_solver.py
-# 
+#
 # Created:  Sep 2017, P Goncalves
+# Modified: Dec 2107, W. Maier
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
 
 import numpy as np
-
 from scipy.optimize import fsolve
 
 # ----------------------------------------------------------------------
@@ -14,36 +18,35 @@ from scipy.optimize import fsolve
 ## @ingroup Methods-Propulsion
 
 
-def fm_solver(Aratio, M0, gamma):
-    """
-    Function that takes in an area ratio and a Mach number associated to
-    one of the areas and outputs the missing Mach number
-    
-    Inputs:
-    M       [dimensionless]
-    gamma   [dimensionless]
-    Aratio  [dimensionless]
-    
-    Outputs:
-    M1      [dimensionless]
-    
-    """
-    
-    func = lambda M1: (M0/M1*((1+(gamma-1)/2*M1**2)/(1+(gamma-1)/2*M0**2))**((gamma+1)/(2*(gamma-1))))-Aratio
+def fm_solver(area_ratio, M0, gamma):
+    """Function that takes in an area ratio and a Mach number associated to
+    one of the areas and outputs the missing Mach number.
 
-    #Initializing the array
+    Inputs:
+    M           [dimensionless]
+    gamma       [dimensionless]
+    area_ratio  [dimensionless]
+
+    Outputs:
+    M1          [dimensionless]
+    """
+
+    func = lambda M1: (M0/M1*((1+(gamma-1)/2*M1**2)/(1+(gamma-1)/2*M0**2))**((gamma+1)/(2*(gamma-1))))-area_ratio
+
+    # Initializing the array
     M1_guess = 1.0*M0/M0
-    
+
     # Separating supersonic and subsonic solutions
     i_low = M0 <= 1.0
     i_high = M0 > 1.0
 
-    #--Subsonic solution
+    # Subsonic solution initialization
     M1_guess[i_low]= .1
-    
-    #--Supersonic solution
+
+    # Supersonic solution initialization
     M1_guess[i_high]= 1.1
- 
+
+    # Solving
     M1 = fsolve(func,M1_guess, factor=0.1)
-    
+
     return M1
