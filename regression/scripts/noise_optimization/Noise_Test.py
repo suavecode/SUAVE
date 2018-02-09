46f7ec7d0c170aa2f3681b0eb57614a1be4fdd21
Emilio Botero <ebotero@stanford.edu>
2018-02-09 12:05:00 -0800

    Fixed differences between Develop
    Updated regression values to match updates to Develop.

--- regression/scripts/noise_optimization/Noise_Test.py
+++ regression/scripts/noise_optimization/Noise_Test.py
@@ -46,7 +46,7 @@ def main():
     noise_cumulative_margin = objectives[0]
     
     actual = Data()    
-    actual.noise_cumulative_margin = 17.7898019866
+    actual.noise_cumulative_margin = 17.24473246767988
 
     error = Data()
     error.noise_cumulative_margin = (actual.noise_cumulative_margin - noise_cumulative_margin)/actual.noise_cumulative_margin
