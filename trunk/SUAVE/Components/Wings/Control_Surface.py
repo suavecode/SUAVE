68fa1f718a4564441876319b538ff63569eb4e83
MatthewClarke <mclake2@stanford.edu>
2018-02-27 01:19:12 -0800

    addition of CS section to CS

--- trunk/SUAVE/Components/Wings/Control_Surface.py
+++ trunk/SUAVE/Components/Wings/Control_Surface.py
@@ -53,3 +53,67 @@ class Control_Surface(Component):
         self.sections = Data()  
 	self.prev = None
 	self.next = None # for connectivity
+	
+    def append_section(self,section):	
+	"""Adds a section	
+	
+	Assumptions:	
+	None	
+	
+	Source:	
+	N/A	
+	
+	Inputs:	
+	None	
+	
+	Outputs:	
+	None	
+	
+	Properties Used:	
+	N/A	
+	"""         	
+	
+	# assert database type	
+	if not isinstance(section,Data):	
+	    raise Exception, 'input control surface section must be of type Data()'	
+	
+	# store data	
+	self.sections.append(section)	
+	
+	return	
+	
+	
+## @ingroup Components-Wings	
+class Control_Surface_Section(Lofted_Body.Section):	
+    def __defaults__(self):	
+	"""This sets the default values control surface sections defined in SUAVE.	
+	
+	Assumptions:	
+	None	
+	
+	Source:	
+	N/A	
+	
+	Inputs:	
+	None	
+	
+	Outputs:	
+	None	
+	
+	Properties Used:	
+	N/A	
+	"""         	
+		
+	self.tag            = 'control_section'	
+	self.chord          = 0.0	
+	self.chord_fraction = 0.0	
+	self.twist          = 0.0 # Offset / deflection in neutral position	
+	
+	self.origins = Data()	
+	self.origins.dimensional    = [0.0,0.0,0.0]	
+	self.origins.span_fraction  = 0.0	
+	self.origins.chord_fraction = 0.0	
+	
+	self.points = Data()	
+	self.points.leading_edge  = 0.0	
+	self.points.trailing_edge = 0.0	
