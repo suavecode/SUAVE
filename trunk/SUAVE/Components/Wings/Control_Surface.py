353829e48374d2bb5354fc5db80c1f14334ada78
MatthewClarke <mclake2@stanford.edu>
2018-02-28 09:44:15 -0800

    readded imported lofted body

--- trunk/SUAVE/Components/Wings/Control_Surface.py
+++ trunk/SUAVE/Components/Wings/Control_Surface.py
@@ -12,6 +12,7 @@
 
 from SUAVE.Core import Data
 from SUAVE.Components    import Component
+from SUAVE.Components import Lofted_Body
 
 # ------------------------------------------------------------
 #  Control Surfaces
