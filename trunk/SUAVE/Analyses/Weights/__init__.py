4ccb0ec9d01f32e2a57f73a0c60dead43eca3697
Unknown <jtrentsmart@gmail.com>
2018-03-02 13:33:59 -0800

    Amended Weights Analyses Init

--- trunk/SUAVE/Analyses/Weights/__init__.py
+++ trunk/SUAVE/Analyses/Weights/__init__.py
@@ -3,8 +3,10 @@
 ## @ingroup Analyses
 
 # Attributes
-from Weights                import Weights
-from Weights_BWB            import Weights_BWB
-from Weights_Tube_Wing      import Weights_Tube_Wing
-from Weights_UAV            import Weights_UAV
-from Weights_eStopped_Rotor import Weights_eStopped_Rotor
+from Weights                        import Weights
+from Weights_BWB                    import Weights_BWB
+from Weights_Tube_Wing              import Weights_Tube_Wing
+from Weights_UAV                    import Weights_UAV
+from Weights_electricHelicopter     import Weights_electricHelicopter
+from Weights_electricStoppedRotor   import Weights_electricStoppedRotor
+from Weights_electricTiltrotor      import Weights_electricTiltrotor
