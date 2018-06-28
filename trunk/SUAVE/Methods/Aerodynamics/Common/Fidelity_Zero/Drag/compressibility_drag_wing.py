e7d3db90e2ef2cbdda9660d5dc1500428098e9c4
MatthewClarke <mclake2@stanford.edu>
2018-06-28 16:27:50 -0700

    compressibility drag correction

--- trunk/SUAVE/Methods/Aerodynamics/Common/Fidelity_Zero/Drag/compressibility_drag_wing.py
+++ trunk/SUAVE/Methods/Aerodynamics/Common/Fidelity_Zero/Drag/compressibility_drag_wing.py
@@ -49,9 +49,7 @@ def compressibility_drag_wing(state,settings,geometry):
     
     # unpack
     conditions    = state.conditions
-    configuration = settings             # unused
-
-    
+    configuration = settings    # unused
     
     wing = geometry
     if wing.tag == 'main_wing':
@@ -63,66 +61,26 @@ def compressibility_drag_wing(state,settings,geometry):
         
     mach           = conditions.freestream.mach_number
     drag_breakdown = conditions.aerodynamics.drag_breakdown
-    Sref          = wing.areas.reference    
-    # start result
     
+    # start result
     total_compressibility_drag = 0.0
         
     # unpack wing
     t_c_w   = wing.thickness_to_chord
+    sweep_w = wing.sweeps.quarter_chord
     
-    if len(wing.Segments.keys())>0: # if wing has segments
-        symm           = wing.symmetric
-        semispan       = wing.spans.projected*0.5 * (2 - symm)
-        root_chord     = wing.chords.root
-        num_segments   = len(wing.Segments.keys())     
-        
-        weighted_sweep = 0
-        #Sref           = 0
-        for i_segs in xrange(num_segments): 
-            if i_segs == num_segments-1:
-                continue 
-            else:                    
-                span_seg        = semispan*(wing.Segments[i_segs+1].percent_span_location - wing.Segments[i_segs].percent_span_location )
-                chord_root      = root_chord*wing.Segments[i_segs].root_chord_percent
-                chord_tip       = root_chord*wing.Segments[i_segs+1].root_chord_percent
-                Sref_seg        = span_seg *(chord_root+chord_tip)*0.5
-                weighted_sweep += wing.Segments[i_segs].sweeps.quarter_chord*Sref_seg 
-                #Sref           += Sref_seg       
-        sweep_w = weighted_sweep/Sref
-        
-  
-    else: # if wing does not have segments          
-        sweep_w = wing.sweeps.quarter_chord
-    
-    # compute compressibility drag  
-    cd_c, mcc, MDiv ,tc = compute_compressibility_drag (sweep_w,t_c_w,wing,wing_lifts,mach)
-    
-    # dump data to conditions
-    wing_results = Data(
-        compressibility_drag      = cd_c    ,
-        thickness_to_chord        = tc      , 
-        wing_sweep                = sweep_w , 
-        crest_critical            = mcc     ,
-        divergence_mach           = MDiv    ,
-    )
-    drag_breakdown.compressible[wing.tag] = wing_results
-    
-    return total_compressibility_drag
-
-def  compute_compressibility_drag (sweep_w, t_c_w,wing,wing_lifts,mach):
     # Currently uses vortex lattice model on all wings
     if wing.tag=='main_wing':
         cl_w = wing_lifts
     else:
         cl_w = 0
-      
+        
     cos_sweep = np.cos(sweep_w)
-    
+
     # get effective Cl and sweep
     tc = t_c_w /(cos_sweep)
     cl = cl_w / (cos_sweep*cos_sweep)
-    
+
     # compressibility drag based on regressed fits from AA241
     mcc_cos_ws = 0.922321524499352       \
                - 1.153885166170620*tc    \
@@ -145,5 +103,18 @@ def  compute_compressibility_drag (sweep_w, t_c_w,wing,wing_lifts,mach):
     
     # compressibility drag
     cd_c = dcdc_cos3g * cos_sweep*cos_sweep*cos_sweep
-
-    return cd_c, mcc, MDiv ,tc
+    
+    # increment
+    #total_compressibility_drag += cd_c
+    
+    # dump data to conditions
+    wing_results = Data(
+        compressibility_drag      = cd_c    ,
+        thickness_to_chord        = tc      , 
+        wing_sweep                = sweep_w , 
+        crest_critical            = mcc     ,
+        divergence_mach           = MDiv    ,
+    )
+    drag_breakdown.compressible[wing.tag] = wing_results
+    
+    return total_compressibility_drag
