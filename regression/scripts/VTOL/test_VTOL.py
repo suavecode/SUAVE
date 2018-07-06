8217e8c9f98109a1af53539bf4d29efc3c5573d8
MatthewClarke <mclake2@stanford.edu>
2018-07-06 12:18:04 -0700

    regression updates

--- regression/scripts/VTOL/test_VTOL.py
+++ regression/scripts/VTOL/test_VTOL.py
@@ -48,40 +48,15 @@ def main():
 
     # vehicle analyses
     configs_analyses = analyses_setup(configs)
-
-    ## ------------------------------------------------------------------
-    ##  Weights
-    #weights = SUAVE.Analyses.Weights.Weights()
-    #weights.settings.empty_weight_method = \
-        #SUAVE.Methods.Weights.Correlations.UAV.empty
-    #weights.vehicle = vehicle
-    #analyses.append(weights)
-    
-    ## ------------------------------------------------------------------
-    ##  Aerodynamics Analysis
-    #aerodynamics                                  = SUAVE.Analyses.Aerodynamics.AERODAS()
-    #aerodynamics.geometry                         = copy.deepcopy(configs.base)   
-    #aerodynamics.settings.drag_coefficient_increment = 0.0000
-    #aerodynamics.settings.maximum_lift_coefficient   = 1.5    
-    #configs_analyses.base.append(aerodynamics)     
-
-    ## ------------------------------------------------------------------
-    ##  Energy
-    #energy = SUAVE.Analyses.Energy.Energy()
-    #energy.network = vehicle.propulsors
-    #analyses.append(energy)
-    
-    ## ------------------------------------------------------------------
-    ##  Planet Analysis
-    #planet = SUAVE.Analyses.Planets.Planet()
-    #analyses.append(planet)
-    
-    ## ------------------------------------------------------------------
-    ##  Atmosphere Analysis
-    #atmosphere = SUAVE.Analyses.Atmospheric.US_Standard_1976()
-    #atmosphere.features.planet = planet.features
-    #analyses.append(atmosphere)       
-    
+ 
+    # ------------------------------------------------------------------
+    #  Aerodynamics Analysis
+    aerodynamics                                  = SUAVE.Analyses.Aerodynamics.AERODAS()
+    aerodynamics.geometry                         = copy.deepcopy(configs.base)   
+    aerodynamics.settings.drag_coefficient_increment = 0.0000
+    aerodynamics.settings.maximum_lift_coefficient   = 1.5    
+    configs_analyses.base.append(aerodynamics)     
+  
     # ------------------------------------------------------------------        
     # mission analyses
     mission  = mission_setup(configs_analyses,vehicle)
@@ -98,23 +73,35 @@ def main():
     mission = analyses.missions.base    
     results = mission.evaluate()
 
-    # lift coefficient check
+    # RPM check during hover
+    RPM            = results.conditions.hover_1.propulsion.rpm[0] 
+    RPM_true       = 4685.21033888
+    print RPM 
+    diff_RPM                        = np.abs(RPM - RPM_true)
+    print 'RPM difference'
+    print diff_RPM
+    assert np.abs((RPM - RPM_true)/RPM_true) < 1e-3  
+    
+    # battery energy check during transition
+    battery_energy_trans_to_hover              = results.conditions.transition_to_hover.propulsion.battery_energy[0]
+    battery_energy_trans_to_hover_true         = 92097.82354179
+    print battery_energy_trans_to_hover
+    diff_battery_energy_trans_to_hover                      = np.abs(battery_energy_trans_to_hover  - battery_energy_trans_to_hover_true) 
+    print 'battery_energy_trans_to_hover difference'
+    print diff_battery_energy_trans_to_hover
+    assert np.abs((battery_energy_trans_to_hover  - battery_energy_trans_to_hover_true)/battery_energy_trans_to_hover) < 1e-3
+
+
+    # lift coefficient check during cruise
     lift_coefficient              = results.conditions.cruise.aerodynamics.lift_coefficient[0]
-    lift_coefficient_true         = 0.59495841
+    lift_coefficient_true         = 0.3527293
     print lift_coefficient
     diff_CL                       = np.abs(lift_coefficient  - lift_coefficient_true) 
     print 'CL difference'
     print diff_CL
     assert np.abs((lift_coefficient  - lift_coefficient_true)/lift_coefficient_true) < 1e-3
 
-    # moment coefficient check
-    moment_coefficient            = results.conditions.cruise.stability.static.CM[0][0]
-    moment_coefficient_true       = -0.620326644
-    print moment_coefficient
-    diff_CM                       = np.abs(moment_coefficient - moment_coefficient_true)
-    print 'CM difference'
-    print diff_CM
-    assert np.abs((moment_coefficient - moment_coefficient_true)/moment_coefficient_true) < 1e-3    
+  
 
     return
 
