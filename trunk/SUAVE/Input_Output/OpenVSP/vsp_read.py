bcf3ed14a81c343e6917c6f6a48986bb087935de
tstfrancis <theo.st.francis@gmail.com>
2018-07-26 17:28:36 -0700

    progress on readFuselage, determining what gopes in SUAVE, post processing, NACA airfoils

--- trunk/SUAVE/Input_Output/OpenVSP/vsp_read.py
+++ trunk/SUAVE/Input_Output/OpenVSP/vsp_read.py
@@ -20,7 +20,10 @@ def vsp_read(tag):
 	
 	vsp_geoms = vsp.FindGeoms()
 	
-	''' The API call for GETGEOMTYPE was not released as of 07/25/18
+	# Label each geom type. 
+	# The API call for GETGEOMTYPE was not released as of 07/25/18
+
+	'''
 	for geom in vsp_geoms:
 		if vsp.GETGEOMTYPE(str(geom)) == 'FUSELAGE':
 			vsp_fuselages.append(geom)
@@ -33,6 +36,7 @@ def vsp_read(tag):
 	vehicle = SUAVE.Vehicle()
 	vehicle.tag = tag
 	
+	# Read VSP geoms and store in SUAVE.
 	'''
 	for vsp_fuselage in vsp_fuselages:
 		fuselage_id = vsp_fuselages[vsp_fuselage]
@@ -49,6 +53,13 @@ def vsp_read(tag):
 	
 	'''
 	
+	# Post-processing
+	
+	fuselage.heights.at_quarter_length          = 3.32 * Units.meter   
+	fuselage.heights.at_wing_root_quarter_chord = 3.32 * Units.meter   
+	fuselage.heights.at_three_quarters_length   = 3.32 * Units.meter 	
+	
+	
 	return vehicle
 
 def readWing(wing_id):
@@ -58,8 +69,7 @@ def readWing(wing_id):
 	if vsp.GetGeomName( wing_id ):
 		wing.tag = vsp.GetGeomName( wing_id )
 	else: 
-		wing.tag = 'wing'
-		vsp.SetGeomName( wing_id, 'wing')
+		wing.tag = '[wing geometry]'
 	
 	wing.origin[0] = vsp.GetParmVal( wing_id, 'X_Rel_Location', 'XForm')
 	wing.origin[1] = vsp.GetParmVal( wing_id, 'Y_Rel_Location', 'XForm')
@@ -119,11 +129,23 @@ def readWing(wing_id):
 		span_sum += segment_spans[i]
 
 		# XSec airfoil			
-		xsec_id = str(vsp.GetXSec(xsec_surf_id, i))		
-		if vsp.GetXSecShape( xsec_id ) == 7:
+		xsec_id = str(vsp.GetXSec(xsec_surf_id, i))
+		airfoil = Airfoil()
+		if vsp.GetXSecShape( xsec_id ) == 7: # XSec shape: NACA 4-series
 			camber = vsp.GetParmVal( wing_id, 'Camber', 'XSecCurve_' + str(i))
+			camber_loc = vsp.GetParmVal( wing_id, 'CamberLoc', 'XSecCurve_' + str(i))
+			thick_cord = vsp.GetParmVal( wing_id, 'ThickChord', 'XSecCurve_' + str(i))
+			airfoil.thickness_to_chord = thick_cord
+			camber_round = int(camber*100)
+			camber_loc_round = int(camber_loc*10)
+			thick_cord_round = int(thick_cord*100)
+			airfoil.tag = 'NACA ' + str(camber_round) + str(camber_loc_round) + str(thick_cord_round)
+		
+		elif vsp.GetXSecShape( xsec_id ) == 8: # XSec shape: NACA 6-series
+			camber = 88888888888888888888888888888888888.
+			
+			
 		elif vsp.GetXSecShape( xsec_id ) == 12:	# XSec shape: 12 is type AF_FILE
-			airfoil = Airfoil()
 			lower = vsp.GetAirfoilLowerPnts( xsec_id )  
 			upper = vsp.GetAirfoilUpperPnts( xsec_id )
 			
@@ -143,7 +165,7 @@ def readWing(wing_id):
 			else: 
 				for jj in xrange(len(upper)-1, 0, -1):
 					airfoil.points.append( [upper[jj].x(), upper[jj].y()])			
-			segment.append_airfoil(airfoil)
+		segment.append_airfoil(airfoil)
 		
 		wing.Segments.append(segment)
 	
@@ -193,17 +215,46 @@ def readWing(wing_id):
 
 
 
-def readFuselage():
-	
-	geoms = vsp.FindGeomsWithName('')
-	print geoms
+def readFuselage( fuselage_id ):
+	# Create SUAVE fuselage.
 	fuselage = SUAVE.Components.Fuselages.Fuselage()
+	# Name fuselage.
+	if vsp.GetGeomName( fuselage_id ):
+		fuselage.tag = vsp.GetGeomName( fuselage_id )
+	else: 
+		fuselage.tag = '[fuselage geometry]'	
 	
-	#fuselage.width = vsp.GetParmVal( fuselage_id, )
+	xsec_surf_id = vsp.GetXSecSurf( fuselage_id, 0 ) # There is only one XSecSurf (I think always).
+	xsec_num = vsp.GetNumXSec( xsec_surf_id ) # Number of xsecs in fuselage.
+	xsecs_ids = []
+	for ii in xrange( 0, xsec_num ): # Store fuselage xsec ids.
+		xsecs_ids.append(vsp.GetXSec( xsec_surf_id, ii ))
+		
+		print vsp.GetXSecHeight(xsecs_ids[ii])
+		print vsp.GetXSecWidth(xsecs_ids[ii])			
+		#print vsp.GetXSecParm(xsecs_ids[ii], '')
+	
+	# get location of nose of fuse to calculate how far back everything is...like fuse height at quarter chord etc
+	# to find fineness of nose and tail: find where the cross section stays relatively the same of the fuselage
+	# then back up to before that segment, go from nose to that segment (likely 2 segments, tho maybe 3 or 4 in some cases)
+	
+	# get final length, all other lengths too, maybe in post process
+	# so just get points now, locations etc
+	# relative locations along x axis for fuselage
 	
 	
+	segment = SUAVE.Components.Lofted_Body.Segment()
 	
-	
+	section = SUAVE.Components.Lofted_Body
+	# Which ones are crucial?
+	fuselage.fineness.nose         = 4.3   * Units.meter   
+	fuselage.fineness.tail         = 6.4   * Units.meter   
+	fuselage.lengths.total         = 61.66 * Units.meter    
+	fuselage.width                 = 2.88  * Units.meter   
+	fuselage.heights.maximum       = 3.32  * Units.meter   
+	fuselage.areas.wetted          = 447. * Units['meter**2'] 
+	fuselage.areas.front_projected = 11.9 * Units['meter**2'] 
+	fuselage.effective_diameter    = 3.1 * Units.meter  
 	
 	
 	return fuselage
