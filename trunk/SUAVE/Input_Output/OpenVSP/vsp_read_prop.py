d88f86d960bf43cb5c2b5600f1a70acbd569f7b6
tstfrancis <theo.st.francis@gmail.com>
2018-08-21 11:44:54 -0700

    fix array resizing (more reliable and slightly less hideous); tabulation

--- trunk/SUAVE/Input_Output/OpenVSP/vsp_read_prop.py
+++ trunk/SUAVE/Input_Output/OpenVSP/vsp_read_prop.py
@@ -67,9 +67,9 @@ def vsp_read_prop(prop_id, units_type='SI'):
 	prop.prop_attributes.tip_radius = tip_radius
 	prop.prop_attributes.hub_radius = vsp.GetParmVal(prop_id, 'RadiusFrac', 'XSec_0') * tip_radius	
 	
-	prop.location[0] = vsp.GetParmVal(prop_id, 'X_Rel_Location', 'XForm') * units_factor
-	prop.location[1] = vsp.GetParmVal(prop_id, 'Y_Rel_Location', 'XForm') * units_factor
-	prop.location[2] = vsp.GetParmVal(prop_id, 'Z_Rel_Location', 'XForm') * units_factor
+	prop.origin[0] = vsp.GetParmVal(prop_id, 'X_Rel_Location', 'XForm') * units_factor
+	prop.origin[1] = vsp.GetParmVal(prop_id, 'Y_Rel_Location', 'XForm') * units_factor
+	prop.origin[2] = vsp.GetParmVal(prop_id, 'Z_Rel_Location', 'XForm') * units_factor
 	prop.rotation[0] = vsp.GetParmVal(prop_id, 'X_Rel_Rotation', 'XForm') * Units.deg
 	prop.rotation[1] = vsp.GetParmVal(prop_id, 'Y_Rel_Rotation', 'XForm') * Units.deg
 	prop.rotation[2] = vsp.GetParmVal(prop_id, 'Z_Rel_Rotation', 'XForm') * Units.deg
@@ -85,73 +85,73 @@ def vsp_read_prop(prop_id, units_type='SI'):
 	# -------------	
 	
 	# Chord
-	chord_curve = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Chord'))]
+	chord_curve       = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Chord'))]
 	chord_split_point = vsp.GetParmVal(prop_id, 'SplitPt', 'Chord')
-	chords = []
-	chords_rad = []  							# This is r/R value.
-	chords_num = 50  							# HARDCODED, see break below.
+	chords            = []
+	chords_rad        = []  						# This is r/R value.
+	chords_num        = 50  						# HARDCODED, see break below.
 	for ii in xrange(chords_num):						# Future API call goes for Pcurve chord number goes here.
 		chords.append(vsp.GetParmVal(prop_id, 'crd_' + str(ii), 'Chord')) * units_factor
 		chords_rad.append(vsp.GetParmVal(prop_id, 'r_' + str(ii), 'Chord'))
 		if ii!=0 and chords[ii] == 0.0 and chords[ii-1] == 0.0:		# Allows for two zero conditions before breaking, then resizes array.
-			chords.remove(chords[-1])
-			chords.remove(chords[-1])
+			chords     = chords[:-2]
+			chords_rad = chords_rad[:-2]
 			break
 	
 	# Twist
-	twist_curve = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Twist'))]
+	twist_curve       = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Twist'))]
 	twist_split_point = vsp.GetParmVal(prop_id, 'SplitPt', 'Twist')	
-	twists = []
-	twists_rad = []								# This is r/R value.
-	twists_num = 50								# HARDCODED
+	twists            = []
+	twists_rad        = []							# This is r/R value.
+	twists_num        = 50							# HARDCODED
 	for ii in xrange(twists_num):
 		twists.append(vsp.GetParmVal(prop_id, 'tw_' + str(ii), 'Twist') * Units.deg) 
 		twists_rad.append(vsp.GetParmVal(prop_id, 'r_' + str(ii), 'Twist'))		
 		if ii!=0 and twists[ii] == 0.0 and twists[ii-1] == 0.0:
-			twists.remove(twists[-1])
-			twists.remove(twists[-1])
+			twists     = twists[:-2]
+			twists_rad = twists_rad[:-2]
 			break
 	
 	# Skew
-	skew_curve = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Skew'))]
+	skew_curve       = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Skew'))]
 	skew_split_point = vsp.GetParmVal(prop_id, 'SplitPt', 'Skew')
-	skews = []
-	skews_rad = []
-	skews_num = 50								#HARDCODED
+	skews            = []
+	skews_rad        = []
+	skews_num        = 50							#HARDCODED
 	for ii in xrange(skews_num):
 		skews.append(vsp.GetParmVal(prop_id, 'skw_' + str(ii), 'Skew') * Units.deg)
 		skews_rad.append(vsp.GetParmVal(prop_id, 'r_' + str(ii), 'Skew'))	
 		if ii!=0 and skews[ii] == 0.0 and skews[ii-1] == 0.0:
-			skews.remove(skews[-1])
-			skews.remove(skews[-1])
+			skews     = skews[:-2]
+			skews_rad = skews_rad[:-2]
 			break
 	
 	# Rake
-	rake_curve = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Rake'))]
+	rake_curve       = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Rake'))]
 	rake_split_point = vsp.GetParmVal(prop_id, 'SplitPt', 'Rake')
-	rakes = []
-	rakes_rad = []	
-	rakes_num = 50								#HARDCODED
+	rakes            = []
+	rakes_rad        = []	
+	rakes_num        = 50							#HARDCODED
 	for ii in xrange(rakes_num):
 		rakes.append(vsp.GetParmVal(prop_id, 'rak_' + str(ii), 'Rake') * Units.deg)
 		rakes_rad.append(vsp.GetParmVal(prop_id, 'r_' + str(ii), 'Rake'))
 		if ii!=0 and rakes[ii] == 0.0 and rakes[ii-1] == 0.0:
-			rakes.remove(rakes[-1])
-			rakes.remove(rakes[-1])
+			rakes     = rakes[:-2]
+			rakes_rad = rakes_rad[:-2]
 			break
 		
 	# Sweep
-	sweep_curve = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Sweep'))]
+	sweep_curve       = curve_type[int(vsp.GetParmVal(prop_id, 'CrvType', 'Sweep'))]
 	sweep_split_point = vsp.GetParmVal(prop_id, 'SplitPt', 'Sweep')
-	sweeps = []
-	sweeps_rad = []
-	sweeps_num = 50								#HARDCODED
+	sweeps            = []
+	sweeps_rad        = []
+	sweeps_num        = 50							#HARDCODED
 	for ii in xrange(sweeps_num):
 		sweeps.append(vsp.GetParmVal(prop_id, 'sw_' + str(ii), 'Sweep') * Units.deg)
 		sweeps_rad.append(vsp.GetParmVal(prop_id, 'r_' + str(ii), 'Sweep'))	
 		if ii!=0 and sweeps[ii] == 0.0 and sweeps[ii-1] == 0.0:
-			sweeps.remove(sweeps[-1])
-			sweeps.remove(sweeps[-1])
+			sweeps     = sweeps[:-2]
+			sweeps_rad = sweeps_rad[:-2]			
 			break
 		
 	return prop
