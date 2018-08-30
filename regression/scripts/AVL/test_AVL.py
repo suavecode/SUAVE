fa4587b035ca57ad596f169d0287c7618b5cd4a2
Emilio Botero <ebotero@stanford.edu>
2018-08-30 14:54:47 -0700

    fixing avl regression data structure

--- regression/scripts/AVL/test_AVL.py
+++ regression/scripts/AVL/test_AVL.py
@@ -92,7 +92,7 @@ def main():
     assert np.abs((lift_coefficient  - lift_coefficient_true)/lift_coefficient_true) < 1e-3
     
     # moment coefficient check
-    moment_coefficient            = results.segments.cruise.conditions.cruise.stability.static.CM[0][0]
+    moment_coefficient            = results.segments.cruise.conditions.stability.static.CM[0][0]
     moment_coefficient_true       = -0.620326644
     print(moment_coefficient)
     diff_CM                       = np.abs(moment_coefficient - moment_coefficient_true)
