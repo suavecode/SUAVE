ad00c96516c48f3be8379d055fd97ea6b8bd35cb
Tim MacDonald <timdmacdo@gmail.com>
2019-02-13 15:51:31 -0800

    doxygen header updates for weight buildup

--- trunk/SUAVE/Methods/Weights/Buildups/Electric_Tiltrotor/__init__.py
+++ trunk/SUAVE/Methods/Weights/Buildups/Electric_Tiltrotor/__init__.py
@@ -1,9 +1,7 @@
 ## @defgroup Methods-Weights-Buildups-Electric_Tiltrotor Electric_Tiltrotor
-
-"""SUAVE.Methods.Weights.Buildups.Electric_Tiltrotor
-   contains methods and attributes used for estimating
-   electric helicopter weights on a component-by-component
-   basis.
-"""
+# contains methods and attributes used for estimating
+# electric helicopter weights on a component-by-component
+# basis.
+# @ingroup Methods-Weights-Buildups
 
 from .empty import empty
