cf5d6f683e2a0bd940e7d04e69672bc2af8e652e
planes <ebotero@stanford.edu>
2019-02-22 21:36:33 -0800

    updated regressions

--- regression/scripts/noise_optimization/Noise_Test.py
+++ regression/scripts/noise_optimization/Noise_Test.py
@@ -45,7 +45,7 @@ def main():
     noise_cumulative_margin = objectives[0]
     
     actual = Data()    
-    actual.noise_cumulative_margin = 19.782360230751067
+    actual.noise_cumulative_margin = 19.793817329395267
 
 
     error = Data()
