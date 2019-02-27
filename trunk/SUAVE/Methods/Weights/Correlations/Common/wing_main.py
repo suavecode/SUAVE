b82db0a08ba9db23a5856712f4e2f9c8f748c513
planes <ebotero@stanford.edu>
2019-02-27 10:40:34 -0800

    good checkpoint for changing units

--- trunk/SUAVE/Methods/Weights/Correlations/Common/wing_main.py
+++ trunk/SUAVE/Methods/Weights/Correlations/Common/wing_main.py
@@ -59,14 +59,16 @@ def wing_main(wing,Nult,TOW,wt_zf,rho,sigma):
     area  = wing.areas.reference
     t_c_w = wing.thickness_to_chord
     
-    #rho_sigma = rho/sigma
-    rho_sigma = (1.642*10.**-6.)
+    rho_sigma = rho/sigma
+    #rho_sigma = (1.642*10.**-6.)
+    
+    
+    # Start the calculations
+    l_tot = Nult*np.sqrt(TOW*wt_zf )*9.81
+    gamma = 16*l_tot*rho_sigma/(np.pi*span)    
     
     if len(wing.Segments)>0:
-        
-        # Start the calculations
-        l_tot = Nult*np.sqrt(TOW*wt_zf )*9.81
-        gamma = 16*l_tot*rho_sigma/(np.pi*span)
+
         
         # Prime some numbers
         RC = wing.chords.root
@@ -76,30 +78,30 @@ def wing_main(wing,Nult,TOW,wt_zf,rho,sigma):
         for i in range(1,len(wing.Segments)):
             C  = wing.Segments[i-1].root_chord_percent*RC
             D  = wing.Segments[i].root_chord_percent*RC
-            E  = wing.Segments[i].percent_span_location - wing.Segments[i-1].percent_span_location
+            E  = wing.Segments[i].percent_span_location/2 - wing.Segments[i-1].percent_span_location/2
             F  = wing.Segments[i-1].thickness_to_chord
             G  = wing.Segments[i].thickness_to_chord
-            H  = wing.Segments[i-1].percent_span_location
-            J  = wing.Segments[i].percent_span_location
+            H  = wing.Segments[i-1].percent_span_location/2
+            J  = wing.Segments[i].percent_span_location/2
             L  = (C-D)/E
             M  = (G-F)/E
             
-            Y1 = wing.Segments[i-1].percent_span_location
-            Y2 = wing.Segments[i].percent_span_location
-            
+            Y1 = wing.Segments[i-1].percent_span_location/2
+            Y2 = wing.Segments[i].percent_span_location/2
             
             if C==D and F==G:
 
-                WB1 = 1/(G*C)* (Y1*(H**2)/8 - Y1**3/6 )
-                WB2 = 1/(G*C)* (Y2*(H**2)/8 - Y2**3/6 )
+                WB1 = 1/(G*C)* (Y1/8 - Y1**3/6 )
+                WB2 = 1/(G*C)* (Y2/8 - Y2**3/6 )
                               
-                
+            
+            # I'm pretty sure the H^2 term is not correct. So only the top equation is right
             elif C!=D and F==G:
 
-                WB1 = (1/(G)) * ((4*D**2 + 8*D*J*L - L**2 *(H**2 - 4*J**2))*np.log(D + J*L - L*Y1) + \
-                                 2*L*Y1 *(2*D + L*(2*J + Y1)))/(8*L**3)
-                WB2 = (1/(G)) * ((4*D**2 + 8*D*J*L - L**2 *(H**2 - 4*J**2))*np.log(D + J*L - L*Y2) + \
-                                 2*L*Y2 *(2*D + L*(2*J + Y2)))/(8*L**3)
+                WB1 = (1/(G)) * ((4*C**2 + 8*C*H*L + (4*H**2 - 1)*L**2)*np.log(C + H*L - L*Y1) +\
+                                 2*L*Y1*(2*C + L*(2*H + Y1)))/(8*L**3)
+                WB2 = (1/(G)) * ((4*C**2 + 8*C*H*L + (4*H**2 - 1)*L**2)*np.log(C + H*L - L*Y2) +\
+                                 2*L*Y2*(2*C + L*(2*H + Y2)))/(8*L**3)
   
             elif C==D and F!=G:
                 
@@ -117,7 +119,7 @@ def wing_main(wing,Nult,TOW,wt_zf,rho,sigma):
                         L*(4*M*Y2*(F*L - C*M) + L*(4*F**2 + 8*F*H*M + 3*H**2 \
                                                    *M**2)*np.log(F + H*M - M*Y2)))/(8*L**2 *M**2 *(F*L - C*M))
                 
-            run_sum += -(WB2-WB1)
+            run_sum += (WB2-WB1)
             
         run_sum = run_sum*b**2 # This b^2 is because the Y is non dimensional
             
@@ -135,6 +137,7 @@ def wing_main(wing,Nult,TOW,wt_zf,rho,sigma):
         #Calculate weight of wing for traditional aircraft wing
         weight = 4.22*area + 1.642*10.**-6. * Nult*(span)**3. *(mtow*zfw)**0.5 \
                  * (1.+2.*taper)/(t_c_w*(np.cos(sweep))**2. * area*(1.+taper) )
+
              
     
     weight = weight * Units.lb # Convert lb to kg
