f7ff2fd4fd818e434dd60305f091f587bb5fa054
Tim MacDonald <timdmacdo@gmail.com>
2019-04-08 11:05:35 -0700

    added forgotten init file

--- trunk/SUAVE/Methods/Aerodynamics/Supersonic_Zero/Drag/__init__.py
+++ trunk/SUAVE/Methods/Aerodynamics/Supersonic_Zero/Drag/__init__.py
@@ -8,6 +8,5 @@ from .Cubic_Spline_Blender import Cubic_Spline_Blender
 from .parasite_drag_propulsor import parasite_drag_propulsor
 from .wave_drag_lift import wave_drag_lift
 from .parasite_drag_fuselage import parasite_drag_fuselage
-from .parasite_drag_wing import parasite_drag_wing
 from .induced_drag_aircraft import induced_drag_aircraft
 from .miscellaneous_drag_aircraft import miscellaneous_drag_aircraft
