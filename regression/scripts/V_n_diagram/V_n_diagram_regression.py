e6d55270eb4aac36b53991e72bbc10785ae6486b
stanislav <stankarpuk93@gmail.com>
2019-04-14 15:47:58 -0700

    fixed regression errors

--- regression/scripts/V_n_diagram/V_n_diagram_regression.py
+++ regression/scripts/V_n_diagram/V_n_diagram_regression.py
@@ -31,9 +31,6 @@ from  DHC6_TwinOtter_wing_only import vehicle_setup as vehicle_setup_DHC6_TwinOt
 from   HAIG_Y12_wing_only   import vehicle_setup as vehicle_setup_HAIG_Y12
 from Pilatus_PC12_wing_only import vehicle_setup as vehicle_setup_Pilatus_PC12
 
-# Part 25
-from Boeing_737   import vehicle_setup as vehicle_setup_Boeing_B737
-
 def main():
 
     analyses = SUAVE.Analyses.Vehicle()
@@ -65,16 +62,16 @@ def main():
 
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 57.923
-    actual.Vs1_neg                  = 57.923
-    actual.Va_pos                   = 173.769
-    actual.Va_neg                   = 153.250
-    actual.Vc                       = 199.834
-    actual.Vd                       = 229.8
+    actual.Vs1_pos                  = 57.93213
+    actual.Vs1_neg                  = 57.93213
+    actual.Va_pos                   = 173.79638
+    actual.Va_neg                   = 153.274
+    actual.Vc                       = 199.86584
+    actual.Vd                       = 229.84571
     actual.limit_load_pos           = 9
     actual.limit_load_neg           = -7
     actual.dive_limit_load_pos      = 9
-    actual.dive_limit_load_neg      = -1.3149 
+    actual.dive_limit_load_neg      = -1.313253
     
     # error calculations
     error                         = Data()
@@ -88,12 +85,7 @@ def main():
     error.limit_load_neg          = (actual.limit_load_neg - V_n_data.limit_loads.negative)/actual.limit_load_neg
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
-        
-    print('Results')
-    print(V_n_data)
     
-    print('Relative Errors')
-    print(error)  
       
     for k,v in error.items():
         assert(np.abs(v)<1E-6)    
@@ -107,16 +99,16 @@ def main():
     
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 58.15307
-    actual.Vs1_neg                  = 58.15307
-    actual.Va_pos                   = 142.4454
-    actual.Va_neg                   = 100.7241
-    actual.Vc                       = 163.8122
-    actual.Vd                       = 211.9856
+    actual.Vs1_pos                  = 58.1772
+    actual.Vs1_neg                  = 58.1772
+    actual.Va_pos                   = 142.5044
+    actual.Va_neg                   = 100.7658
+    actual.Vc                       = 163.8801
+    actual.Vd                       = 212.0243
     actual.limit_load_pos           = 6
     actual.limit_load_neg           = -3
     actual.dive_limit_load_pos      = 6
-    actual.dive_limit_load_neg      = -1.12414
+    actual.dive_limit_load_neg      = -1.126836
     
     # error calculations
     error                         = Data()
@@ -131,11 +123,6 @@ def main():
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg 
     
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error) 
       
     for k,v in error.items():
         assert(np.abs(v)<1E-6) 
@@ -150,19 +137,19 @@ def main():
     vehicle  = vehicle_setup_SR22()    
     weight   = vehicle.mass_properties.max_takeoff
     V_n_data = V_n_diagram(vehicle,analyses,weight,altitude,delta_ISA)
-    
+
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 45.469
-    actual.Vs1_neg                  = 54.752
-    actual.Va_pos                   = 88.636
-    actual.Va_neg                   = 67.503
-    actual.Vc                       = 105.837
-    actual.Vd                       = 147.217
-    actual.limit_load_pos           = 4.535
-    actual.limit_load_neg           = -2.535
+    actual.Vs1_pos                  = 45.47889
+    actual.Vs1_neg                  = 54.76384
+    actual.Va_pos                   = 88.65468
+    actual.Va_neg                   = 67.51739
+    actual.Vc                       = 105.8268
+    actual.Vd                       = 147.2140
+    actual.limit_load_pos           = 4.53441
+    actual.limit_load_neg           = -2.534413
     actual.dive_limit_load_pos      = 3.8
-    actual.dive_limit_load_neg      = -1.458
+    actual.dive_limit_load_neg      = -1.458333
     
     # error calculations
     error                         = Data()
@@ -177,11 +164,6 @@ def main():
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
         
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error) 
       
     for k,v in error.items():
         assert(np.abs(v)<1E-6)    
@@ -195,16 +177,16 @@ def main():
     
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 72.333
-    actual.Vs1_neg                  = 76.8585
-    actual.Va_pos                   = 140.5138
-    actual.Va_neg                   = 94.42832
-    actual.Vc                       = 165.3697
-    actual.Vd                       = 227.7445
-    actual.limit_load_pos           = 3.774
-    actual.limit_load_neg           = -1.5095
-    actual.dive_limit_load_pos      = 3.8
-    actual.dive_limit_load_neg      = -0.46376
+    actual.Vs1_pos                  = 72.3381
+    actual.Vs1_neg                  = 76.8636
+    actual.Va_pos                   = 140.52111
+    actual.Va_neg                   = 94.433205
+    actual.Vc                       = 165.35441
+    actual.Vd                       = 227.75333
+    actual.limit_load_pos           = 3.7735348
+    actual.limit_load_neg           = -1.509414
+    actual.dive_limit_load_pos      = 3.7735348
+    actual.dive_limit_load_neg      = -0.4633023
     
     # error calculations
     error                         = Data()
@@ -219,11 +201,6 @@ def main():
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
         
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error)  
       
     for k,v in error.items():
         assert(np.abs(v)<1E-6)    
@@ -241,16 +218,16 @@ def main():
     
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 84.0859
-    actual.Vs1_neg                  = 89.3462
-    actual.Va_pos                   = 155.9128
-    actual.Va_neg                   = 109.4263
-    actual.Vc                       = 198.4436
-    actual.Vd                       = 259.8548
-    actual.limit_load_pos           = 3.438
+    actual.Vs1_pos                  = 84.3000
+    actual.Vs1_neg                  = 89.21476
+    actual.Va_pos                   = 156.2416
+    actual.Va_neg                   = 109.2653
+    actual.Vc                       = 198.4253
+    actual.Vd                       = 260.4157
+    actual.limit_load_pos           = 3.435089
     actual.limit_load_neg           = -1.5
-    actual.dive_limit_load_pos      = 3.438
-    actual.dive_limit_load_neg      = -0.3728
+    actual.dive_limit_load_pos      = 3.435089
+    actual.dive_limit_load_neg      = -0.3706622
     
     # error calculations
     error                         = Data()
@@ -265,11 +242,6 @@ def main():
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
        
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error)  
       
     for k,v in error.items():
         assert(np.abs(v)<1E-6)    
@@ -284,16 +256,16 @@ def main():
     
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 79.2398
-    actual.Vs1_neg                  = 84.1970
-    actual.Va_pos                   = 141.008
-    actual.Va_neg                   = 94.7606
-    actual.Vc                       = 198.444
-    actual.Vd                       = 246.861
-    actual.limit_load_pos           = 3.5076
-    actual.limit_load_neg           = -1.5076
-    actual.dive_limit_load_pos      = 3.1667
-    actual.dive_limit_load_neg      = -0.5597
+    actual.Vs1_pos                  = 79.2618
+    actual.Vs1_neg                  = 84.2203
+    actual.Va_pos                   = 141.0472
+    actual.Va_neg                   = 94.7867
+    actual.Vc                       = 198.4253
+    actual.Vd                       = 246.9155
+    actual.limit_load_pos           = 3.5066162
+    actual.limit_load_neg           = -1.506615
+    actual.dive_limit_load_pos      = 3.166656
+    actual.dive_limit_load_neg      = -0.559585
     
     # error calculations
     error                         = Data()
@@ -307,12 +279,7 @@ def main():
     error.limit_load_neg          = (actual.limit_load_neg - V_n_data.limit_loads.negative)/actual.limit_load_neg
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
-       
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error)  
+        
       
     for k,v in error.items():
         assert(np.abs(v)<1E-6)   
@@ -331,12 +298,12 @@ def main():
     
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 81.4823
-    actual.Vs1_neg                  = 86.5798
-    actual.Va_pos                   = 170.919
-    actual.Va_neg                   = 114.861
-    actual.Vc                       = 198.444
-    actual.Vd                       = 268.470
+    actual.Vs1_pos                  = 81.4788
+    actual.Vs1_neg                  = 86.5761
+    actual.Va_pos                   = 170.9114
+    actual.Va_neg                   = 114.8561
+    actual.Vc                       = 198.4253
+    actual.Vd                       = 268.4589
     actual.limit_load_pos           = 4.4
     actual.limit_load_neg           = -1.76
     actual.dive_limit_load_pos      = 4.4
@@ -355,11 +322,6 @@ def main():
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
     
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error)  
       
     for k,v in error.items():
         assert(np.abs(v)<1E-6)    
@@ -373,12 +335,12 @@ def main():
     
     # regression values    
     actual                          = Data()
-    actual.Vs1_pos                  = 89.29371
-    actual.Vs1_neg                  = 94.87986
-    actual.Va_pos                   = 187.3041
-    actual.Va_neg                   = 125.8724
-    actual.Vc                       = 231.7575
-    actual.Vd                       = 288.712
+    actual.Vs1_pos                  = 89.2905
+    actual.Vs1_neg                  = 94.8765
+    actual.Va_pos                   = 187.2974
+    actual.Va_neg                   = 125.8679
+    actual.Vc                       = 231.4961
+    actual.Vd                       = 288.7028
     actual.limit_load_pos           = 4.4
     actual.limit_load_neg           = -1.76
     actual.dive_limit_load_pos      = 4.4
@@ -396,61 +358,11 @@ def main():
     error.limit_load_neg          = (actual.limit_load_neg - V_n_data.limit_loads.negative)/actual.limit_load_neg
     error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
     error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
-    
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error)  
+      
       
     for k,v in error.items():
-        assert(np.abs(v)<1E-6)    
-    #--------------------------------------------------------------------------------------------------------------------
+        assert(np.abs(v)<1E-6)  
 
-    # Part 25 aircraft
-    #--------------------------------------------------------------------------
-
-    # Boeing B737
-    #---------------------------------------------
-    vehicle  = vehicle_setup_Boeing_B737()    
-    weight   = vehicle.mass_properties.max_takeoff
-    V_n_data = V_n_diagram(vehicle,analyses,weight,altitude,delta_ISA)
-    
-    # regression values    
-    actual                          = Data()
-    actual.Vs1_pos                  = 159.0879
-    actual.Vs1_neg                  = 195.6588
-    actual.Va_pos                   = 251.5401
-    actual.Va_neg                   = 239.6321
-    actual.Vc                       = 519.2608
-    actual.Vd                       = 649.076
-    actual.limit_load_pos           = 3.2668
-    actual.limit_load_neg           = -1.5
-    actual.dive_limit_load_pos      = 2.5
-    actual.dive_limit_load_neg      = -0.41675
-    
-    # error calculations
-    error                         = Data()
-    error.Vs1_pos                 = (actual.Vs1_pos - V_n_data.Vs1.positive)/actual.Vs1_pos
-    error.Vs1_neg                 = (actual.Vs1_neg - V_n_data.Vs1.negative)/actual.Vs1_neg
-    error.Va_pos                  = (actual.Va_pos - V_n_data.Va.positive)/actual.Va_pos
-    error.Va_neg                  = (actual.Va_neg - V_n_data.Va.negative)/actual.Va_neg
-    error.Vc                      = (actual.Vc - V_n_data.Vc)/actual.Vc
-    error.Vd                      = (actual.Vd - V_n_data.Vd)/actual.Vd
-    error.limit_load_pos          = (actual.limit_load_pos - V_n_data.limit_loads.positive)/actual.limit_load_pos
-    error.limit_load_neg          = (actual.limit_load_neg - V_n_data.limit_loads.negative)/actual.limit_load_neg
-    error.dive_limit_load_pos     = (actual.dive_limit_load_pos - V_n_data.limit_loads.dive.positive)/actual.dive_limit_load_pos
-    error.dive_limit_load_neg     = (actual.dive_limit_load_neg - V_n_data.limit_loads.dive.negative)/actual.dive_limit_load_neg
-    
-    print('Results')
-    print(V_n_data)
-    
-    print('Relative Errors')
-    print(error) 
-      
-    for k,v in error.items():
-        assert(np.abs(v)<1E-6)   
-    return
 
 # ----------------------------------------------------------------------        
 #   Call Main
