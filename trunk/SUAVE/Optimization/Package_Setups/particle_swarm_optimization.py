a1cf6657483b5f413cb3d5b7ffd86a50cdf0a4eb
mclarke2 <mclarke2@stanford.edu>
2019-10-10 19:36:37 -0400

    standardizing docstring

--- trunk/SUAVE/Optimization/Package_Setups/particle_swarm_optimization.py
+++ trunk/SUAVE/Optimization/Package_Setups/particle_swarm_optimization.py
@@ -17,59 +17,37 @@ def particle_swarm_optimization(func, lb, ub, ieqcons=[], f_ieqcons=None, args=(
         minstep=1e-8, minfunc=1e-8, debug=False):
     """
     This function perform a particle swarm optimization (PSO)
+    
+    Source:
+        Pyswarm: https://github.com/tisimst/pyswarm
+          
+    Inputs: 
+        func      : The function to be minimized                                                                    [function] 
+        lb        : The lower bounds of the design variable(s)                                                      [array] 
+        ub        :  The upper bounds of the design variable(s)                                                     [array]
+                                                                                                                    
+        ieqcons   : A list of functions of length n such that ieqcons[j](x,*args)                                   
+                    >= 0.0 in a successfully optimized problem (Default: [])                                        [list]
+        f_ieqcons : Returns a 1-D array in which each element must be greater or equal                              
+                    to 0.0 in a successfully optimized problem. If f_ieqcons is specified,                          
+                    ieqcons is ignored (Default: None)                                                              [function]
+        args      : Additional arguments passed to objective and constraint functions                               [tuple]                       
+        kwargs    : Additional keyword arguments passed to objective and constraint functions                       [dict] 
+        swarmsize : The number of particles in the swarm (Default: 100)                                             [int] 
+        omega     : Particle velocity scaling factor (Default: 0.5)                                                 [float] 
+        phip      : Scaling factor to search away from the particle's best known position (Default: 0.5)            [scalar] 
+        phig      : Scaling factor to search away from the swarm's best known position (Default: 0.5)               [scalar]
+        maxiter   : The maximum number of iterations for the swarm to search (Default: 100) [int]                   
+        minstep   : The minimum stepsize of swarm's best position before the search terminates (Default: 1e-8)      [scalar]
+        minfunc   : The minimum change of swarm's best objective value before the search terminates (Default: 1e-8) [scalar]
+        debug     : If True, progress statements will be displayed every iteration (Default: False)                 [boolean]
    
-    Parameters
-    ==========
-    func : function
-        The function to be minimized
-    lb : array
-        The lower bounds of the design variable(s)
-    ub : array
-        The upper bounds of the design variable(s)
-   
-    Optional
-    ========
-    ieqcons : list
-        A list of functions of length n such that ieqcons[j](x,*args) >= 0.0 in 
-        a successfully optimized problem (Default: [])
-    f_ieqcons : function
-        Returns a 1-D array in which each element must be greater or equal 
-        to 0.0 in a successfully optimized problem. If f_ieqcons is specified, 
-        ieqcons is ignored (Default: None)
-    args : tuple
-        Additional arguments passed to objective and constraint functions
-        (Default: empty tuple)
-    kwargs : dict
-        Additional keyword arguments passed to objective and constraint 
-        functions (Default: empty dict)
-    swarmsize : int
-        The number of particles in the swarm (Default: 100)
-    omega : scalar
-        Particle velocity scaling factor (Default: 0.5)
-    phip : scalar
-        Scaling factor to search away from the particle's best known position
-        (Default: 0.5)
-    phig : scalar
-        Scaling factor to search away from the swarm's best known position
-        (Default: 0.5)
-    maxiter : int
-        The maximum number of iterations for the swarm to search (Default: 100)
-    minstep : scalar
-        The minimum stepsize of swarm's best position before the search
-        terminates (Default: 1e-8)
-    minfunc : scalar
-        The minimum change of swarm's best objective value before the search
-        terminates (Default: 1e-8)
-    debug : boolean
-        If True, progress statements will be displayed every iteration
-        (Default: False)
-   
-    Returns
-    =======
-    g : array
-        The swarm's best known position (optimal design)
-    f : scalar
-        The objective value at ``g``
+    Outputs:
+        g         : The swarm's best known position (optimal design)                                                [list] 
+        f         : The objective value at ``g``                                                                    [float]
+         
+    Properties Used:
+        None
    
     """
    
