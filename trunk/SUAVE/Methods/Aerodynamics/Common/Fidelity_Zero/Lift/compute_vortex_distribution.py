f6c372ab0a6a23dffc349dc32b70f0f799d3c346
planes <ebotero@stanford.edu>
2019-11-08 12:15:32 -0800

    updating from vlm aero

--- trunk/SUAVE/Methods/Aerodynamics/Common/Fidelity_Zero/Lift/compute_vortex_distribution.py
+++ trunk/SUAVE/Methods/Aerodynamics/Common/Fidelity_Zero/Lift/compute_vortex_distribution.py
@@ -77,7 +77,7 @@ def compute_vortex_distribution(geometry,settings):
         sym_para    = wing.symmetric
         Sref        = wing.areas.reference
         vertical_wing = wing.vertical
-        wing_origin = wing.origin[0]
+        wing_origin = wing.origin
 
         # determine of vehicle has symmetry 
         if sym_para is True :
