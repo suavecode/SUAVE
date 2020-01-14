a556d8a4174ab34e198adf2464bd835195aec7a8
stanislav <stankarpuk93@gmail.com>
2020-01-14 13:10:55 -0800

    Creates airfoils from CST files

---
+++ trunk/SUAVE/Methods/Geometry/Two_Dimensional/Cross_Section/Airfoil/CST/__init__.py
@@ -0,0 +1,4 @@
+## @defgroup Methods-Geometry-Two_Dimensional-Cross_Section-Airfoil Airfoil
+# Geometry functions for two dimensional airfoils.
+# @ingroup Methods-Geometry-Two_Dimensional-Cross_Section
+from .airfoil_from_CST import airfoil_from_CST
--- trunk/SUAVE/Methods/Geometry/Two_Dimensional/Cross_Section/Airfoil/__init__.py
+++ trunk/SUAVE/Methods/Geometry/Two_Dimensional/Cross_Section/Airfoil/__init__.py
@@ -1,5 +1,7 @@
 ## @defgroup Methods-Geometry-Two_Dimensional-Cross_Section-Airfoil Airfoil
 # Geometry functions for two dimensional airfoils.
 # @ingroup Methods-Geometry-Two_Dimensional-Cross_Section
+from . import CST
 from .compute_naca_4series import compute_naca_4series
 from .import_airfoil_dat import import_airfoil_dat
+from .import_airfoil_dat_Xfoil import import_airfoil_dat_Xfoil
