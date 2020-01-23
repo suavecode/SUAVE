1bb74d9e7e65f0c6ad7c60e5d20046b1c57007f1
KHamRRI <mbp@MBPs-MacBook-Pro.local>
2020-01-24 12:10:27 +1300

    Add Cryogen Tanks

---
+++ trunk/SUAVE/Components/Energy/Storages/Cryogen_Tanks/__init__.py
@@ -0,0 +1,5 @@
+## @defgroup Components-Energy-Storages-Cryogen_Tanks Cryogen_Tanks
+# Contains different cryogen tank types
+# @ingroup Components-Energy-Storages
+
+from .Cryogen_Tank import Cryogen_Tank
