9b9aa36f8f68f904393a22914e0ab667cd6d9f0c
mclarke2 <mclarke2@stanford.edu>
2020-02-03 23:31:02 -0800

    fixing silly typo in automatic regression

--- regression/automatic_regression.py
+++ regression/automatic_regression.py
@@ -84,7 +84,7 @@ modules = [
     'scripts/test_input_output/test_freemind_write.py',    
     'scripts/variable_cruise_distance/variable_cruise_distance.py',
     'scripts/weights/weights.py',   
-    'scripts/VTOL/test_VTOLs.py',  
+    'scripts/VTOL/test_VTOL.py',  
     'scripts/V_n_diagram/V_n_diagram_regression.py', 
 ]
 
