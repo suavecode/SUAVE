b4071ed0b842671e92438f39927afe2dbee1f583
mclarke2 <mclarke2@stanford.edu>
2020-02-24 23:31:47 -0800

    new Li battery

--- trunk/SUAVE/Components/Energy/Storages/Batteries/Constant_Mass/Lithium_Ion_LiNiMnCoO2_18650.py
+++ trunk/SUAVE/Components/Energy/Storages/Batteries/Constant_Mass/Lithium_Ion_LiNiMnCoO2_18650.py
@@ -17,19 +17,22 @@ class Lithium_Ion_LiNiMnCoO2_18650(Battery):
     Text 
     """
     def __defaults__(self):
-        self.cell                        = Data() 
+        self.tag                         = 'LiNiMnCoO2' 
         self.amp_hour_rating             = 3
         self.nominal_voltage             = 3.6
         self.watt_hour_rating            = self.amp_hour_rating * self.nominal_voltage
         self.mass_properties.mass        = 0.048 * Units.kg
-        self.cell.mass                   = 0.048 * Units.kg
         self.specific_energy             = self.watt_hour_rating*Units.Wh/self.mass_properties.mass  # J/kg
         self.specific_power              = 1.      *Units.kW/Units.kg #  self.specific_energy/amp_hour_rating               
-        self.resistance                  = 0.025
-        self.cell.specific_heat_capacity = 1800. #1040  #Thermal properties of lithium-ion battery and components 950 https://pdfs.semanticscholar.org/6e93/0e0c4dc0cb256d8ae0aa85cacc2c383efc08.pdf  
+        self.resistance                  = 0.025 
         self.specific_heat_capacity      = 1800. 
         self.heat_transfer_coefficient   = 20.   #  Determination of the optimum heat transfer coefficient and temperature rise analysis for a lithium-ion battery under the conditions of Harbin city bus driving cycles. Energies, 10(11). https://doi.org/10.3390/en10111723
+        
+        self.cell                        = Data() 
+        self.cell.mass                   = 0.048 * Units.kg
+        self.cell.specific_heat_capacity = 1800. #1040  #Thermal properties of lithium-ion battery and components 950 https://pdfs.semanticscholar.org/6e93/0e0c4dc0cb256d8ae0aa85cacc2c383efc08.pdf  
         self.cell.surface_area           = 4.18E-3 
+        
         self.discharge_performance_map   = discharge_performance_map()
         
         return 
