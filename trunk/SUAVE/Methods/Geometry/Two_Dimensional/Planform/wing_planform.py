c3c04f774fa6caf85b53d68bcc6e9b61a4b976c7
Tim MacDonald <timdmacdo@gmail.com>
2020-03-10 09:18:35 -0700

    wing planform functions

--- trunk/SUAVE/Methods/Geometry/Two_Dimensional/Planform/wing_planform.py
+++ trunk/SUAVE/Methods/Geometry/Two_Dimensional/Planform/wing_planform.py
@@ -8,19 +8,17 @@
 # ----------------------------------------------------------------------
 
 import numpy as np
+from SUAVE.Methods.Flight_Dynamics.Static_Stability.Approximations.Supporting_Functions import convert_sweep
 
 # ----------------------------------------------------------------------
 #  Methods
 # ----------------------------------------------------------------------
 def wing_planform(wing):
     """Computes standard wing planform values.
-
     Assumptions:
     Trapezoidal wing with no leading/trailing edge extensions
-
     Source:
     None
-
     Inputs:
     wing.
       areas.reference          [m^2]
@@ -37,12 +35,12 @@ def wing_planform(wing):
         span_start             [-]       Span start position (.1 is 10% span)
         span_end               [-]       Span end position (.1 is 10% span)
         chord                  [-]       Portion of wing chord used (.1 is 10% chord)
-
     Outputs:
     wing.
       chords.root              [m]
       chords.tip               [m]
       chords.mean_aerodynamics [m]
+      chords.mean_geometric    [m]
       areas.wetted             [m^2]
       areas.affected           [m^2]
       spans.projected          [m]
@@ -50,7 +48,6 @@ def wing_planform(wing):
       flaps.chord_dimensional  [m]
       flaps.area               [m^2]
         
-
     Properties Used:
     N/A
     """      
@@ -70,6 +67,7 @@ def wing_planform(wing):
     span       = (ar*sref)**.5
     chord_root = 2*sref/span/(1+taper)
     chord_tip  = taper * chord_root
+    mgc        = (chord_root+chord_tip)/2
     
     swet = 2.*span/2.*(chord_root+chord_tip) *  (1.0 + 0.2*t_c_w)
 
@@ -90,34 +88,40 @@ def wing_planform(wing):
 
     if symmetric:
         y_coord = 0    
+    
+    # Total length calculation
+    total_length = np.tan(le_sweep)*span/2. + chord_tip
         
     # Computing flap geometry
     affected_area = 0.
     if wing.high_lift:
-        flap = wing.control_surfaces.flap
+        flap = wing.flaps
         #compute wing chords at flap start and end
         delta_chord = chord_tip - chord_root
         
-        wing_chord_flap_start = chord_root + delta_chord * flap.span_fraction_start 
-        wing_chord_flap_end   = chord_root + delta_chord * flap.span_fraction_end
+        wing_chord_flap_start = chord_root + delta_chord * flap.span_start 
+        wing_chord_flap_end   = chord_root + delta_chord * flap.span_end  
         wing_mac_flap = 2./3.*( wing_chord_flap_start+wing_chord_flap_end - \
                                 wing_chord_flap_start*wing_chord_flap_end/  \
                                 (wing_chord_flap_start+wing_chord_flap_end) )
         
-        flap.chord_dimensional = wing_mac_flap * flap.chord_fraction
-        flap_chord_start        = wing_chord_flap_start * flap.chord_fraction
-        flap_chord_end          = wing_chord_flap_end * flap.chord_fraction
-        flap.area               = (flap_chord_start + flap_chord_end) * (flap.span_fraction_end- flap.span_fraction_start)*span / 2.    
-        affected_area           = (wing_chord_flap_start + wing_chord_flap_end) * (flap.span_fraction_end- flap.span_fraction_start)*span / 2.          
-         
+        flap.chord_dimensional = wing_mac_flap * flap.chord
+        flap_chord_start = wing_chord_flap_start * flap.chord
+        flap_chord_end   = wing_chord_flap_end * flap.chord
+        flap.area        = (flap_chord_start + flap_chord_end) * (flap.span_end - flap.span_start)*span / 2.    
+        affected_area    = (wing_chord_flap_start + wing_chord_flap_end) * (flap.span_end - flap.span_start)*span / 2.          
+        
     # update
     wing.chords.root                = chord_root
     wing.chords.tip                 = chord_tip
     wing.chords.mean_aerodynamic    = mac
+    wing.chords.mean_geometric      = mgc
+    wing.sweeps.leading_edge        = le_sweep
     wing.areas.wetted               = swet
     wing.areas.affected             = affected_area
     wing.spans.projected            = span
     wing.aerodynamic_center         = [x_coord , y_coord, z_coord]
+    wing.total_length               = total_length
     
     return wing
 
