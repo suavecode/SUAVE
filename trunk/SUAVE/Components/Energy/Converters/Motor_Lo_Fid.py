2fc113c69bfaf469970eb0d18915bf404dae0a87
planes <ebotero@stanford.edu>
2020-03-21 14:06:13 -0700

    comments

--- trunk/SUAVE/Components/Energy/Converters/Motor_Lo_Fid.py
+++ trunk/SUAVE/Components/Energy/Converters/Motor_Lo_Fid.py
@@ -102,7 +102,11 @@ class Motor_Lo_Fid(Energy_Component):
 
         # Omega
         omega1 = (Kv*v)/2. + (Kv*(Res*Res*io*io - 2.*Res*etam*io*v - 2.*Res*io*v + etam*etam*v*v - 2.*etam*v*v + v*v)**(1./2.))/2. - (Kv*Res*io)/2. + (Kv*etam*v)/2.
+
+        # If the voltage supplied is too low this function will NaN. However, that really means it won't spin
         omega1[np.isnan(omega1)] = 0.0
+        
+        # The torqu
         Q      = ((v-omega1/Kv)/Res -io)/Kv
         
         omega1[v==0] = 0.
