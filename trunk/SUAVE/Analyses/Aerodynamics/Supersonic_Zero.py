9b8d5014f64013d9d5c319b63dd656f4b3098e6e
planes <ebotero@stanford.edu>
2020-06-12 10:26:26 -0700

    removing aircraft span efficiency factor

--- trunk/SUAVE/Analyses/Aerodynamics/Supersonic_Zero.py
+++ trunk/SUAVE/Analyses/Aerodynamics/Supersonic_Zero.py
@@ -62,7 +62,6 @@ class Supersonic_Zero(Markup):
         settings.trim_drag_correction_factor        = 1.02
         settings.wing_parasite_drag_form_factor     = 1.1
         settings.fuselage_parasite_drag_form_factor = 2.3
-        settings.aircraft_span_efficiency_factor    = 0.0
         settings.viscous_lift_dependent_drag_factor = 0.38
         settings.drag_coefficient_increment         = 0.0000
         settings.spoiler_drag_increment             = 0.00 
