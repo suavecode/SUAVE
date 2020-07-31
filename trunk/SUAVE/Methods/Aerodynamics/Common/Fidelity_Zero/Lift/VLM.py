9970e118748ba0df1e848a7780c67dbd657277be
Jordan <jtrentsmart@gmail.com>
2020-07-30 22:20:48 -0700

    VLM.py Complete

--- trunk/SUAVE/Methods/Aerodynamics/Common/Fidelity_Zero/Lift/VLM.py
+++ trunk/SUAVE/Methods/Aerodynamics/Common/Fidelity_Zero/Lift/VLM.py
@@ -9,7 +9,13 @@
 
 # package imports
 import SUAVE
+
+import jax
 import jax.numpy as np
+from jax.ops import index_update
+
+import numpy as onp
+
 from SUAVE.Core import Units
 from SUAVE.Methods.Aerodynamics.Common.Fidelity_Zero.Lift.compute_induced_velocity_matrix import compute_induced_velocity_matrix
 from SUAVE.Methods.Aerodynamics.Common.Fidelity_Zero.Lift.compute_vortex_distribution     import compute_vortex_distribution
@@ -111,8 +117,10 @@ def VLM(conditions,settings,geometry):
     
     # Compute flow tangency conditions   
     inv_root_beta = np.zeros_like(mach)
-    inv_root_beta[mach<1] = 1/np.sqrt(1-mach[mach<1]**2)     
-    inv_root_beta[mach>1] = 1/np.sqrt(mach[mach>1]**2-1) 
+    inv_root_beta = index_update(inv_root_beta,jax.ops.index[mach < 1],1 / np.sqrt(1 - mach[mach < 1] ** 2))
+    inv_root_beta = index_update(inv_root_beta,jax.ops.index[mach > 1],1 / np.sqrt(mach[mach > 1] ** 2 - 1))
+    # inv_root_beta[mach<1] = 1/np.sqrt(1-mach[mach<1]**2)
+    # inv_root_beta[mach>1] = 1/np.sqrt(mach[mach>1]**2-1)
     if np.any(mach==1):
         raise('Mach of 1 cannot be used in building compressibility corrections.')
     inv_root_beta = np.atleast_2d(inv_root_beta)
@@ -155,39 +163,43 @@ def VLM(conditions,settings,geometry):
     Del_Y = np.abs(VD.YB1 - VD.YA1)*ones 
     
     # Use split to divide u, w, gamma, and Del_y into more arrays
-    u_n_w        = np.array(np.array_split(u,n_w,axis=1))
-    u_n_w_sw     = np.array(np.array_split(u,n_w*n_sw,axis=1)) 
-    w_ind_n_w    = np.array(np.array_split(w_ind,n_w,axis=1))
-    w_ind_n_w_sw = np.array(np.array_split(w_ind,n_w*n_sw,axis=1))    
-    gamma_n_w    = np.array(np.array_split(gamma,n_w,axis=1))
-    gamma_n_w_sw = np.array(np.array_split(gamma,n_w*n_sw,axis=1))
-    Del_Y_n_w    = np.array(np.array_split(Del_Y,n_w,axis=1))
-    Del_Y_n_w_sw = np.array(np.array_split(Del_Y,n_w*n_sw,axis=1)) 
+    u_n_w        = np.array(onp.array_split(u,n_w,axis=1))
+    u_n_w_sw     = np.array(onp.array_split(u,n_w*n_sw,axis=1))
+    w_ind_n_w    = np.array(onp.array_split(w_ind,n_w,axis=1))
+    w_ind_n_w_sw = np.array(onp.array_split(w_ind,n_w*n_sw,axis=1))    
+    gamma_n_w    = np.array(onp.array_split(gamma,n_w,axis=1))
+    gamma_n_w_sw = np.array(onp.array_split(gamma,n_w*n_sw,axis=1))
+    Del_Y_n_w    = np.array(onp.array_split(Del_Y,n_w,axis=1))
+    Del_Y_n_w_sw = np.array(onp.array_split(Del_Y,n_w*n_sw,axis=1))
     
     # lift coefficients on each wing   
-    machw             = np.tile(mach,len(wing_areas))     
-    L_wing            = np.sum(np.multiply(u_n_w+1,(gamma_n_w*Del_Y_n_w)),axis=2).T
-    CL_wing           = L_wing/(0.5*wing_areas)
-    CL_wing[machw>1]  = CL_wing[machw>1]*8 # supersonic lift off by a factor of 8 
+    machw   = np.tile(mach,len(wing_areas))
+    L_wing  = np.sum(np.multiply(u_n_w+1,(gamma_n_w*Del_Y_n_w)),axis=2).T
+    CL_wing = L_wing/(0.5*wing_areas)
+    CL_wing = index_update(CL_wing,jax.ops.index[machw>1],CL_wing[machw > 1] * 8)
+    # CL_wing[machw>1]  = CL_wing[machw>1]*8 # supersonic lift off by a factor of 8
     
     # drag coefficients on each wing  
-    Di_wing           = np.sum(np.multiply(-w_ind_n_w,(gamma_n_w*Del_Y_n_w)),axis=2).T
-    CDi_wing          = Di_wing/(0.5*wing_areas)
-    CDi_wing[machw>1] = CDi_wing[machw>1]*2   # supersonic drag off by a factor of 2 
+    Di_wing  = np.sum(np.multiply(-w_ind_n_w,(gamma_n_w*Del_Y_n_w)),axis=2).T
+    CDi_wing = Di_wing/(0.5*wing_areas)
+    CDi_wing = index_update(CDi_wing, jax.ops.index[machw>1], CDi_wing[machw>1]*2)
+    # CDi_wing[machw>1] = CDi_wing[machw>1]*2   # supersonic drag off by a factor of 2
     
     # Calculate each spanwise set of Cls and Cds
     cl_y        = np.sum(np.multiply(u_n_w_sw +1,(gamma_n_w_sw*Del_Y_n_w_sw)),axis=2).T/CS
     cdi_y       = np.sum(np.multiply(-w_ind_n_w_sw,(gamma_n_w_sw*Del_Y_n_w_sw)),axis=2).T/CS 
             
     # total lift and lift coefficient
-    L           = np.atleast_2d(np.sum(np.multiply((1+u),gamma*Del_Y),axis=1)).T 
-    CL          = L/(0.5*Sref)           # validated form page 402-404, aerodynamics for engineers # supersonic lift off by 2^3 
-    CL[mach>1]  = CL[mach>1]*8   # supersonic lift off by a factor of 8 
+    L  = np.atleast_2d(np.sum(np.multiply((1+u),gamma*Del_Y),axis=1)).T
+    CL = L/(0.5*Sref)           # validated form page 402-404, aerodynamics for engineers # supersonic lift off by 2^3
+    CL = index_update(CL, jax.ops.index[mach>1], CL[mach>1]*8)
+    # CL[mach>1]  = CL[mach>1]*8   # supersonic lift off by a factor of 8
     
     # total drag and drag coefficient
-    D           =   -np.atleast_2d(np.sum(np.multiply(w_ind,gamma*Del_Y),axis=1)).T   
-    CDi         = D/(0.5*Sref)  
-    CDi[mach>1] = CDi[mach>1]*2 # supersonic drag off by a factor of 2 
+    D   = -np.atleast_2d(np.sum(np.multiply(w_ind,gamma*Del_Y),axis=1)).T
+    CDi = D/(0.5*Sref)
+    CDi = index_update(CDi, jax.ops.index[mach>1], CDi[mach>1]*2)
+    # CDi[mach>1] = CDi[mach>1]*2 # supersonic drag off by a factor of 2
     
     # pressure coefficient
     U_tot       = np.sqrt((1+u)*(1+u) + v*v + w*w)
