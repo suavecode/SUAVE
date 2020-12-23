5ea5fbedbbf30cc683968aa976d109df196b1eef
stankarpuk93 <42099530+stankarpuk93@users.noreply.github.com>
2020-12-23 10:27:31 +0100

    Revert "descent init script update"
    This reverts commit 764d7ca13491d413b96d0026fc37117872ae4329.

--- trunk/SUAVE/Methods/Missions/Segments/Descent/__init__.py
+++ trunk/SUAVE/Methods/Missions/Segments/Descent/__init__.py
@@ -7,4 +7,3 @@ from . import Linear_Mach_Constant_Rate
 from . import Constant_Speed_Constant_Angle
 from . import Constant_Speed_Constant_Angle_Noise
 from . import Constant_EAS_Constant_Rate
-from . import Constant_CAS_Constant_Rate
