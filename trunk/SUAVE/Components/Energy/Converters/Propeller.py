correcting rotation bug

trunk/SUAVE/Components/Energy/Converters/Propeller.py
@@ -62,7 +62,7 @@ class Propeller(Energy_Component):
         self.thickness_to_chord        = 0.0
         self.blade_solidity            = 0.0
         self.thrust_angle              = 0.0
-        self.pitch_command             = 0.0
+        self.pitch_command             = 0.0 
         self.design_power              = None
         self.VTOL_flag                 = False
         self.design_thrust             = None
@@ -71,7 +71,7 @@ class Propeller(Energy_Component):
         self.airfoil_polars            = None
         self.airfoil_polar_stations    = None 
         self.radius_distribution       = None
-        self.rotation                  = None
+        self.rotation                  = [1]
         self.ducted                    = False         
         self.number_azimuthal_stations = 24
         self.induced_power_factor      = 1.48  #accounts for interference effects
@@ -242,8 +242,7 @@ class Propeller(Energy_Component):
             Vz  = np.repeat(Vz, Na,axis=1)
             Vz  = np.repeat(Vz, Nr,axis=2)
             
-            if rotation == None:
-                print("Propeller rotation not specified. Set to 1 (clockwise when viewed from behind).")
+            if (rotation != 1) or (rotation != -1):  
                 rotation = 1
             
             # compute resulting radial and tangential velocities in propeller frame