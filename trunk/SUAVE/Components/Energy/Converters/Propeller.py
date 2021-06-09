Incorporate Changes from Rotation Bug PR

trunk/SUAVE/Components/Energy/Converters/Propeller.py
@@ -72,7 +72,7 @@ class Propeller(Energy_Component):
         self.airfoil_polars            = None
         self.airfoil_polar_stations    = None 
         self.radius_distribution       = None
-        self.rotation                  = None
+        self.rotation                  = [1]
         self.ducted                    = False         
         self.number_azimuthal_stations = 24
         self.induced_power_factor      = 1.48  #accounts for interference effects
@@ -243,12 +243,13 @@ class Propeller(Energy_Component):
             Vz  = np.repeat(Vz, Na,axis=1)
             Vz  = np.repeat(Vz, Nr,axis=2)
             
-            if rotation == None:
-                print("Propeller rotation not specified. Set to 1 (clockwise when viewed from behind).")
-                rotation = 1
+            if (rotation == [1]) or (rotation == [-1]):
+                pass
+            else:
+                rotation = [1]
             
             # compute resulting radial and tangential velocities in propeller frame
-            ut =  ( Vz*np.cos(psi_2d)  ) * rotation
+            ut =  ( Vz*np.cos(psi_2d)  ) * rotation[0]
             ur =  (-Vz*np.sin(psi_2d)  )
             ua =  np.zeros_like(ut)
             