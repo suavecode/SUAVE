5f0bc7d8c469a5ffaa540e68a17180458d04db19
AaronBlau <aaron.blaufox@exosonic.com>
2021-06-25 00:39:43 -0700

    Revert "increase E190 regression n_sw to 7 to handle control surfaces"
    This reverts commit 272581af69000e92226ef00cff027511c73b863c.

--- regression/scripts/Embraer_E190_constThr/mission_Embraer_E190_constThr.py
+++ regression/scripts/Embraer_E190_constThr/mission_Embraer_E190_constThr.py
@@ -159,7 +159,7 @@ def base_analysis(vehicle):
     #  Aerodynamics Analysis
     aerodynamics = SUAVE.Analyses.Aerodynamics.Fidelity_Zero()
     aerodynamics.geometry = vehicle
-    aerodynamics.settings.number_spanwise_vortices   = 7 #changed from 4 to deal with control surface breaks
+    aerodynamics.settings.number_spanwise_vortices   = 4
     aerodynamics.settings.number_chordwise_vortices  = 2       
     aerodynamics.settings.drag_coefficient_increment = 0.0000
     analyses.append(aerodynamics)
