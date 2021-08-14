6c884c533df62ba4fe493d4f2d939171b5d2a12a
Jayant Mukhopadhaya <jmukho@stanford.edu>
2021-08-13 17:03:07 -0700

    fix typo

--- trunk/SUAVE/Analyses/Mission/Segments/Climb/Constant_CAS_Constant_Rate.py
+++ trunk/SUAVE/Analyses/Mission/Segments/Climb/Constant_CAS_Constant_Rate.py
@@ -58,7 +58,7 @@ class Constant_CAS_Constant_Rate(Unknown_Throttle):
         self.altitude_start       = None # Optional
         self.altitude_end         = 10. * Units.km
         self.climb_rate           = 3.  * Units.m / Units.s
-        self.equivalent_air_speed = 100 * Units.m / Units.s
+        self.calibrated_air_speed = 100 * Units.m / Units.s
         
         # --------------------------------------------------------------
         #   The Solving Process
