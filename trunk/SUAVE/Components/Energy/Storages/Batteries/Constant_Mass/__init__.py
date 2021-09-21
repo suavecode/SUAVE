85e70a7cfc6efaa3f9a08cb5168f89a37c651416
Racheal Erhard <rerhard@stanford.edu>
2021-09-21 14:39:42 -0700

    updating init

--- trunk/SUAVE/Components/Energy/Storages/Batteries/Constant_Mass/__init__.py
+++ trunk/SUAVE/Components/Energy/Storages/Batteries/Constant_Mass/__init__.py
@@ -2,7 +2,7 @@
 #Contains different battery types that don't change mass, such as lithium-ion and lithium-sulfur batteries
 # @ingroup Components-Energy-Storages-Batteries
 
-
+from .Lithium_Ion                  import Lithium_Ion
 from .Lithium_Ion_LiNCA_18650      import Lithium_Ion_LiNCA_18650 
 from .Lithium_Ion_LiFePO4_38120    import Lithium_Ion_LiFePO4_38120
 from .Lithium_Ion_LiNiMnCoO2_18650 import Lithium_Ion_LiNiMnCoO2_18650  
