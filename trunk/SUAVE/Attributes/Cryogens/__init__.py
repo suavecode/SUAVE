c24e4e1e3b06670d1b08a2e74887611ace3d07ee
Sofie C <sofie.claridge@gmail.com>
2021-11-15 13:30:57 +1300

    removing uneeded attributes

--- trunk/SUAVE/Attributes/Cryogens/__init__.py
+++
@@ -1,8 +0,0 @@
-## @defgroup Attributes-Cryogens Cryogens
-# Common Cryogens
-# @ingroup Attributes
-# classes
-
-from .Cryogen import Cryogen
-from .Liquid_H2 import Liquid_H2
-# from .Liquid_N2 import Liquid_N2
