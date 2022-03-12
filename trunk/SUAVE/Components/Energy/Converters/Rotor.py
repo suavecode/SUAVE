9aa566bd3245c0eaa4ebf35422e7f0ecedd9b3b9
Racheal Erhard <rerhard@stanford.edu>
2022-03-11 21:33:58 -0800

    correction to rotation calc

--- trunk/SUAVE/Components/Energy/Converters/Rotor.py
+++ trunk/SUAVE/Components/Energy/Converters/Rotor.py
@@ -605,10 +605,10 @@ class Rotor(Energy_Component):
         body_2_vehicle = sp.spatial.transform.Rotation.from_rotvec([0,np.pi,0]).as_matrix()
 
         # Go from vehicle frame to propeller vehicle frame: rot 1 including the extra body rotation
-        cpts      = len(np.atleast_1d(self.inputs.y_axis_rotation))
-        rots      = np.array(self.orientation_euler_angles) * 1.
-        rots      = np.repeat(rots[None,:], cpts, axis=0)
-        rots[:,1] = np.atleast_2d(self.inputs.y_axis_rotation)[:,0]
+        cpts       = len(np.atleast_1d(self.inputs.y_axis_rotation))
+        rots       = np.array(self.orientation_euler_angles) * 1.
+        rots       = np.repeat(rots[None,:], cpts, axis=0)
+        rots[:,1] += np.atleast_2d(self.inputs.y_axis_rotation)[:,0]
         
         vehicle_2_prop_vec = sp.spatial.transform.Rotation.from_rotvec(rots).as_matrix()
 
