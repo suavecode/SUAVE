a3b8f71336fb6f7195b2d0449ad229aea0393d7d
Racheal Erhard <racheal.erhard@jobyaviation.com>
2022-05-25 13:48:12 -0700

    wake data structure removal

--- trunk/SUAVE/Analyses/Propulsion/Rotor_Wake_Fidelity_One.py
+++ trunk/SUAVE/Analyses/Propulsion/Rotor_Wake_Fidelity_One.py
@@ -341,10 +341,9 @@ class Rotor_Wake_Fidelity_One(Energy_Component):
                 wVD.reshaped_wake[mat] += offset[2]        
         
         # update wake distribution
-        self.wake_vortex_distribution = wVD
         self.vortex_distribution = wVD
         return
-        
+    
         
 
 
