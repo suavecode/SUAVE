596756f618230ac05dc84753700828525d68dab7
Racheal Erhard <rerhard@stanford.edu>
2022-07-26 09:20:14 -0700

    adding logic for non-xfoil polar data

--- trunk/SUAVE/Methods/Geometry/Two_Dimensional/Cross_Section/Airfoil/import_airfoil_polars.py
+++ trunk/SUAVE/Methods/Geometry/Two_Dimensional/Cross_Section/Airfoil/import_airfoil_polars.py
@@ -12,6 +12,7 @@
 # ----------------------------------------------------------------------
 from SUAVE.Core import Data  
 import numpy as np
+import pandas as pd
 
 ## @ingroup Methods-Geometry-Two_Dimensional-Cross_Section-Airfoil
 def  import_airfoil_polars(airfoil_polar_files):
@@ -53,44 +54,60 @@ def  import_airfoil_polars(airfoil_polar_files):
     Ma           = np.zeros((num_airfoils,num_polars))
     
     AoA_interp = np.linspace(-6,16,dim_aoa) 
+    xfoilPolarFormat = False
     
     for i in range(num_airfoils): 
     
         for j in range(len(airfoil_polar_files[i])):   
             # Open file and read column names and data block
             f = open(airfoil_polar_files[i][j]) 
-            data_block = f.readlines()
-            f.close()
             
-            # Ignore header
-            for header_line in range(len(data_block)):
-                line = data_block[header_line]   
-                if 'Re =' in line:    
-                    Re[i,j] = float(line[25:40].strip().replace(" ", ""))
-                if 'Mach =' in line:    
-                    Ma[i,j] = float(line[7:20].strip().replace(" ", ""))    
-                if '---' in line:
-                    data_block = data_block[header_line+1:]
-                    break
+            
+            if xfoilPolarFormat:
+                data_block = f.readlines()
+                f.close()
                 
-            # Remove any extra lines at end of file:
-            last_line = False
-            while last_line == False:
-                if data_block[-1]=='\n':
-                    data_block = data_block[0:-1]
-                else:
-                    last_line = True
+                # Ignore header
+                for header_line in range(len(data_block)):
+                    line = data_block[header_line]   
+                    if 'Re =' in line:    
+                        Re[i,j] = float(line[25:40].strip().replace(" ", ""))
+                    if 'Mach =' in line:    
+                        Ma[i,j] = float(line[7:20].strip().replace(" ", ""))    
+                    if '---' in line:
+                        data_block = data_block[header_line+1:]
+                        break
+                    
+                # Remove any extra lines at end of file:
+                last_line = False
+                while last_line == False:
+                    if data_block[-1]=='\n':
+                        data_block = data_block[0:-1]
+                    else:
+                        last_line = True
+                
+                data_len = len(data_block)
+                airfoil_aoa= np.zeros(data_len)
+                airfoil_cl = np.zeros(data_len)
+                airfoil_cd = np.zeros(data_len)     
             
-            data_len = len(data_block)
-            airfoil_aoa= np.zeros(data_len)
-            airfoil_cl = np.zeros(data_len)
-            airfoil_cd = np.zeros(data_len)     
-        
-            # Loop through each value: append to each column
-            for line_count , line in enumerate(data_block):
-                airfoil_aoa[line_count] = float(data_block[line_count][0:8].strip())
-                airfoil_cl[line_count]  = float(data_block[line_count][10:17].strip())
-                airfoil_cd[line_count]  = float(data_block[line_count][20:27].strip())   
+                # Loop through each value: append to each column
+                for line_count , line in enumerate(data_block):
+                    airfoil_aoa[line_count] = float(data_block[line_count][0:8].strip())
+                    airfoil_cl[line_count]  = float(data_block[line_count][10:17].strip())
+                    airfoil_cd[line_count]  = float(data_block[line_count][20:27].strip())  
+            else:
+                # get Re, Ma info
+                ReString = airfoil_polar_files[i][j].split('Re_',1)[1].split('e6',1)[0]
+                MaString = airfoil_polar_files[i][j].split('Ma_',1)[1].split('_',1)[0]
+                Re[i,j] = float (ReString) * 1e6
+                Ma[i,j] = float (MaString)
+            
+                # Open file and read column names and data block
+                polarData = pd.read_csv(airfoil_polar_files[i][j], sep=" ")   
+                airfoil_aoa = polarData["alpha"]
+                airfoil_cl = polarData["CL"]
+                airfoil_cd = polarData["CD"]
           
             CL[i,j,:] = np.interp(AoA_interp,airfoil_aoa,airfoil_cl)
             CD[i,j,:] = np.interp(AoA_interp,airfoil_aoa,airfoil_cd)       
