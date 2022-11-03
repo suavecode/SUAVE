f39e4f2aa15eef5619a7c6d34791e049d7dfc0a4
planes <ebotero@stanford.edu>
2022-11-03 13:24:23 -0700

    allowing for different units in payload range

--- regression/scripts/electric_performance/electric_payload_range.py
+++ regression/scripts/electric_performance/electric_payload_range.py
@@ -35,7 +35,7 @@ def main():
 
     payload_range = electric_payload_range(vehicle, mission, 'cruise', display_plot=True)
 
-    payload_range_r = [     0.  , 105275.60817101, 112026.18114827]
+    payload_range_r = [ 0.        , 65.41523008, 69.60984136]
 
     assert (np.abs(payload_range.range[1] - payload_range_r[1]) / payload_range_r[1] < 1e-6), "Payload Range Regression Failed at Max Payload Test"
     assert (np.abs(payload_range.range[2] - payload_range_r[2]) / payload_range_r[2] < 1e-6), "Payload Range Regression Failed at Ferry Range Test"
