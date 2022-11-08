45230a7f4679521564aab946a253e5d6d708053e
planes <ebotero@stanford.edu>
2022-11-07 16:02:07 -0800

    individual level weights

--- trunk/SUAVE/Plots/Performance/Weights_Plots.py
+++ trunk/SUAVE/Plots/Performance/Weights_Plots.py
@@ -41,20 +41,43 @@ def weight_pie_charts(vehicle,analyses):
     # split into two categories: individual component level and type level (fuselage/wing)
     top_level_weights = Data()
     top_level_percent = Data()
-    run_sum           = 0
+    top_run_sum       = 0
     for key, value in vehicle.items():
         if key == 'mass_properties':
             continue
         if isinstance(value,Physical_Component.Container):
             mass                   = value.sum_mass()
             top_level_weights[key] = mass
-            run_sum               += mass
+            top_run_sum           += mass
             top_level_percent[key] = mass/results.total
             
-    top_level_weights['Additional'] = results.total - run_sum
+    top_level_weights['Additional'] = results.total - top_run_sum
     
-    print(top_level_weights)
-    
+    individual_weights  = Data()
+    individual_percents = Data()
     
 
     
+    _, individual_weights = recursive_masses(vehicle,individual_weights)
+    
+    
+    print(individual_weights)
+    
+    print(top_level_weights)
+
+
+    
+# This is not the same as data.do_recursive
+def recursive_masses(data_structure,res):
+    
+    for k,val in data_structure.items():
+        if k=='_diff': # If it's part of a config, skip
+            continue
+        if isinstance(val,Physical_Component): # Keep this mass value
+            d = Data({k:val.mass_properties.mass})
+            d.tag = k
+            res.append(d)
+        elif isinstance(val,Data):             # Recurse
+            data_structure, res = recursive_masses(val,res)
+
+    return data_structure, res
