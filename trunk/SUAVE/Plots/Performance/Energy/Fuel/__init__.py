d1c00a8aa464fd33b3c65159e504a8fde063af4b
Jordan <jtrentsmart@gmail.com>
2022-11-15 15:08:36 -0800

    Performance Plots Inits

---
+++ trunk/SUAVE/Plots/Performance/Energy/Fuel/__init__.py
@@ -0,0 +1,6 @@
+## @defgroup Plots-Performance-Energy-Fuel Fuel
+# Plotting functions related to fuel burning energy networks
+# @ingroup Plots-Performance-Energy
+
+from .plot_fuel_use import plot_fuel_use
+from .plot_altitude_sfc_weight import plot_altitude_sfc_weight
---
+++ trunk/SUAVE/Plots/Performance/Mission/__init__.py
@@ -0,0 +1,7 @@
+## @defgroup Plots-Performance-Mission Mission
+# Plots related to mission parameters
+# @ingroup Plots-Performance
+
+from .plot_aircraft_velocities  import plot_aircraft_velocities
+from .plot_flight_conditions    import plot_flight_conditions
+from .plot_flight_trajectory    import plot_flight_trajectory
