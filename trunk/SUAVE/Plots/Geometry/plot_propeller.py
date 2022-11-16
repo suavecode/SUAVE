86e75cf106fa64c5e2fea9cec0bcc2757fadbe51
Racheal Erhard <rerhard@stanford.edu>
2022-11-16 07:28:37 -0800

    switching propeller and airfoil plots to use plotly

--- trunk/SUAVE/Plots/Geometry/plot_propeller.py
+++ trunk/SUAVE/Plots/Geometry/plot_propeller.py
@@ -5,14 +5,16 @@
 # Modified: Apr 2020, M. Clarke
 #           Jul 2020, M. Clarke
 #           Feb 2022, M. Clarke
+#           Nov 2022, R. Erhard
 
 # ----------------------------------------------------------------------
 #  Imports
 # ----------------------------------------------------------------------  
 from SUAVE.Core import Units
-import matplotlib.pyplot as plt    
-from SUAVE.Plots.Geometry.plot_vehicle import plot_propeller_geometry
-from SUAVE.Components.Energy.Networks.Battery_Propeller import Battery_Propeller
+
+from plotly.subplots import make_subplots
+import pandas as pd
+import plotly.graph_objects as go
 
 ## @ingroup Plots-Geometry
 def plot_propeller(prop, face_color = 'red', edge_color = 'black' , save_figure = False, save_filename = "Propeller_Geometry", file_type = ".png"):
@@ -33,52 +35,33 @@ def plot_propeller(prop, face_color = 'red', edge_color = 'black' , save_figure 
     Properties Used:
     N/A	
     """	
-    # unpack  
-    
     # initalize figure 
-    fig_1 = plt.figure(save_filename + '_3D') 
-    fig_1.set_size_inches(8,8) 
-    axes_1 = plt.axes(projection='3d')
-    axes_1.view_init(elev= 30, azim= 30)   
-    axes_1.set_xlim(-1,1)
-    axes_1.set_ylim(-1,1)
-    axes_1.set_zlim(-1,1) 
-    axes_1.set_xlabel('x')  
-    axes_1.set_ylabel('y')   
-    axes_1.set_zlabel('z')    
+    fig = make_subplots(rows=2, cols=2)
     
-    # append a network for origin and thrust angle default values
-    network = Battery_Propeller() 
+    df1 = pd.DataFrame(dict(x=prop.radius_distribution, y=prop.twist_distribution/Units.degrees))
+    df2 = pd.DataFrame(dict(x=prop.radius_distribution, y=prop.chord_distribution))
+    df3 = pd.DataFrame(dict(x=prop.radius_distribution, y=prop.max_thickness_distribution))
+    df4 = pd.DataFrame(dict(x=prop.radius_distribution, y=prop.mid_chord_alignment))
     
-    # plot propeller geometry
-    plot_propeller_geometry(axes_1,prop) 
+    fig.append_trace(go.Line(df1), row=1, col=1)
+    fig.append_trace(go.Line(df2), row=1, col=2)    
+    fig.append_trace(go.Line(df3), row=2, col=1)     
+    fig.append_trace(go.Line(df4), row=2, col=2)        
     
-    if save_figure:
-        plt.savefig(save_filename + '_3D' + file_type)  
-        
-    fig_2 = plt.figure(save_filename + '_2D')
-    fig_2.set_size_inches(12, 8)    
-    axes_1 = fig_2.add_subplot(2,2,1)
-    axes_1.plot(prop.radius_distribution, prop.twist_distribution/Units.degrees,'bo-')  
-    axes_1.set_ylabel('Twist (Deg)') 
-    axes_1.set_xlabel('Radial Station')    
+    fig.update_xaxes(title_text="Radial Station", row=1, col=1)
+    fig.update_yaxes(title_text="Twist (Deg)", row=1, col=1)
+    fig.update_xaxes(title_text="Radial Station", row=1, col=2)
+    fig.update_yaxes(title_text="Chord (m)", row=1, col=2)
+    fig.update_xaxes(title_text="Radial Station", row=2, col=1)
+    fig.update_yaxes(title_text="Thickness (m)", row=2, col=1)
+    fig.update_xaxes(title_text="Radial Station", row=2, col=2)
+    fig.update_yaxes(title_text="Mid Chord Alignment (m)", row=2, col=2)
     
-    axes_2 = fig_2.add_subplot(2,2,2)
-    axes_2.plot(prop.radius_distribution, prop.chord_distribution,'bo-')    
-    axes_2.set_ylabel('Chord (m)') 
-    axes_2.set_xlabel('Radial Station')    
-    
-    axes_3 = fig_2.add_subplot(2,2,3)
-    axes_3.plot(prop.radius_distribution  , prop.max_thickness_distribution,'bo-')     
-    axes_3.set_ylabel('Thickness (m)')  
-    axes_3.set_xlabel('Radial Station')    
-    
-    axes_4 = fig_2.add_subplot(2,2,4)
-    axes_4.plot(prop.radius_distribution  , prop.mid_chord_alignment,'bo-')  
-    axes_4.set_ylabel('Mid Chord Alignment (m)')  
-    axes_4.set_xlabel('Radial Station')    
+    fig.update_layout(title_text="Propeller Geometry", height=700, showlegend=False)
     
     if save_figure:
-        plt.savefig(save_filename + '_2D' + file_type)  
-        
+        fig.write_image(save_filename + '_2D' + file_type) 
+    
+    fig.show()
+    
     return 
