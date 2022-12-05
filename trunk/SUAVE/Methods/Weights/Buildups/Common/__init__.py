409fafd08a6b3b952a8089da9ca0b2af04ad0e4f
planes <ebotero@stanford.edu>
2022-12-04 17:10:13 -0800

    stack mass fix

--- trunk/SUAVE/Methods/Weights/Buildups/Common/__init__.py
+++ trunk/SUAVE/Methods/Weights/Buildups/Common/__init__.py
@@ -3,9 +3,9 @@
 # utilizing buildup weight methods.
 # @ingroup Methods-Weights-Buildups
 
+from .stack_mass import stack_mass 
 from .elliptical_shell import elliptical_shell
 from .fuselage import fuselage
 from .prop import prop
 from .wing import wing
 from .wiring import wiring
-from .stack_mass import stack_mass #TODO: Determine Why This Doesn't Work Properly
