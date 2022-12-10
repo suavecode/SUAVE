cd57f93f12d93303dc2d5562593754223d5c31b0
Matthew Clarke <mclarke2@stanford.edu>
2022-12-09 22:28:57 -0400

    Update convert_to_third_octave_band.py

--- trunk/SUAVE/Methods/Noise/Fidelity_One/Noise_Tools/convert_to_third_octave_band.py
+++ trunk/SUAVE/Methods/Noise/Fidelity_One/Noise_Tools/convert_to_third_octave_band.py
@@ -63,6 +63,6 @@ def convert_to_third_octave_band(SPL,f,settings):
     SPL_array        = boolean*SPL_vals
     p_prefs          = 10**(SPL_array/10)
     SPL_third_octave = 10*np.log10(np.sum(boolean*p_prefs, axis =4)) 
-    SPL_third_octave[np.isinf(SPL_third_octave)]         = 0 
+    SPL_third_octave[np.isinf(SPL_third_octave)]  = 0 
     
     return SPL_third_octave
