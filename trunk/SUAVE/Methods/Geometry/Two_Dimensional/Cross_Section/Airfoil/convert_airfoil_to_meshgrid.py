7aafa004f4420eaa5eaf677470d010c6b9676bf3
Jordan <jtrentsmart@gmail.com>
2022-12-18 21:04:09 -0800

    First Draft

---
+++ trunk/SUAVE/Methods/Geometry/Two_Dimensional/Cross_Section/Airfoil/convert_airfoil_to_meshgrid.py
@@ -0,0 +1,35 @@
+## @ingroup [ADD DOCUMENTATION GROUP]
+# convert_airfoil_to_meshgrid.py
+# 
+# Created:    Dec 2022, J. Smart
+# Modified:   
+
+# ----------------------------------------------------------------------
+#  Imports
+# ---------------------------------------------------------------------- 
+
+#TODO: ADD IMPORTS
+
+## @ingroup [ADD DOCUMENTATION GROUP]
+def convert_airfoil_to_meshgrid(*args, **kwargs):
+    """ADD FUNCTION DESCRIPTION 
+
+    Assumptions:
+    None
+
+    Source:
+    None
+
+    Inputs:
+    [FUNCTION INPUT]   <INPUT TYPE>
+
+    Outputs: 
+    [FUNCTION OUTPUT]   <OUTPUT TYPE>
+
+    Properties Used:
+    N/A	
+    """
+    
+    #TODO: Write Function          
+
+    return
