b4751cf41981cdec054026ad3604fdbcbaacb370
Matthew Clarke <mclarke2@stanford.edu>
2022-12-25 20:09:28 +0100

    segment fuel updates

--- trunk/SUAVE/Visualization/Performance/Energy/Fuel/__init__.py
+++ trunk/SUAVE/Visualization/Performance/Energy/Fuel/__init__.py
@@ -2,5 +2,5 @@
 # Plotting functions related to fuel burning energy networks
 # @ingroup Visualization-Performance-Energy
 
-from .plot_fuel_use import plot_fuel_use
+from .plot_fuel_use            import plot_fuel_use
 from .plot_altitude_sfc_weight import plot_altitude_sfc_weight
