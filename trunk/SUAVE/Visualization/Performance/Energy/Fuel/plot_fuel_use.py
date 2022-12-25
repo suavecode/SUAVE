95140c0ed5838a826489f3b811bb1bd1cb1ce417
Matthew Clarke <mclarke2@stanford.edu>
2022-12-25 20:16:46 +0100

    Update plot_fuel_use.py

--- trunk/SUAVE/Visualization/Performance/Energy/Fuel/plot_fuel_use.py
+++ trunk/SUAVE/Visualization/Performance/Energy/Fuel/plot_fuel_use.py
@@ -60,17 +60,19 @@ def plot_fuel_use(results,
 
     # Get the segment-by-segment results for altitude, mass, and the
     # SFC (calculated)
-    prev_seg_fuel       = 0
-    prev_seg_extra_fuel = 0
-    total_fuel          = 0
+    prev_seg_fuel        = 0
+    prev_seg_extra_fuel  = 0
+    total_fuel           = 0
+    additional_fuel_flag = False 
     
     for seg_idx in range(len(results.segments)): 
         segment  = results.segments[seg_idx] 
         time     = segment.conditions.frames.inertial.time[:,0] / Units.min 
         
         if "has_additional_fuel" in segment.conditions.weights and segment.conditions.weights.has_additional_fuel == True:
-            fuel     = segment.conditions.weights.fuel_mass[:,0]
-            alt_fuel = segment.conditions.weights.additional_fuel_mass[:,0]
+            additional_fuel_flag = True 
+            fuel                 = segment.conditions.weights.fuel_mass[:,0]
+            alt_fuel             = segment.conditions.weights.additional_fuel_mass[:,0]
         
             if seg_idx == 0:
         
@@ -104,43 +106,72 @@ def plot_fuel_use(results,
             
                 # Append to collecting data-frame 
                 df = df.append(segment_frame) 
+        else: 
+            initial_weight  = results.segments[0].conditions.weights.total_mass[:,0][0]
+     
+            fuel        = segment.conditions.weights.total_mass[:,0] 
+            total_fuel  = np.negative(segment.conditions.weights.total_mass[:,0] - initial_weight )
+            
+
+            # Assemble data into temporary holding data frame 
+            segment_frame = pd.DataFrame(
+            np.column_stack((np.negative(current_fuel) ,np.negative(current_alt_fuel ),np.negative(current_fuel + current_alt_fuel))),
+            columns=['Tot_Fuel'], index=time)
+            segment_frame['Segment'] = [segment.tag for i in range(len(time))]
         
+            # Append to collecting data-frame 
+            df = df.append(segment_frame)  
+                
     # Set plot parameters 
-    fig = make_subplots(rows=3, cols=1,
-                        vertical_spacing=0.05)
+    if additional_fuel_flag: 
+        fig = make_subplots(rows=3, cols=1,vertical_spacing=0.05)
+    else:
+        fig = make_subplots(rows=1, cols=1,vertical_spacing=0.05)
 
     # Add traces to the figure for each value by segment.
 
     for seg, data in df.groupby("Segment"):
-        seg_name = ' '.join(seg.split("_")).capitalize()
+        if additional_fuel_flag: 
+            seg_name = ' '.join(seg.split("_")).capitalize()
+    
+            fig.add_trace(go.Scatter(
+                x=data.index,
+                y=data['Fuel'],
+                name=seg_name),
+                          row=1, col=1)
+    
+            fig.add_trace(go.Scatter(
+                x=data.index,
+                y=data['Add_Fuel'],
+                name=seg_name,
+                showlegend=False),
+                          row=2, col=1)
+    
+            fig.add_trace(go.Scatter(
+                x=data.index,
+                y=data['Tot_Fuel'],
+                name=seg_name,
+                showlegend=False),
+                          row=3, col=1)
+        else:
 
-        fig.add_trace(go.Scatter(
-            x=data.index,
-            y=data['Fuel'],
-            name=seg_name),
-                      row=1, col=1)
+            fig.add_trace(go.Scatter(
+                x=data.index,
+                y=data['Tot_Fuel'],
+                name=seg_name,
+                showlegend=False),
+                          row=1, col=1)            
 
-        fig.add_trace(go.Scatter(
-            x=data.index,
-            y=data['Add_Fuel'],
-            name=seg_name,
-            showlegend=False),
-                      row=2, col=1)
-
-        fig.add_trace(go.Scatter(
-            x=data.index,
-            y=data['Tot_Fuel'],
-            name=seg_name,
-            showlegend=False),
-                      row=3, col=1)
-
-    # Set sublot axis titles
-
-    fig.update_yaxes(title_text='Fuel (kg)', row=1, col=1)
-    fig.update_yaxes(title_text='Additional Fuel (kg)', row=2, col=1)
-    fig.update_yaxes(title_text='Total Fuel (kg)', row=3, col=1)
-
-    fig.update_xaxes(title_text='Time (min)', row=3, col=1)
+    # Set sublot axis titles 
+    if additional_fuel_flag:
+        fig.update_yaxes(title_text='Fuel (kg)', row=1, col=1)
+        fig.update_yaxes(title_text='Additional Fuel (kg)', row=2, col=1)
+        fig.update_yaxes(title_text='Total Fuel (kg)', row=3, col=1) 
+        fig.update_xaxes(title_text='Time (min)', row=3, col=1)
+    else: 
+        if additional_fuel_flag:
+            fig.update_yaxes(title_text='Fuel (kg)', row=1, col=1) 
+            fig.update_xaxes(title_text='Time (min)', row=1, col=1)        
 
     # Set overall figure layout style and legend title
 
