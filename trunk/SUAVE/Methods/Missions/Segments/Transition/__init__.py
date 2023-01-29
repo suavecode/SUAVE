43ef4e0580015a4bc625230940764ea446e32f39
Racheal Erhard <rerhard@stanford.edu>
2023-01-28 22:32:35 -0800

    Creating linear acceleration, constant pitchrate, constant altitude transition segment

--- trunk/SUAVE/Methods/Missions/Segments/Transition/__init__.py
+++ trunk/SUAVE/Methods/Missions/Segments/Transition/__init__.py
@@ -3,4 +3,5 @@
 # @ingroup Methods-Missions-Segments
 
 from . import Constant_Acceleration_Constant_Pitchrate_Constant_Altitude
+from . import Linear_Acceleration_Constant_Pitchrate_Constant_Altitude
 from . import Constant_Acceleration_Constant_Angle_Linear_Climb 
